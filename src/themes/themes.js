de0a7ed5aaf330149878bfafe00b1fc265e4ac5c
lesley <lnorton@mozilla.com>
2020-08-25 22:58:48 -0500

    Updates ui in ViewDevices.qml

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -5,20 +5,42 @@ const bgColor = "#F9F9FA";
 const bgWhite = "#FFFFFF";
 
 const green = "#3FE1B0";
+
+const grey = "#CACACA";
+const greyHovered = "#C2C2C2";
+const greyPressed = "#C2C2C2";
+
 const red = "#FF4F5E";
+const redHovered = "#E22850"
+const redPressed = "#C50042"
+
 const stateBgColor = "#321C64";
 
 const fontColor = "#6D6D6E";
 const fontColorDark = "#3D3D3D";
-const fontColorLight = "#3D3D3D";
 
 const fontSize = 15;
 const fontSizeLarge = 22;
 const fontSizeSmall = 13;
+const fontSizeSmallest = 11;
 const fontWeightBold = 600;
 
 const iconSize = 16;
-const labelLineHeight = 22
+const labelLineHeight = 22;
 
 const hSpacing = 20;
 const vSpacing = 24;
+const radius = 10;
+const windowMargin = 16;
+
+const redButton = {
+    "defaultColor" : red,
+    "buttonHovered": redHovered,
+    "buttonPressed": redPressed
+};
+
+const greyButton = {
+    "defaultColor": grey,
+    "buttonHovered": greyHovered,
+    "buttonPressed": greyPressed
+};
