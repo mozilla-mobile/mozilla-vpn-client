3cf5d9db42cb55db81ddd39760ee9f9c3e0ad7c7
Andrea Marchesini <baku@mozilla.com>
2020-10-14 22:33:22 +0200

    Removed a debugging font message

--- src/captiveportal/captiveportaldetection.cpp
+++ src/captiveportal/captiveportaldetection.cpp
@@ -64,4 +64,3 @@ void CaptivePortalDetection::detectCaptivePortal()
         emit captivePortalDetected();
     });
 }
-
--- src/fontloader.cpp
+++ src/fontloader.cpp
@@ -14,9 +14,4 @@ void FontLoader::loadFonts()
         int id = QFontDatabase::addApplicationFont(":/resources/fonts/" + *i);
         qDebug() << "Result:" << id;
     }
-
-    QFontDatabase a;
-
-    qDebug() << a.families();
-    qDebug() << a.bold("MetropolisSemiBold", "Regular");
 }
