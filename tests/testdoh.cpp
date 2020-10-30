66c69a057ebf7107cdb9ae34faf5987c477e95d3
Andrea Marchesini <amarchesini@mozilla.com>
2020-10-30 17:57:00 +0100

    Identation of the testDOH

--- tests/testdoh.cpp
+++ tests/testdoh.cpp
@@ -22,42 +22,51 @@ void TestDOH::basic_data()
     QTest::addRow("invalid") << QByteArray() << true << QStringList() << QStringList();
 
     QJsonObject json;
-    QTest::addRow("empty") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("empty") << QJsonDocument(json).toJson() << true << QStringList()
+                           << QStringList();
 
     json.insert("Status", 42);
-    QTest::addRow("error status") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("error status") << QJsonDocument(json).toJson() << true << QStringList()
+                                  << QStringList();
 
     json.insert("Status", 0);
     json.insert("Answer", 42);
-    QTest::addRow("invalid answer") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("invalid answer")
+        << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
 
     QJsonArray answers;
     json.insert("Answer", answers);
-    QTest::addRow("empty answer") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("empty answer") << QJsonDocument(json).toJson() << true << QStringList()
+                                  << QStringList();
 
     answers.append(42);
     json.insert("Answer", answers);
-    QTest::addRow("invalid answer object") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("invalid answer object")
+        << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
 
     QJsonObject answer;
     answers.replace(0, answer);
     json.insert("Answer", answers);
-    QTest::addRow("empty answer object") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("empty answer object")
+        << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
 
     answer.insert("type", 42);
     answers.replace(0, answer);
     json.insert("Answer", answers);
-    QTest::addRow("invalid answer object type") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("invalid answer object type")
+        << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
 
     answer.insert("type", 1);
     answers.replace(0, answer);
     json.insert("Answer", answers);
-    QTest::addRow("invalid answer object type") << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
+    QTest::addRow("invalid answer object type")
+        << QJsonDocument(json).toJson() << true << QStringList() << QStringList();
 
     answer.insert("data", "ipv4");
     answers.replace(0, answer);
     json.insert("Answer", answers);
-    QTest::addRow("ipv4") << QJsonDocument(json).toJson() << true << QStringList{"ipv4"} << QStringList();
+    QTest::addRow("ipv4") << QJsonDocument(json).toJson() << true << QStringList{"ipv4"}
+                          << QStringList();
 
     answer.insert("type", 28);
     answer.insert("data", "ipv6");
@@ -74,34 +83,36 @@ void TestDOH::basic()
     if (result) {
         // ipv4:
         TestHelper::networkConfig.append(
-                    TestHelper::NetworkConfig(TestHelper::NetworkConfig::Success, json));
+            TestHelper::NetworkConfig(TestHelper::NetworkConfig::Success, json));
         // ipv6:
         TestHelper::networkConfig.append(
-                    TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
+            TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
     } else {
         // ipv4:
         TestHelper::networkConfig.append(
-                    TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
+            TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
         // ipv6:
         TestHelper::networkConfig.append(
-                    TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
+            TestHelper::NetworkConfig(TestHelper::NetworkConfig::Failure, QByteArray()));
     }
 
     DOHRequest *r = new DOHRequest(this);
     r->resolve("foobar");
-    
+
     bool completed = false;
 
     QEventLoop loop;
-    connect(r, &DOHRequest::completed, [&](const QStringList &resultIpv4Addresses, const QStringList &resultIpv6Addresses) {
-        QFETCH(QStringList, ipv4Addresses);
-        QCOMPARE(ipv4Addresses, resultIpv4Addresses);
+    connect(r,
+            &DOHRequest::completed,
+            [&](const QStringList &resultIpv4Addresses, const QStringList &resultIpv6Addresses) {
+                QFETCH(QStringList, ipv4Addresses);
+                QCOMPARE(ipv4Addresses, resultIpv4Addresses);
 
-        QFETCH(QStringList, ipv6Addresses);
-        QCOMPARE(ipv6Addresses, resultIpv6Addresses);
+                QFETCH(QStringList, ipv6Addresses);
+                QCOMPARE(ipv6Addresses, resultIpv6Addresses);
 
-        completed = true;
-    });
+                completed = true;
+            });
 
     connect(r, &DOHRequest::destroyed, [&]() {
         QVERIFY(completed);
