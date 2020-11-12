c30e99c5915d660bf5c2be761e4c62fb13084a82
Sebastian Streich <streich.mobile@gmail.com>
2020-11-12 17:53:18 +0100

    Cleanup

--- src/platforms/android/androidutils.cpp
+++ src/platforms/android/androidutils.cpp
@@ -1,12 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "androidutils.h"
 
-
+#include <jni.h>
+#include <QtAndroid>
 #include <QAndroidJniEnvironment>
 #include <QAndroidJniObject>
-#include <jni.h>
-#include <QAndroidServiceConnection>
-#include <QDebug>
-#include <QtAndroid>
+
 
 /**
  *  Gets the Device
@@ -16,8 +18,13 @@ QString AndroidUtils::GetDeviceName(){
     jclass BUILD = env->FindClass("android/os/Build");
     jfieldID model = env->GetStaticFieldID(BUILD,"MODEL","Ljava/lang/String;");
     jstring value = (jstring) env->GetStaticObjectField(BUILD,model);
-
+    if(!value){
+        return QString("Android Device");
+    }
     const char* buffer = env->GetStringUTFChars(value,nullptr);
+    if(!buffer){
+        return QString("Android Device");
+    }
     QString res = QString(buffer);
     env->ReleaseStringUTFChars(value,buffer);
     return res;
