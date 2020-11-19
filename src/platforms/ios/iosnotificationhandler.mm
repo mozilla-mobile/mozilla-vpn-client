90714b286f30505e926fd7621856550fbe973f28
Andrea Marchesini <baku@mozilla.com>
2020-11-19 11:39:10 +0100

    Empty classes for android and ios

---
+++ src/platforms/ios/iosnotificationhandler.mm
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "platforms/ios/iosnotificationhandler.h"
+
+IOSNotificationHandler::IOSNotificationHandler(QObject *parent) : NotificationHandler(parent) {}
+
+void IOSNotificationHandler::notify(const QString &title, const QString &message)
+{
+    // TODO
+    Q_UNUSED(title);
+    Q_UNUSED(message);
+}
