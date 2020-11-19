e357c1537a0d9f7715ed26f05dd62a3bbf8880f6
Andrea Marchesini <baku@mozilla.com>
2020-11-19 11:39:10 +0100

    Empty classes for android and ios

---
+++ src/platforms/android/androidnotificationhandler.cpp
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "platforms/android/androidnotificationhandler.h"
+
+AndroidNotificationHandler::AndroidNotificationHandler(QObject *parent)
+    : NotificationHandler(parent)
+{}
+
+void AndroidNotificationHandler::notify(const QString &title, const QString &message)
+{
+    // TODO
+    Q_UNUSED(title);
+    Q_UNUSED(message);
+}
