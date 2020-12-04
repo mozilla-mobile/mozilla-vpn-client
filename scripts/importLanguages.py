0234a244038aa35cc800637df68e40e1b955de94
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-12-04 16:05:09 +0100

    Clean up script

--- scripts/importLanguages.py
+++ scripts/importLanguages.py
@@ -5,73 +5,83 @@
 
 # This script must be executed at the root of the repository.
 
+import argparse
 import xml.etree.ElementTree as ET
 import os
 import sys
 
-THRESHOLD = 0.70  # 70% Target Completeness for import
-FILES = []
+prod_locales = ["en"]
+# Include only locales above this threshold (e.g. 70%)
+l10n_threshold = 0.70
 
-PROD = False
-PROD_LANGS = ["en"]
-
-if len(sys.argv) > 1 and (sys.argv[1] == "-p" or sys.argv[1] == "--prod"):
-    PROD = True
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-p', '--prod', default=False, action="store_true", dest="isprod",
+    help='Build only for production locales.')
+args = parser.parse_args()
 
 # Step 1
-# Go through the i18n repo, check each xliff file and take
-# note which locale is >70% complete
-# Adds path of {.xliff / .ts } to FILES
+# Go through the i18n repo, check each XLIFF file and take
+# note which locale is complete above the minimum threshold.
+# Adds path of .xliff and .ts to l10n_files.
+l10n_files = []
 for locale in os.listdir('i18n'):
-    # Skip non folder
+    # Skip non folders
     if not os.path.isdir(os.path.join('i18n', locale)):
         continue
+
     # Skip hidden folders
     if locale.startswith('.'):
         continue
 
-    if PROD and not(locale in PROD_LANGS):
+    # Ignore locale if it's a production build and the locale should not be
+    # included
+    if args.isprod and locale not in prod_locales:
         continue
 
-    filePath = os.path.join('i18n', locale, 'mozillavpn.xliff')
-    tree = ET.parse(filePath)
+    xliff_path = os.path.join('i18n', locale, 'mozillavpn.xliff')
+
+    # If it's the source locale (en), ignore parsing for completeness and
+    # add it to the list.
+    if locale == 'en':
+        print(f'✔\t- en added (reference locale)')
+        l10n_files.append({
+            'ts': os.path.join('translations', f'mozillavpn_en.ts'),
+            'xliff': xliff_path
+        })
+        continue
+
+    tree = ET.parse(xliff_path)
     root = tree.getroot()
 
-    elementCount = 0
+    sources = 0
     translations = 0
 
     for element in root.iter('{urn:oasis:names:tc:xliff:document:1.2}source'):
-        elementCount += 1
+        sources += 1
     for element in root.iter('{urn:oasis:names:tc:xliff:document:1.2}target'):
-        if element.text:
-            translations += 1
+        translations += 1
 
-    complete = translations/(elementCount*1.0)
-    if complete < THRESHOLD:
-        print(f'❌\t- {locale} is translated at {round(complete*100,2)}%, at least {THRESHOLD*100}% is needed')
+    completeness = translations/(sources*1.0)
+    if completeness < l10n_threshold:
+        print(f'❌\t- {locale} is translated at {round(completeness*100, 2)}%, at least {l10n_threshold*100}% is needed')
         continue  # Not enough translations next file please
     baseName = f'mozillavpn_{locale}'
-    print(f'✔\t- {locale} added')
-    FILES.append({
+    print(f'✔\t- {locale} added ({round(completeness*100, 2)}% translated)')
+    l10n_files.append({
         'ts': os.path.join('translations', f'{baseName}.ts'),
-        'xliff': filePath
-    })
-
-if len(FILES) == 0 or len([x for x in FILES if x['ts'] == os.path.join('translations', 'mozillavpn_en.ts')]) == 0:
-    print('No fallback language (en) was imported')
-    os.system('lupdate src -no-obsolete -ts translations/mozillavpn_en.ts')
-    FILES.append({
-        'ts': os.path.join('translations', 'mozillavpn_en.ts')
+        'xliff': xliff_path
     })
 
 # Step 2
-# Write PRI file to import the done languages
-with open('translations/translations.pri', 'w') as projectFile:
-    projectFile.write('TRANSLATIONS += \\ \n')
-    for file in FILES:
-        projectFile.write(f"../{file['ts']} \\ \n")
-        os.system(f"touch {file['ts']}")
-    projectFile.write('\n \n##End')
+# Write PRI file to import the locales that are ready
+with open('translations/translations.pri', 'w') as pri_file:
+    output = []
+    output.append('TRANSLATIONS += \\ ')
+    for file in l10n_files:
+        output.append(f"../{file['ts']} \\ ")
+    output.append('\n \n##End')
+    pri_file.write('\n'.join(output))
 print('Updated translations.pri')
 
 # Step 3
@@ -79,9 +89,8 @@ print('Updated translations.pri')
 os.system('lupdate src/src.pro')
 
 # Step 4
-# Now import done translations into the files
-for file in FILES:
-    if 'xliff' in file.keys():
-        os.system(f"lconvert -if xlf -i {file['xliff']} -o {file['ts']}")
+# Now import translations into the files
+for l10n_file in l10n_files:
+    os.system(f"lconvert -if xlf -i {l10n_file['xliff']} -o {l10n_file['ts']}")
 
-print('Imported Languages')
+print(f'Imported {len(l10n_files)} locales')
