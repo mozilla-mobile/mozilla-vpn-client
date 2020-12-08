55115b28f4c01c2b6a0115c2129485980fe012a2
Andrea Marchesini <amarchesini@mozilla.com>
2020-12-08 11:29:23 +0100

    Tests for networkmanager

---
+++ tests/unit/testnetworkmanager.cpp
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "testnetworkmanager.h"
+#include "../../src/simplenetworkmanager.h"
+#include "helper.h"
+
+void TestNetworkManager::basic() {
+  SimpleNetworkManager snm;
+  QCOMPARE(&snm, NetworkManager::instance());
+  QVERIFY(snm.userAgent().contains("MozillaVPN"));
+}
+
+static TestNetworkManager s_testNetworkManager;
