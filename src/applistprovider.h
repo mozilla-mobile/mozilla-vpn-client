661f2498e0b5ea201c8ecbf53830acb87f08815e
Sebastian Streich <streich.mobile@gmail.com>
2020-12-09 17:28:10 +0100

    Fix formatting

--- src/applistprovider.h
+++ src/applistprovider.h
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
 #ifndef APPLISTPROVIDER_H
 #define APPLISTPROVIDER_H
 
@@ -10,29 +9,25 @@
 #include <QObject>
 #include <QQuickImageProvider>
 
-class AppListProvider : public QObject
-{
-    Q_OBJECT
-    Q_DISABLE_COPY_MOVE(AppListProvider)
+class AppListProvider : public QObject {
+  Q_OBJECT
+  Q_DISABLE_COPY_MOVE(AppListProvider)
 
-   public:
-    AppListProvider(QObject* parent) : QObject(parent){};
+ public:
+  AppListProvider(QObject* parent) : QObject(parent){};
 
-    virtual ~AppListProvider() = default;
+  virtual ~AppListProvider() = default;
 
+  // Requests a fresh Application List
+  // Impl should emit newAppList signal when done.
+  virtual void getApplicationList() = 0;
 
-    // Requests a fresh Application List
-    // Impl should emit newAppList signal when done.
-    virtual void getApplicationList() =0;
-
-signals:
-    // Should be emitted when the new Application List is Ready
-    // QMap key should be the identifier that the controller can
-    // use on activation.
-    // QMap Value should be a User readable Name of the app
-    void newAppList(QMap<QString,QString> applist);
-
+ signals:
+  // Should be emitted when the new Application List is Ready
+  // QMap key should be the identifier that the controller can
+  // use on activation.
+  // QMap Value should be a User readable Name of the app
+  void newAppList(QMap<QString, QString> applist);
 };
 
-
-#endif // APPLISTPROVIDER_H
+#endif  // APPLISTPROVIDER_H
--- src/platforms/dummy/dummyapplistprovider.cpp
+++ src/platforms/dummy/dummyapplistprovider.cpp
@@ -10,20 +10,19 @@ DummyAppListProvider::DummyAppListProvider(QObject* parent)
   MVPN_COUNT_CTOR(DummyAppListProvider);
 }
 
-DummyAppListProvider::~DummyAppListProvider(){
+DummyAppListProvider::~DummyAppListProvider() {
   MVPN_COUNT_DTOR(DummyAppListProvider);
 }
 
-void DummyAppListProvider::getApplicationList(){
-    QMap<QString,QString> appList;
-    appList["com.example.one"] = "Example App 1";
-    appList["com.example.two"] = "Example App 2";
-    appList["org.example.one"] = "Example App 3";
-    appList["org.example.two"] = "Example App 4";
-    appList["com.example.a"] = "Example App 5";
-    appList["com.example.b"] = "Example App 6";
-    appList["org.example.c"] = "Example App 7";
-    appList["org.example.d"] = "Example App 8";
-    emit newAppList(appList);
+void DummyAppListProvider::getApplicationList() {
+  QMap<QString, QString> appList;
+  appList["com.example.one"] = "Example App 1";
+  appList["com.example.two"] = "Example App 2";
+  appList["org.example.one"] = "Example App 3";
+  appList["org.example.two"] = "Example App 4";
+  appList["com.example.a"] = "Example App 5";
+  appList["com.example.b"] = "Example App 6";
+  appList["org.example.c"] = "Example App 7";
+  appList["org.example.d"] = "Example App 8";
+  emit newAppList(appList);
 }
-
