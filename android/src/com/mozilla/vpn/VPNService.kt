4223fc0d16e22ac16962f322ea4063a4831e5a1d
Sebastian Streich <streich.mobile@gmail.com>
2021-02-04 14:17:31 +0100

    Also fix formatting issues from #541

--- android/src/com/mozilla/vpn/VPNService.kt
+++ android/src/com/mozilla/vpn/VPNService.kt
@@ -4,7 +4,6 @@
 
 package org.mozilla.firefox.vpn
 
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
@@ -13,6 +12,7 @@ import com.mozilla.vpn.NotificationUtil
 import com.mozilla.vpn.VPNTunnel
 import com.wireguard.android.backend.*
 import com.wireguard.android.backend.GoBackend
+import com.wireguard.config.Config
 import java.lang.Exception
 
 class VPNService : android.net.VpnService() {
@@ -23,21 +23,20 @@ class VPNService : android.net.VpnService() {
     private var mConfig: Config? = null
 
     override fun onUnbind(intent: Intent?): Boolean {
-        if(state == Tunnel.State.DOWN){
+        if (state == Tunnel.State.DOWN) {
             // If the Qt Client got closed while we were not connected
             // we do not need to stay as a foreground service.
-            stopForeground(true);
+            stopForeground(true)
         }
         return super.onUnbind(intent)
     }
-
 
     /**
      * EntryPoint for the Service, gets Called when AndroidController.cpp
      * calles bindService. Returns the [VPNServiceBinder] so QT can send Requests to it.
      */
     override fun onBind(intent: Intent?): IBinder? {
-        NotificationUtil.show(this);
+        NotificationUtil.show(this)
         Log.v(tag, "Got Bind request")
         return mBinder
     }
@@ -50,9 +49,9 @@ class VPNService : android.net.VpnService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         this.startService(Intent(this, GoBackend.VpnService::class.java))
         intent?.let {
-            if(intent.getBooleanExtra("startOnly",false)){
+            if (intent.getBooleanExtra("startOnly", false)) {
                 Log.i(tag, "Start only!")
-                return super.onStartCommand(intent, flags, startId);
+                return super.onStartCommand(intent, flags, startId)
             }
             if (intent.getBooleanExtra("startOnBoot", false)) {
                 Log.v(tag, "Starting VPN because 'start on boot is enabled'")
@@ -61,7 +60,7 @@ class VPNService : android.net.VpnService() {
             }
         }
         // Go foreground if we need to connect
-        NotificationUtil.show(this);
+        NotificationUtil.show(this)
 
         if (this.mConfig == null) {
             // We don't have tunnel to turn on - Try to create one with last config the service got
@@ -70,12 +69,12 @@ class VPNService : android.net.VpnService() {
             if (lastConfString.isNullOrEmpty()) {
                 // We have nothing to connect to -> Exit
                 Log.e(tag, "VPN service was triggered without defining a Server or having a tunnel")
-                return super.onStartCommand(intent, flags, startId);
+                return super.onStartCommand(intent, flags, startId)
             }
             this.mConfig = mBinder.buildConfigFromJSON(lastConfString)
         }
         turnOn(this.mConfig)
-        return super.onStartCommand(intent, flags, startId);
+        return super.onStartCommand(intent, flags, startId)
     }
 
     // Invoked when the application is revoked.
@@ -94,7 +93,7 @@ class VPNService : android.net.VpnService() {
             return mBackend.getState(this.mTunnel)
         }
     var connectionTime: Long = 0
-        get(){
+        get() {
             return mTunnel.mConnectionTime
         }
 
@@ -113,19 +112,19 @@ class VPNService : android.net.VpnService() {
             return true
         }
         Log.e(tag, "Requesting VPN Permission")
-        return false;
+        return false
     }
 
-    fun turnOn(newConf:Config?) {
-        if(!checkPermissions()){
-            Log.e(tag,"turn on was called without no permissions present!");
-            mTunnel.abort();
-            return;
+    fun turnOn(newConf: Config?) {
+        if (!checkPermissions()) {
+            Log.e(tag, "turn on was called without no permissions present!")
+            mTunnel.abort()
+            return
         }
         NotificationUtil.show(this) // Go foreground
         this.startService(Intent(this, GoBackend.VpnService::class.java))
-        if(newConf == null && mConfig == null){
-            Log.e(tag, "Tried to start VPN with null config - abort");
+        if (newConf == null && mConfig == null) {
+            Log.e(tag, "Tried to start VPN with null config - abort")
         }
         if (newConf != null) {
             mConfig = newConf
@@ -134,27 +133,28 @@ class VPNService : android.net.VpnService() {
         // wgBackend will "DOWN" the tunnel before connecting,
         // we don't need the onchange event to be passed to the controller.
         // so set the current expected state to be down.
-        mTunnel.mState = Tunnel.State.DOWN;
-       try {
-            mBackend.setState(mTunnel, Tunnel.State.UP, mConfig);
-       }catch(e:Exception){
-            mTunnel.abort();
-       }
+        mTunnel.mState = Tunnel.State.DOWN
+        try {
+            mBackend.setState(mTunnel, Tunnel.State.UP, mConfig)
+        } catch (e: Exception) {
+            mTunnel.abort()
+        }
     }
 
     fun turnOff() {
         Log.v(tag, "Try to disable tunnel")
-        mBackend.setState(mTunnel,Tunnel.State.DOWN, null);
+        mBackend.setState(mTunnel, Tunnel.State.DOWN, null)
         stopForeground(false)
     }
 
-
-    companion object{
+    companion object {
         @JvmStatic
-        fun startService(c : Context){
-            c.applicationContext.startService(Intent(c.applicationContext,VPNService::class.java).apply {
-                putExtra("startOnly",true)
-            })
+        fun startService(c: Context) {
+            c.applicationContext.startService(
+                Intent(c.applicationContext, VPNService::class.java).apply {
+                    putExtra("startOnly", true)
+                }
+            )
         }
     }
 }
