a0b4fce87e9463a90a6abe4f9f71e697f535b643
Andrea Marchesini <amarchesini@mozilla.com>
2021-02-11 16:05:16 +0100

    Fix a language code issue

--- src/localizer.cpp
+++ src/localizer.cpp
@@ -57,16 +57,14 @@ void Localizer::initialize() {
   QDir dir(":/i18n");
   QStringList files = dir.entryList();
   for (const QString& file : files) {
-    if (!file.endsWith(".qm")) {
+    if (!file.startsWith("mozillavpn_") || !file.endsWith(".qm")) {
       continue;
     }
 
     QStringList parts = file.split(".");
     Q_ASSERT(parts.length() == 2);
 
-    parts = parts[0].split("_");
-
-    QString code = parts.at(1);
+    QString code = parts[0].remove(0, 11);
     m_languages.append(code);
   }
 }
