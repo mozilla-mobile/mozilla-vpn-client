99d95abe839f54ded40f172ee9a1cd3cb1f2d394
Sebastian Streich <strseb@users.noreply.github.com>
2021-02-23 22:43:23 +0100

    Update qt5_compile.sh
    Closes #617
    
    We're using qtwebsockets. So we should include it :)

--- scripts/qt5_compile.sh
+++ scripts/qt5_compile.sh
@@ -103,7 +103,6 @@ print Y "Wait..."
   -skip qtmultimedia \
   -skip qtserialport \
   -skip qtsensors \
-  -skip qtwebsockets \
   -skip qtgamepad \
   -skip qtwebchannel \
   -skip qtandroidextras \
