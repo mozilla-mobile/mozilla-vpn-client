8caa65446d32f6ab4c290363e84aede008858926
Andrea Marchesini <baku@mozilla.com>
2021-03-08 14:20:48 +0100

    BuildID in the github windows builds

--- scripts/windows_compile.bat
+++ scripts/windows_compile.bat
@@ -56,6 +56,9 @@ SET TEST_BUILD=F
 if "%1"== "-t" SET TEST_BUILD=T
 if "%1"== "--test" SET TEST_BUILD=T
 
+ECHO Extract version...
+FOR /F "tokens=2* delims==" %%A IN ('FINDSTR /IC:":VERSION" version.pri') DO call :SetVersion %%A
+
 SET FLAGS=BUILD_ID=%VERSION%
 
 if "%PROD_BUILD%" == "T" (
@@ -90,10 +93,7 @@ CALL :CopyDependency Microsoft_VC142_CRT_x64.msm "c:\\Program Files (x86)\\Micro
 ECHO Importing languages...
 python scripts\importLanguages.py
 
-ECHO Extract version...
-FOR /F "tokens=2* delims==" %%A IN ('FINDSTR /IC:":VERSION" version.pri') DO call :SetVersion %%A
-
-ECHO Creating the project...
+ECHO Creating the project with flags: %FLAGS%
 qmake -tp vc src/src.pro CONFIG-=debug CONFIG+=release CONFIG-=debug_and_release %FLAGS%
 
 IF %ERRORLEVEL% NEQ 0 (
