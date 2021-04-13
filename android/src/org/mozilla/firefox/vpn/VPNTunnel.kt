32eacad774c05c9007ff3403e6c0b51e6961a12b
Sebastian Streich <streich.mobile@gmail.com>
2021-04-13 14:32:42 +0200

    Cleanup the Package namespace

--- android/src/org/mozilla/firefox/vpn/VPNTunnel.kt
+++ android/src/org/mozilla/firefox/vpn/VPNTunnel.kt
@@ -2,10 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.mozilla.vpn
+package org.mozilla.firefox.vpn
 
 import com.wireguard.android.backend.Tunnel
-import org.mozilla.firefox.vpn.VPNServiceBinder
+
 
 class VPNTunnel : Tunnel {
     val mName: String
