220790f49fa4342607f053d2e3323e096fd21d6a
Andrea Marchesini <baku@mozilla.com>
2021-05-12 13:59:43 +0200

    Apply review comments

--- src/platforms/macos/macosutils.mm
+++ src/platforms/macos/macosutils.mm
@@ -91,7 +91,7 @@ void MacOSUtils::showDockIcon() {
 }
 
 void MacOSUtils::installerStrings() {
-  //% "Mozilla VPN for MacOS"
+  //% "Mozilla VPN for macOS"
   qtTrId("macosinstaller.title");
 
   //% "Unable to install"
@@ -103,26 +103,28 @@ void MacOSUtils::installerStrings() {
   //% "Previous Installation Detected"
   qtTrId("macosinstaller.previous_build.title");
 
-  //% "A previous installation of Mozilla VPN exists at /Applications/Mozilla VPN.app. This"
-  //% "installer will remove the previous installation prior to installing. Please back up any data"
-  //% "before proceeding."
+  //% "A previous installation of Mozilla VPN exists at /Applications/Mozilla "
+  //% "VPN.app. This installer will remove the previous installation prior to "
+  //% "installing. Please back up any data before proceeding."
   qtTrId("macosinstaller.previous_build.message");
 
-  //% "You will now be guided through the installation steps for the Mozilla VPN. Thank you for"
-  //% "choosing your VPN from the trusted pioneer of internet privacy."
+  //% "You will now be guided through the installation steps for the Mozilla "
+  //% "VPN. Thank you for choosing your VPN from the trusted pioneer of "
+  //% "internet privacy."
   qtTrId("macosinstaller.welcome.message1");
 
-  //% "Click \"Continue\" to continue the setup."
+  //% "Click “Continue” to continue the setup."
   qtTrId("macosinstaller.welcome.message2");
 
   //% "Success!"
   qtTrId("macosinstaller.conclusion.title");
 
-  //% "The Mozilla VPN is successfully installed. Go to your Applications folder to open up the VPN
-  // and start taking control of your online privacy."
+  //% "The Mozilla VPN is successfully installed. Go to your Applications "
+  //% "folder to open up the VPN and start taking control of your online "
+  //% "privacy."
   qtTrId("macosinstaller.conclusion.message1");
 
-  //% "Trouble with this install?"
+  //% "Trouble with this installation?"
   qtTrId("macosinstaller.conclusion.message2");
 
   //% "Get help."
