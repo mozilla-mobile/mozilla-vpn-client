b951ce6dab204c1aa7925cf85d46bb5067cb832f
Owen Kirby <oskirby@gmail.com>
2021-06-17 13:45:11 -0700

    Fix builds for VC2019 Community Edition

--- scripts/qt5_compile.bat
+++ scripts/qt5_compile.bat
@@ -69,7 +69,7 @@ IF NOT EXIST configure.bat (
   EXIT /B 1
 )
 
-configure -static -opensource -release -no-dbus -no-feature-qdbus -confirm-license -strip -silent -no-compile-examples -nomake tests -make libs -no-sql-psql -no-sql-sqlite -skip qt3d -skip webengine -skip qtmultimedia -skip qtserialport -skip qtsensors -skip qtgamepad -skip qtwebchannel -skip qtandroidextras -feature-imageformat_png -qt-libpng -qt-zlib -recheck-all -openssl-linked -I c:\MozillaVPNBuild\include -L c:\MozillaVPNBuild\lib -prefix c:\MozillaVPNBuild
+configure -static -opensource -debug-and-release -no-dbus -no-feature-qdbus -confirm-license -strip -silent -no-compile-examples -nomake tests -make libs -no-sql-psql -no-sql-sqlite -skip qt3d -skip webengine -skip qtmultimedia -skip qtserialport -skip qtsensors -skip qtgamepad -skip qtwebchannel -skip qtandroidextras -feature-imageformat_png -qt-libpng -qt-zlib -recheck-all -openssl-linked -I c:\MozillaVPNBuild\include -L c:\MozillaVPNBuild\lib -prefix c:\MozillaVPNBuild
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Failed to configure QT5.
   EXIT /B 1
