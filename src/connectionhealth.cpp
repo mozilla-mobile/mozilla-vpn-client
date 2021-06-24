mozilla-mobile/mozilla-vpn-client

commit ddec7fbf6a3df6c9863c947098fe0769b79eb09e

Use packet loss and RTT to drive silent server switching (#1231)

src/connectionhealth.cpp
@@ -16,6 +16,9 @@ constexpr uint32_t PING_TIME_UNSTABLE_SEC = 1;
 // In seconds, the timeout to detect no-signal pings.
 constexpr uint32_t PING_TIME_NOSIGNAL_SEC = 3;
 
+// Packet loss threshold for a connection to be considered unstable.
+constexpr double PING_LOSS_UNSTABLE_THRESHOLD = 0.10;
+
 namespace {
 Logger logger(LOG_NETWORKING, "ConnectionHealth");
 }
@@ -23,9 +26,10 @@ Logger logger(LOG_NETWORKING, "ConnectionHealth");
 ConnectionHealth::ConnectionHealth() {
   MVPN_COUNT_CTOR(ConnectionHealth);
 
-  connect(&m_noSignalTimer, &QTimer::timeout, this,
-          &ConnectionHealth::noSignalDetected);
   m_noSignalTimer.setSingleShot(true);
+
+  connect(&m_healthCheckTimer, &QTimer::timeout, this,
+          &ConnectionHealth::healthCheckup);
 
   connect(&m_pingHelper, &PingHelper::pingSentAndReceived, this,
           &ConnectionHealth::pingSentAndReceived);
@@ -58,6 +62,7 @@ void ConnectionHealth::start(const QString& serverIpv4Gateway,
   m_deviceAddress = deviceIpv4Address;
   m_pingHelper.start(serverIpv4Gateway, deviceIpv4Address);
   m_noSignalTimer.start(PING_TIME_NOSIGNAL_SEC * 1000);
+  m_healthCheckTimer.start(PING_TIME_UNSTABLE_SEC * 1000);
 }
 
 void ConnectionHealth::setStability(ConnectionStability stability) {
@@ -68,6 +73,8 @@ void ConnectionHealth::setStability(ConnectionStability stability) {
   logger.log() << "Stability changed:" << stability;
 
   if (stability == Unstable) {
+    MozillaVPN::instance()->silentSwitch();
+
     emit MozillaVPN::instance()->triggerGleanSample(
         GleanSample::connectionHealthUnstable);
   } else if (stability == NoSignal) {
@@ -99,22 +106,38 @@ void ConnectionHealth::connectionStateChanged() {
 }
 
 void ConnectionHealth::pingSentAndReceived(qint64 msec) {
+#ifdef QT_DEBUG
   logger.log() << "Ping answer received in msec:" << msec;
+#else
+  Q_UNUSED(msec);
+#endif
 
-  // If a ping has been received, we have signal. Restart the timer.
+  // If a ping has been received, we have signal. Restart the timers.
   m_noSignalTimer.start(PING_TIME_NOSIGNAL_SEC * 1000);
+  m_healthCheckTimer.start(PING_TIME_UNSTABLE_SEC * 1000);
 
-  if (msec < PING_TIME_UNSTABLE_SEC * 1000) {
-    setStability(Stable);
-  } else {
-    MozillaVPN::instance()->silentSwitch();
-    setStability(Unstable);
-  }
+  healthCheckup();
+
+  emit pingChanged();
 }
 
-void ConnectionHealth::noSignalDetected() {
-  logger.log() << "No signal detected";
-  setStability(NoSignal);
+void ConnectionHealth::healthCheckup() {
+  // If the no-signal timer has elapsed, then we probably lost the connection.
+  if (!m_noSignalTimer.isActive()) {
+    setStability(NoSignal);
+  }
+  // If there are too many lost pings, then mark the connection as unstable.
+  else if (m_pingHelper.loss() > PING_LOSS_UNSTABLE_THRESHOLD) {
+    setStability(Unstable);
+  }
+  // If recent pings took to long, then mark the connection as unstable.
+  else if (m_pingHelper.maximum() > (PING_TIME_UNSTABLE_SEC * 1000)) {
+    setStability(Unstable);
+  }
+  // Otherwise, the connection is stable.
+  else {
+    setStability(Stable);
+  }
 }
 
 void ConnectionHealth::applicationStateChanged(Qt::ApplicationState state) {

src/connectionhealth.h
@@ -22,6 +22,8 @@ class ConnectionHealth final : public QObject {
 
   Q_PROPERTY(ConnectionStability stability READ stability()
                  NOTIFY stabilityChanged)
+  Q_PROPERTY(uint latency READ latency NOTIFY pingChanged)
+  Q_PROPERTY(double loss READ loss NOTIFY pingChanged)
 
  public:
   ConnectionHealth();
@@ -29,12 +31,16 @@ class ConnectionHealth final : public QObject {
 
   ConnectionStability stability() const { return m_stability; }
 
+  uint latency() const { return m_pingHelper.latency(); }
+  double loss() const { return m_pingHelper.loss(); }
+
  public slots:
   void connectionStateChanged();
   void applicationStateChanged(Qt::ApplicationState state);
 
  signals:
   void stabilityChanged();
+  void pingChanged();
 
  private:
   void stop();
@@ -45,12 +51,13 @@ class ConnectionHealth final : public QObject {
 
   void setStability(ConnectionStability stability);
 
-  void noSignalDetected();
+  void healthCheckup();
 
  private:
   ConnectionStability m_stability = Stable;
 
   QTimer m_noSignalTimer;
+  QTimer m_healthCheckTimer;
 
   PingHelper m_pingHelper;
 

src/pinghelper.cpp
@@ -7,11 +7,27 @@
 #include "logger.h"
 #include "pingsender.h"
 
+#if defined(MVPN_LINUX) || defined(MVPN_ANDROID)
+#  include "platforms/linux/linuxpingsender.h"
+#elif defined(MVPN_MACOS) || defined(MVPN_IOS)
+#  include "platforms/macos/macospingsender.h"
+#elif defined(MVPN_WINDOWS)
+#  include "platforms/windows/windowspingsender.h"
+#elif defined(MVPN_DUMMY) || defined(UNIT_TEST)
+#  include "platforms/dummy/dummypingsender.h"
+#else
+#  error "Unsupported platform"
+#endif
+
+#include <QDateTime>
+
+#include <cmath>
+
 // Any X seconds, a new ping.
 constexpr uint32_t PING_TIMOUT_SEC = 1;
 
-// How many concurrent pings
-constexpr int PINGS_MAX = 20;
+// Maximum window size for ping statistics.
+constexpr int PING_STATS_WINDOW = 32;
 
 namespace {
 Logger logger(LOG_NETWORKING, "PingHelper");
@@ -19,6 +35,9 @@ Logger logger(LOG_NETWORKING, "PingHelper");
 
 PingHelper::PingHelper() {
   MVPN_COUNT_CTOR(PingHelper);
+
+  m_sequence = 0;
+  m_pingData.resize(PING_STATS_WINDOW);
 
   connect(&m_pingTimer, &QTimer::timeout, this, &PingHelper::nextPing);
 }
@@ -31,53 +50,145 @@ void PingHelper::start(const QString& serverIpv4Gateway,
 
   m_gateway = serverIpv4Gateway;
   m_source = deviceIpv4Address.section('/', 0, 0);
+  m_pingSender =
+#if defined(MVPN_LINUX) || defined(MVPN_ANDROID)
+      new LinuxPingSender(m_source, this);
+#elif defined(MVPN_MACOS) || defined(MVPN_IOS)
+      new MacOSPingSender(m_source, this);
+#elif defined(MVPN_WINDOWS)
+      new WindowsPingSender(m_source, this);
+#else
+      new DummyPingSender(m_source, this);
+#endif
+  connect(m_pingSender, &PingSender::recvPing, this, &PingHelper::pingReceived);
+
+  // Reset the ping statistics
+  m_sequence = 0;
+  for (int i = 0; i < PING_STATS_WINDOW; i++) {
+    m_pingData[i].timestamp = -1;
+    m_pingData[i].latency = -1;
+    m_pingData[i].sequence = 0;
+  }
+
   m_pingTimer.start(PING_TIMOUT_SEC * 1000);
 }
 
 void PingHelper::stop() {
   logger.log() << "PingHelper deactivated";
 
-  m_pingTimer.stop();
-
-  for (PingSender* pingSender : m_pings) {
-    pingSender->deleteLater();
+  if (m_pingSender) {
+    delete m_pingSender;
+    m_pingSender = nullptr;
   }
-  m_pings.clear();
+
+  m_pingTimer.stop();
 }
 
 void PingHelper::nextPing() {
-  logger.log() << "Sending a new ping. Total:" << m_pings.length();
+#ifdef QT_DEBUG
+  logger.log() << "Sending ping seq:" << m_sequence;
+#endif
 
-  PingSender* pingSender = new PingSender(this);
-  connect(pingSender, &PingSender::completed, this, &PingHelper::pingReceived);
-  m_pings.append(pingSender);
-  pingSender->send(m_gateway, m_source);
+  // The ICMP sequence number is used to match replies with their originating
+  // request, and serves as an index into the circular buffer. Overflows of
+  // the sequence number acceptable.
+  int index = m_sequence % PING_STATS_WINDOW;
+  m_pingData[index].timestamp = QDateTime::currentMSecsSinceEpoch();
+  m_pingData[index].latency = -1;
+  m_pingData[index].sequence = m_sequence;
+  m_pingSender->sendPing(m_gateway, m_sequence);
 
-  while (m_pings.length() > PINGS_MAX) {
-    m_pings.at(0)->deleteLater();
-    m_pings.removeAt(0);
+  m_sequence++;
+}
+
+void PingHelper::pingReceived(quint16 sequence) {
+  int index = sequence % PING_STATS_WINDOW;
+  if (m_pingData[index].sequence == sequence) {
+    qint64 sendTime = m_pingData[index].timestamp;
+    m_pingData[index].latency = QDateTime::currentMSecsSinceEpoch() - sendTime;
+    emit pingSentAndReceived(m_pingData[index].latency);
+#ifdef QT_DEBUG
+    logger.log() << "Ping answer received seq:" << sequence
+                 << "avg:" << latency()
+                 << "loss:" << QString("%1%").arg(loss() * 100.0)
+                 << "stddev:" << stddev();
+#endif
   }
 }
 
-void PingHelper::pingReceived(PingSender* pingSender, qint64 msec) {
-  logger.log() << "Ping answer received in msec:" << msec;
+uint PingHelper::latency() const {
+  int recvCount = 0;
+  qint64 totalMsec = 0;
 
-  if (!m_pingTimer.isActive()) {
-    logger.log() << "Race condition. Let's ignore this ping";
-    return;
-  }
-
-  QMutableListIterator<PingSender*> i(m_pings);
-  while (i.hasNext()) {
-    PingSender* thisPingSender = i.next();
-    if (thisPingSender != pingSender) {
+  for (const PingSendData& data : m_pingData) {
+    if (data.latency < 0) {
       continue;
     }
-
-    i.remove();
-    break;
+    recvCount++;
+    totalMsec += data.latency;
   }
 
-  pingSender->deleteLater();
-  emit pingSentAndReceived(msec);
+  if (recvCount <= 0) {
+    return 0.0;
+  }
+
+  // Add half the denominator to produce nearest-integer rounding.
+  totalMsec += recvCount / 2;
+  return totalMsec / recvCount;
+}
+
+uint PingHelper::stddev() const {
+  int recvCount = 0;
+  qint64 totalVariance = 0;
+  uint average = PingHelper::latency();
+
+  for (const PingSendData& data : m_pingData) {
+    if (data.latency < 0) {
+      continue;
+    }
+    recvCount++;
+    totalVariance += (average - data.latency) * (average - data.latency);
+  }
+
+  if (recvCount <= 0) {
+    return 0.0;
+  }
+
+  return std::sqrt((double)totalVariance / recvCount);
+}
+
+uint PingHelper::maximum() const {
+  uint maxRtt = 0;
+
+  for (const PingSendData& data : m_pingData) {
+    if (data.latency < 0) {
+      continue;
+    }
+    if (data.latency > maxRtt) {
+      maxRtt = data.latency;
+    }
+  }
+  return maxRtt;
+}
+
+double PingHelper::loss() const {
+  int sendCount = 0;
+  int recvCount = 0;
+  // Don't count pings that are possibly still in flight as losses.
+  qint64 sendBefore =
+      QDateTime::currentMSecsSinceEpoch() - (PING_TIMOUT_SEC * 1000);
+
+  for (const PingSendData& data : m_pingData) {
+    if (data.latency > 0) {
+      recvCount++;
+      sendCount++;
+    } else if ((data.timestamp > 0) && (data.timestamp < sendBefore)) {
+      sendCount++;
+    }
+  }
+
+  if (sendCount <= 0) {
+    return 0.0;
+  }
+  return (double)(sendCount - recvCount) / sendCount;
 }

src/pinghelper.h
@@ -8,6 +8,7 @@
 #include <QList>
 #include <QObject>
 #include <QTimer>
+#include <QVector>
 
 class PingSender;
 
@@ -24,6 +25,10 @@ class PingHelper final : public QObject {
              const QString& deviceIpv4Address);
 
   void stop();
+  uint latency() const;
+  uint stddev() const;
+  uint maximum() const;
+  double loss() const;
 
  signals:
   void pingSentAndReceived(qint64 msec);
@@ -31,15 +36,28 @@ class PingHelper final : public QObject {
  private:
   void nextPing();
 
-  void pingReceived(PingSender* pingSender, qint64 msec);
+  void pingReceived(quint16 sequence);
 
  private:
   QString m_gateway;
   QString m_source;
+  quint16 m_sequence = 0;
+
+  class PingSendData {
+   public:
+    PingSendData() {
+      timestamp = -1;
+      latency = -1;
+      sequence = 0;
+    }
+    qint64 timestamp;
+    qint64 latency;
+    quint16 sequence;
+  };
+  QVector<PingSendData> m_pingData;
 
   QTimer m_pingTimer;
-
-  QList<PingSender*> m_pings;
+  PingSender* m_pingSender = nullptr;
 };
 
 #endif  // PINGHELPER_H

src/pingsender.cpp
@@ -1,63 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#include "pingsender.h"
-#include "leakdetector.h"
-#include "logger.h"
-#include "pingsendworker.h"
-
-#if defined(MVPN_LINUX) || defined(MVPN_ANDROID)
-#  include "platforms/linux/linuxpingsendworker.h"
-#elif defined(MVPN_MACOS) || defined(MVPN_IOS)
-#  include "platforms/macos/macospingsendworker.h"
-#elif defined(MVPN_WINDOWS)
-#  include "platforms/windows/windowspingsendworker.h"
-#elif defined(MVPN_DUMMY) || defined(UNIT_TEST)
-#  include "platforms/dummy/dummypingsendworker.h"
-#else
-#  error "Unsupported platform"
-#endif
-
-namespace {
-Logger logger(LOG_NETWORKING, "PingSender");
-}
-
-PingSender::PingSender(QObject* parent) : QObject(parent) {
-  MVPN_COUNT_CTOR(PingSender);
-
-  m_time.start();
-
-  PingSendWorker* worker =
-#if defined(MVPN_LINUX) || defined(MVPN_ANDROID)
-      new LinuxPingSendWorker(this);
-#elif defined(MVPN_MACOS) || defined(MVPN_IOS)
-      new MacOSPingSendWorker(this);
-#elif defined(MVPN_WINDOWS)
-      new WindowsPingSendWorker(this);
-#else
-      new DummyPingSendWorker(this);
-#endif
-
-  connect(this, &PingSender::sendPing, worker, &PingSendWorker::sendPing);
-  connect(worker, &PingSendWorker::pingFailed, this, &PingSender::pingFailed);
-  connect(worker, &PingSendWorker::pingSucceeded, this,
-          &PingSender::pingSucceeded);
-}
-
-PingSender::~PingSender() { MVPN_COUNT_DTOR(PingSender); }
-
-void PingSender::send(const QString& destination, const QString& source) {
-  logger.log() << "PingSender send to" << destination;
-  emit sendPing(destination, source);
-}
-
-void PingSender::pingFailed() {
-  logger.log() << "PingSender - Ping Failed";
-  emit completed(this, m_time.elapsed());
-}
-
-void PingSender::pingSucceeded() {
-  logger.log() << "PingSender - Ping Succeeded";
-  emit completed(this, m_time.elapsed());
-}

src/pingsender.h
@@ -8,28 +8,17 @@
 #include <QElapsedTimer>
 #include <QObject>
 
-class PingSender final : public QObject {
+class PingSender : public QObject {
   Q_OBJECT
   Q_DISABLE_COPY_MOVE(PingSender)
 
  public:
-  PingSender(QObject* parent);
-  ~PingSender();
+  PingSender(QObject* parent = nullptr) : QObject(parent) {}
 
-  void send(const QString& destination, const QString& source);
+  virtual void sendPing(const QString& destination, quint16 sequence) = 0;
 
  signals:
-  void completed(PingSender* pingSender, qint64 msec);
-
-  // internal only
-  void sendPing(const QString& destination, const QString& source);
-
- private slots:
-  void pingFailed();
-  void pingSucceeded();
-
- private:
-  QElapsedTimer m_time;
+  void recvPing(quint16 sequence);
 };
 
 #endif  // PINGSENDER_H

src/pingsendworker.h
@@ -1,24 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#ifndef PINGSENDWORKER_H
-#define PINGSENDWORKER_H
-
-#include <QObject>
-
-class PingSendWorker : public QObject {
-  Q_OBJECT
-
- public:
-  PingSendWorker(QObject* parent = nullptr) : QObject(parent) {}
-
- public slots:
-  virtual void sendPing(const QString& destination, const QString& source) = 0;
-
- signals:
-  void pingSucceeded();
-  void pingFailed();
-};
-
-#endif  // PINGSENDWORKER_H

src/platforms/dummy/dummypingsender.cpp
@@ -1,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "dummypingsender.h"
+#include "leakdetector.h"
+#include "logger.h"
+
+namespace {
+Logger logger(LOG_NETWORKING, "DummyPingSender");
+}
+
+DummyPingSender::DummyPingSender(const QString& source, QObject* parent)
+    : PingSender(parent) {
+  MVPN_COUNT_CTOR(DummyPingSender);
+  Q_UNUSED(source);
+}
+
+DummyPingSender::~DummyPingSender() { MVPN_COUNT_DTOR(DummyPingSender); }
+
+void DummyPingSender::sendPing(const QString& dest, quint16 sequence) {
+  logger.log() << "Dummy ping to:" << dest;
+  emit recvPing(sequence);
+}

commit b5cf8e663197da7f665ae7ca84a236a7c3f0d5b1

Fix: No-Signal shown after disconnecting VPN
The last commit ddec7fbf6a3df6c9863c947098fe0769b79eb09e introduced
a bug where the health check timer would be left running after stopping
the connection health check. This would trigger a No-Signal warning
even though the VPN is disconnected.

src/connectionhealth.cpp
@@ -45,6 +45,7 @@ void ConnectionHealth::stop() {
 
   m_pingHelper.stop();
   m_noSignalTimer.stop();
+  m_healthCheckTimer.stop();
 
   setStability(Stable);
 }