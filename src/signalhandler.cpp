76a6ba01112d57f9affd10e187192c6f100d8305
Owen Kirby <oskirby@gmail.com>
2021-07-09 05:32:17 -0400

    Fix broken SignalHandler slot connection on Qt<5.15 (#1276)
    The API to the QSocketNotifier changed between Qt 5.14 and 5.15, which
    caused the SignalHandler class to stop receiving signals. We can work
    around the problem by using function pointers instead of marshalling
    the signal with the SIGNAL() macro.

--- src/signalhandler.cpp
+++ src/signalhandler.cpp
@@ -6,6 +6,7 @@
 #include "logger.h"
 #include "signal.h"
 
+#include <fcntl.h>
 #include <unistd.h>
 
 namespace {
@@ -31,11 +32,11 @@ SignalHandler::SignalHandler() {
     logger.log() << "Unable to create signal wakeup pipe";
     return;
   }
+  fcntl(m_pipefds[0], F_SETFL, fcntl(m_pipefds[0], F_GETFL) | O_NONBLOCK);
   s_signalpipe = m_pipefds[1];
   m_notifier = new QSocketNotifier(m_pipefds[0], QSocketNotifier::Read, this);
-  connect(m_notifier,
-          SIGNAL(activated(QSocketDescriptor, QSocketNotifier::Type)),
-          SLOT(pipeReadReady()));
+  connect(m_notifier, &QSocketNotifier::activated, this,
+          &SignalHandler::pipeReadReady);
 
   struct sigaction sa;
   sa.sa_handler = SignalHandler::saHandler;
