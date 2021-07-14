ebe3ec7a8a96751f77d704926e20eac03e120afe
Owen Kirby <oskirby@gmail.com>
2021-07-14 14:39:21 -0700

    Fix logo URL when connected to staging

--- src/constants.h
+++ src/constants.h
@@ -70,9 +70,8 @@ CONSTEXPR(uint32_t, surveyTimerMsec, 300000, 4000, 0)
 
 constexpr const char* API_PRODUCTION_URL = "https://vpn.mozilla.org";
 constexpr const char* API_STAGING_URL = "https://stage-vpn.guardian.nonprod.cloudops.mozgcp.net";
+constexpr const char* LOGO_URL = ":/ui/resources/logo-dock.png";
 
-PRODBETAEXPR(const char*, logoUrl, ":/ui/resources/logo-dock.png",
-             ":/ui/resources/logo-dock-beta.png")
 PRODBETAEXPR(const char*, fxaUrl, "https://api.accounts.firefox.com",
              "https://api-accounts.stage.mozaws.net")
 PRODBETAEXPR(
