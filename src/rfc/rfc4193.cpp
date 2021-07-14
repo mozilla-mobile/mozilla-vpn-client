e3f0c212e5790b48bc6bf3d339be06b26ae7628f
Andrea Marchesini <amarchesini@mozilla.com>
2021-07-14 10:15:52 +0200

    Support captive-portal in ipv6 - closes #593

--- src/rfc/rfc4193.cpp
+++ src/rfc/rfc4193.cpp
@@ -5,25 +5,10 @@
 #include "rfc4193.h"
 
 // static
-QList<IPAddressRange> RFC4193::ipv6() {
-  // Allow all IP's except fc00::/7
-  QList<IPAddressRange> list;
-  list.append(IPAddressRange("0:0:0:0:0:0:0:0", 1, IPAddressRange::IPv6));
-  list.append(IPAddressRange("8000::", 2, IPAddressRange::IPv6));
-  list.append(IPAddressRange("c000::", 3, IPAddressRange::IPv6));
-  list.append(IPAddressRange("e000::", 4, IPAddressRange::IPv6));
-  list.append(IPAddressRange("f000::", 5, IPAddressRange::IPv6));
-  list.append(IPAddressRange("f800::", 6, IPAddressRange::IPv6));
+QList<IPAddress> RFC4193::ipv6() {
+  QList<IPAddress> list;
 
-  list.append(IPAddressRange("fc01::", 16, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc02::", 15, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc04::", 14, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc08::", 13, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc10::", 12, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc20::", 11, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc40::", 10, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fc80::", 9, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fd00::", 8, IPAddressRange::IPv6));
-  list.append(IPAddressRange("fe00::", 7, IPAddressRange::IPv6));
+  list.append(IPAddress::create("fc00::/7"));
+
   return list;
 }
