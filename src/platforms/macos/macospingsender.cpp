fe266f52f9bbfe8fddd2e1396af028a3f2c87aae
Owen Kirby <oskirby@gmail.com>
2021-07-14 12:28:44 -0400

    Add fallback to raw sockets in LinuxPingSender (#1321)
    Some Linux distros have disabled the use of ICMP ping sockets, in which case
    it might be less intrusive to use a raw socket, assuming binary can be granted
    the CAP_NET_RAW capability.

--- src/platforms/macos/macospingsender.cpp
+++ src/platforms/macos/macospingsender.cpp
@@ -22,44 +22,6 @@ Logger logger({LOG_MACOS, LOG_NETWORKING}, "MacOSPingSender");
 
 int identifier() { return (getpid() & 0xFFFF); }
 
-// From ping.c implementation:
-u_short in_cksum(u_short* addr, int len) {
-  int nleft, sum;
-  u_short* w;
-  union {
-    u_short us;
-    u_char uc[2];
-  } last;
-  u_short answer;
-
-  nleft = len;
-  sum = 0;
-  w = addr;
-
-  /*
-   * Our algorithm is simple, using a 32 bit accumulator (sum), we add
-   * sequential 16 bit words to it, and at the end, fold back all the
-   * carry bits from the top 16 bits into the lower 16 bits.
-   */
-  while (nleft > 1) {
-    sum += *w++;
-    nleft -= 2;
-  }
-
-  /* mop up an odd byte, if necessary */
-  if (nleft == 1) {
-    last.uc[0] = *(u_char*)w;
-    last.uc[1] = 0;
-    sum += last.us;
-  }
-
-  /* add back carry outs from top 16 bits to low 16 bits */
-  sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
-  sum += (sum >> 16);                 /* add carry */
-  answer = ~sum;                      /* truncate to 16 bits */
-  return (answer);
-}
-
 };  // namespace
 
 MacOSPingSender::MacOSPingSender(const QString& source, QObject* parent)
@@ -119,7 +81,7 @@ void MacOSPingSender::sendPing(const QString& dest, quint16 sequence) {
   packet.icmp_type = ICMP_ECHO;
   packet.icmp_id = identifier();
   packet.icmp_seq = htons(sequence);
-  packet.icmp_cksum = in_cksum((u_short*)&packet, sizeof(packet));
+  packet.icmp_cksum = inetChecksum(&packet, sizeof(packet));
 
   if (sendto(m_socket, (char*)&packet, sizeof(packet), 0,
              (struct sockaddr*)&addr, sizeof(addr)) != sizeof(packet)) {
