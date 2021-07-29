8353eb94296f625d899067eb3c65554e7ee64d50
Moritz Birghan <mb@mbirghan.com>
2021-07-29 16:43:42 +0200

    update qt

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,4 +8,13 @@
 
 cd /Volumes/workspace/repository
 
+https://github.com/mbirghan/qt_ios_build
+
+pip3 install "glean_parser==3.5"
+pip3 install pyhumps
+pip3 install pyyaml
+python3 scripts/generate_glean.py
+
+export QT_IOS_BIN=`pwd`/qt_ios_build/ios/bin
+
 ./scripts/apple_compile.sh ios
