43bb926fe6ab9d61b10a87922099fd37095ad1a9
Moritz Birghan <mb@mbirghan.com>
2021-07-29 16:33:35 +0200

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,11 +8,4 @@
 
 cd /Volumes/workspace/repository
 
-curl -L https://download.qt.io/archive/qt/5.15/5.15.1/single/qt-everywhere-src-5.15.1.tar.xz --output qt-everywhere-src-5.15.1.tar.xz
-tar vxf qt-everywhere-src-5.15.1.tar.xz
-mv qt-everywhere-src-5.15.1 qt
-./qt/configure -xplatform macx-ios-clang -release
-bash scripts/qt5_compile.sh `pwd`/qt qt
-export QT_IOS_BIN=`pwd`/qt/qt/bin
-
 ./scripts/apple_compile.sh ios
