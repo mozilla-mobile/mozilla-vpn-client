8886a3fb575f33e4c368843da405af30007fa7b2
Owen Kirby <oskirby@gmail.com>
2021-08-17 15:04:04 -0700

    Fix pyyaml exception on Windows when parsing strings.yaml

--- scripts/importLanguages.py
+++ scripts/importLanguages.py
@@ -3,8 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This script must be executed at the root of the repository.
-
 import argparse
 import xml.etree.ElementTree as ET
 import os
