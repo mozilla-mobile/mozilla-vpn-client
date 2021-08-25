5d667bfb0e2c3e176416c6aa5d8dd2565b250e0c
Sarah Bird <birdsarah@mozilla.com>
2021-08-25 10:06:23 -0500

    Initialize feature list in testnetworkmanager

--- tests/unit/testnetworkmanager.cpp
+++ tests/unit/testnetworkmanager.cpp
@@ -3,11 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "testnetworkmanager.h"
+#include "../../src/featurelist.h"
+#include "../../src/settingsholder.h"
 #include "../../src/simplenetworkmanager.h"
 #include "helper.h"
 
 void TestNetworkManager::basic() {
   SimpleNetworkManager snm;
+  SettingsHolder settingsHolder;
+  FeatureList::instance()->initialize();
   QCOMPARE(&snm, NetworkManager::instance());
   QVERIFY(snm.userAgent().contains("MozillaVPN"));
   QCOMPARE(snm.networkAccessManager(), snm.networkAccessManager());
