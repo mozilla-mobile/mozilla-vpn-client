6add85177c675816f736a7b34961a94c35b6e415
Sarah Bird <birdsarah@mozilla.com>
2021-08-24 21:57:47 -0500

    Add new test references for reset function

--- tests/unit/mocmozillavpn.cpp
+++ tests/unit/mocmozillavpn.cpp
@@ -149,3 +149,5 @@ void MozillaVPN::addCurrentDeviceAndRefreshData() {}
 void MozillaVPN::appReviewRequested() {}
 
 bool MozillaVPN::validateUserDNS(const QString&) const { return false; }
+
+void MozillaVPN::reset(bool forceInitialState) {}
