15eabad8b759e7aaf575df4295ac2687685e4c48
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-09-02 15:38:28 +0200

    update adjust event tokens (#1694)

--- src/mozillavpn.cpp
+++ src/mozillavpn.cpp
@@ -1314,7 +1314,7 @@ void MozillaVPN::subscriptionCompleted() {
 
   logger.debug() << "Subscription completed";
 #ifdef MVPN_ADJUST
-  AdjustHandler::trackEvent("subscriptionCompleted");
+  AdjustHandler::trackEvent(Constants::ADJUST_SUBSCRIPTION_COMPLETED);
 #endif
   completeActivation();
 }
