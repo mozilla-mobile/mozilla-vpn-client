8f6e222fc9a1c8ab741fd635247ba1099863c8f6
Andrea Marchesini <baku@mozilla.com>
2021-09-03 19:59:04 +0200

    Custom staging env for 2.4.1 - DNS

--- src/constants.h
+++ src/constants.h
@@ -60,7 +60,8 @@ CONSTEXPR(uint32_t, SURVEY_TIMER_MSEC, 300000, 4000, 0)
 #endif
 
 PRODBETAEXPR(const char*, API_URL, "https://vpn.mozilla.org",
-             "https://stage-vpn.guardian.nonprod.cloudops.mozgcp.net")
+             "https://guardian-dev.herokuapp.com")
+
 PRODBETAEXPR(const char*, LOGO_URL, ":/ui/resources/logo-dock.png",
              ":/ui/resources/logo-dock-beta.png")
 PRODBETAEXPR(
