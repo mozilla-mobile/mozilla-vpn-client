5645c40aa7eb31fc6aea171752ec85ddfdd8fa35
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-09-15 18:32:16 +0200

    add imports

--- tests/unit/main.cpp
+++ tests/unit/main.cpp
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "../../src/leakdetector.h"
+#include "../../src/settingsholder.h"
 #include "constants.h"
 #include "helper.h"
 
