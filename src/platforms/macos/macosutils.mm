be17f9ff804c812a56a1a93674ce877e9e610cb9
Jeremy Swinarton <jswinarton@mozilla.com>
2021-09-20 11:29:53 -0400

    MacOS: Fix squashed/overlarge status bar icon (#1823)
    Fix a MacOS issue where the status bar icon appears horizontally squashed under the following conditions:
    - Using MacOS Big Sur
    - Using an external display
    - The display has a device pixel ratio greater than 1
    
    The icon also appeared much larger than other status bar icons, which is also fixed.
    
    The underlying reason for this is [a bug in Qt 5.15](https://bugreports.qt.io/browse/QTBUG-88600) that causes the system tray icon class to not properly respect the device pixel ratio of the display. The irritating thing is that this bug has actually been fixed, but was released as part of Qt 5.15.3 which apparently is a [commercial-only patch release](https://www.qt.io/blog/commercial-lts-qt-5.15.3-released). This means we need to resort to an ugly hack, because capitalism.
    
    The hack implementation (borrowed from the original bug report) uses an Objective-C technique called "swizzling" that makes it possible to replace the underlying implementation of an interface at runtime. The fix replaces the `setImage` method on `NSStatusBarButton` with a new version that correctly scales the image according to MacOS UI specifications.

--- src/platforms/macos/macosutils.mm
+++ src/platforms/macos/macosutils.mm
@@ -89,3 +89,105 @@ void MacOSUtils::hideDockIcon() {
 void MacOSUtils::showDockIcon() {
   [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 }
+
+/**
+ * Replace the setImage method on NSStatusBarButton with a method that scales
+ * images proportionally before setting.
+ *
+ * The reason for this is that there is a bug in Qt 5.15 that causes status bar
+ * icons to be displayed larger than UI recommendations, and out of proportion
+ * on displays with a device pixel ratio greater than 1 (MacOS Big Sur only).
+ * This bug will not be fixed in Qt open source versions, so we have to resort
+ * to a hack that exchanges the implementation of a method on NSStatusBarButton
+ * with one that correctly scales the icon.
+ *
+ * Original bug (and sample implementation):
+ * https://bugreports.qt.io/browse/QTBUG-88600
+ */
+void MacOSUtils::patchNSStatusBarSetImageForBigSur() {
+  Method original = class_getInstanceMethod([NSStatusBarButton class], @selector(setImage:));
+  Method patched = class_getInstanceMethod([NSStatusBarButton class], @selector(setImagePatched:));
+  method_exchangeImplementations(original, patched);
+}
+
+@interface NSImageScalingHelper : NSObject
+
+/**
+ * Create a proportionally scaled image according to the given target size.
+ *
+ * @param sourceImage The original image to be scaled.
+ * @param targetSize The required size of the image.
+ * @return A scaled image.
+ */
++ (NSImage*)imageByScaling:(NSImage*)sourceImage size:(NSSize)targetSize;
+@end
+
+@implementation NSImageScalingHelper
++ (NSImage*)imageByScaling:(NSImage*)sourceImage size:(NSSize)targetSize {
+  NSImage* newImage = nil;
+
+  if ([sourceImage isValid]) {
+    NSSize sourceSize = [sourceImage size];
+
+    if (sourceSize.width != 0.0 && sourceSize.height != 0.0) {
+      float scaleFactor = 0.0;
+      float scaledWidth = targetSize.width;
+      float scaledHeight = targetSize.height;
+
+      NSPoint thumbnailPoint = NSZeroPoint;
+
+      if (NSEqualSizes(sourceSize, targetSize) == NO) {
+        float widthFactor = targetSize.width / sourceSize.width;
+        float heightFactor = targetSize.height / sourceSize.height;
+
+        if (widthFactor < heightFactor) {
+          scaleFactor = widthFactor;
+        } else {
+          scaleFactor = heightFactor;
+        }
+        scaledWidth = sourceSize.width * scaleFactor;
+        scaledHeight = sourceSize.height * scaleFactor;
+
+        if (widthFactor < heightFactor) {
+          thumbnailPoint.y = (targetSize.height - scaledHeight) * 0.5;
+        } else {
+          thumbnailPoint.x = (targetSize.width - scaledWidth) * 0.5;
+        }
+      }
+
+      newImage = [[NSImage alloc] initWithSize:targetSize];
+
+      [newImage lockFocus];
+
+      NSRect thumbnailRect;
+      thumbnailRect.origin = thumbnailPoint;
+      thumbnailRect.size.width = scaledWidth;
+      thumbnailRect.size.height = scaledHeight;
+      [sourceImage drawInRect:thumbnailRect
+                     fromRect:NSZeroRect
+                    operation:NSCompositingOperationSourceOver
+                     fraction:1.0];
+
+      [newImage unlockFocus];
+
+      [newImage setTemplate:[sourceImage isTemplate]];
+    }
+  }
+  return [newImage autorelease];
+}
+@end
+
+@implementation NSStatusBarButton (Swizzle)
+- (void)setImagePatched:(NSImage*)image {
+  NSImage* img = image;
+
+  if (@available(macOS 11.0, *)) {
+    if (image != nil) {
+      int thickness = [[NSStatusBar systemStatusBar] thickness];
+      img = [NSImageScalingHelper imageByScaling:image size:NSMakeSize(thickness, thickness)];
+    }
+  }
+
+  [self setImagePatched:img];
+}
+@end
