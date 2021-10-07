3fffa02708860b0529e92e2a9958bcf8225d6eb8
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-10-07 16:57:37 +0200

    remove glean for now

--- scripts/apple_compile.sh
+++ scripts/apple_compile.sh
@@ -143,9 +143,6 @@ print Y "Importing translation files..."
 git submodule update --remote --depth 1 i18n || die "Failed to fetch newest translation files"
 python3 scripts/importLanguages.py $([[ "$OS" = "macos" ]] && echo "-m" || echo "") || die "Failed to import languages"
 
-print Y "Generating glean samples..."
-python3 scripts/generate_glean.py || die "Failed to generate glean samples"
-
 printn Y "Extract the project version... "
 SHORTVERSION=$(cat version.pri | grep VERSION | grep defined | cut -d= -f2 | tr -d \ )
 FULLVERSION=$(echo $SHORTVERSION | cut -d. -f1).$(date +"%Y%m%d%H%M")
