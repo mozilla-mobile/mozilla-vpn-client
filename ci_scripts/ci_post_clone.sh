3d75b97dc67f70ecf2df626f429b7ab51acfd863
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-10-07 16:23:31 +0200

    clone qt

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,7 +8,7 @@
 
 cd /Volumes/workspace/repository
 
-https://github.com/mbirghan/qt_ios_build
+git clone https://github.com/mbirghan/qt_ios_build
 
 pip3 install "glean_parser==3.5"
 pip3 install pyhumps
