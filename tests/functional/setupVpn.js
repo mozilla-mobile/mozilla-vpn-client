de73c3061a5abd8060bde163c93994374d41ff94
Sarah Bird <birdsarah@mozilla.com>
2021-10-15 14:07:45 -0500

    Switch to modern root hooks, no bail.

--- tests/functional/setupVpn.js
+++ tests/functional/setupVpn.js
@@ -23,63 +23,57 @@ const app = process.env.MVPN_BIN;
 let vpnProcess = null;
 let stdErr = '';
 
-before('Check VPN available', function() {
-  exec(`${app} --version`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Could not run "${app}".`);
-      console.error('Have you set MVPN_BIN in .env or environment?');
-      console.error(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-      process.exit(1);
+exports.mochaHooks = {
+  beforeAll() {
+    // Check VPN app exists. If not, bail.
+    exec(`${app} --version`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Could not run "${app}".`);
+        console.error('Have you set MVPN_BIN in .env or environment?');
+        console.error(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        process.exit(1);
+      }
+      console.log(`VPN Version is: ${stdout}`);
+    })
+  },
+  async beforeEach() {
+    // Start VPN app
+    vpnProcess = spawn(app, ['ui', '--testing']);
+    stdErr += 'VPN Process ID: ' + vpnProcess.pid;
+    vpnProcess.stderr.on('data', (data) => {
+      stdErr += data;
+    });
+    // Connect to VPN
+    await vpn.connect();
+    // Reset the VPN
+    await vpn.reset();
+    vpn.resetLastNotification();
+  },
+  async afterEach() {
+    // Collect errors on failure
+    if (this.currentTest.state === 'failed') {
+      // Print error logs
+      console.log('::group::Error Logs');
+      console.log(stdErr);
+      console.log('::endgroup');
+
+      // Screenshot of failure state
+      const data = await vpn.screenCapture();
+      const buffer = Buffer.from(data, 'base64');
+      const dir = process.env.ARTIFACT_DIR + '/screencapture';
+      const title = this.currentTest.title.toLowerCase();
+      const filename = title.replace(/[^a-z0-9]/g, '_');
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      fs.writeFileSync(`${dir}/${filename}.png`, buffer);
     }
-    console.log(`VPN Version is: ${stdout}`);
-  });
-})
-
-beforeEach('Start VPN app', () => {
-  vpnProcess = spawn(app, ['ui', '--testing']);
-  stdErr += 'VPN Process ID: ' + vpnProcess.pid;
-  vpnProcess.stderr.on('data', (data) => {
-    stdErr += data;
-  });
-})
-
-beforeEach('Connect to VPN', async () => {
-  await vpn.connect();
-});
-
-beforeEach('Reset the VPN', async () => {
-  await vpn.reset();
-  vpn.resetLastNotification();
-})
-
-// Notes:
-// - Cannot be an arrow function, as we need `this`.
-// - Must be before disconnect.
-afterEach('Collect errors on failure', async function() {
-  if (this.currentTest.state === 'failed') {
-    // Print error logs
-    console.log('::group::Error Logs');
-    console.log(stdErr);
-    console.log('::endgroup');
-
-    // Screenshot of failure state
-    const data = await vpn.screenCapture();
-    const buffer = Buffer.from(data, 'base64');
-    const dir = process.env.ARTIFACT_DIR + '/screencapture';
-    const title = this.currentTest.title.toLowerCase();
-    const filename = title.replace(/[^a-z0-9]/g, '_');
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    fs.writeFileSync(`${dir}/${filename}.png`, buffer);
+    // Reset error logs
+    stdErr = '';
+    // Close VPN app
+    await vpn.quit()
+    vpn.disconnect();
+    vpnProcess.kill();
   }
-  // Reset error logs
-  stdErr = '';
-})
-
-afterEach('Close VPN app', async () => {
-  await vpn.quit()
-  vpn.disconnect();
-  vpnProcess.kill();
-})
+}
