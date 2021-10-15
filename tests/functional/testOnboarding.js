a4e534f5f05c99c95d604b5a9434e2399264e6f1
Sarah Bird <birdsarah@mozilla.com>
2021-10-14 22:07:23 -0500

    Fixup testOnboarding.js

--- tests/functional/testOnboarding.js
+++ tests/functional/testOnboarding.js
@@ -9,23 +9,11 @@ const vpn = require('./helper.js');
 describe('Initial view and onboarding', function() {
   this.timeout(300000);
 
-  before(async () => {
-    await vpn.connect();
-  });
-
-  beforeEach(() => {});
-
-  afterEach(vpn.dumpFailure);
-
-  after(async () => {
-    vpn.disconnect();
-  });
-
-  it('reset the app', async () => await vpn.reset());
-
-  it('wait for the main view', async () => {
+  beforeEach(async () => {
     assert(await vpn.getLastUrl() === '');
+  })
 
+  it('Check for links on mainView', async () => {
     await vpn.waitForElement('getHelpLink');
     await vpn.waitForElementProperty('getHelpLink', 'visible', 'true');
     assert(await vpn.getElementProperty('getStarted', 'visible') === 'true');
@@ -40,7 +28,10 @@ describe('Initial view and onboarding', function() {
     await vpn.waitForElementProperty('getHelpBack', 'visible', 'true');
   });
 
-  it('Open some links', async () => {
+  it('Open help links', async () => {
+    await vpn.clickOnElement('getHelpLink');
+    await vpn.waitForElementProperty('getHelpLink', 'visible', 'false');
+
     await vpn.waitForElement('getHelpLinks');
     await vpn.waitForElementProperty('getHelpLinks', 'visible', 'true');
 
@@ -74,17 +65,6 @@ describe('Initial view and onboarding', function() {
       const url = await vpn.getLastUrl();
       return url.endsWith('/r/vpn/contact');
     });
-  });
-
-  it('Go back to the main view', async () => {
-    await vpn.clickOnElement('getHelpBack');
-
-    await vpn.waitForElement('getHelpLink');
-    await vpn.waitForElementProperty('getHelpLink', 'visible', 'true');
-
-    // This is needed just for humans. The UI is already in the other state
-    // before completing the animation.
-    await vpn.wait();
   });
 
   it('Complete the onboarding (aborting in each phase)', async () => {
@@ -134,6 +114,4 @@ describe('Initial view and onboarding', function() {
 
     assert(onboardingView, 4);
   });
-
-  it('quit the app', async () => await vpn.quit());
 });
