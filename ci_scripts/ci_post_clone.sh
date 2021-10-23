662fb7806750e2e385dc9b405d1ec15c6dd164cc
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-10-23 17:29:52 +0200

    Clean up

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-#  ci_post_clone.sh
-#  MozillaVPN
-#
-#  Created by mozilla on 29.07.21.
-#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# switch to repository directory for setup
 cd /Volumes/workspace/repository
 
+# make sure submodules are up to date
+# should already be done by XCode cloud cloning but just to make sure
 git submodule init
 git submodule update
 
+# generate qt_ios
 git clone https://github.com/mbirghan/qt_ios_build
 cd qt_ios_build
 cat x* > qt_static.tar.gz
@@ -19,23 +21,26 @@ cd ..
 
 export QT_IOS_BIN=`pwd`/qt_ios_build/ios/bin
 
+# add necessary directories to path
 export PATH=`pwd`/qt_ios_build/ios/bin:/Users/local/.gem/ruby/2.6.0/bin:/Users/local/Library/Python/3.8/bin:$PATH
+
+# install xcodeproj which is needed by xcode_patcher.rb
+# use --user-install for permissions
 gem install xcodeproj --user-install
 
+# install python packages
+# use --user for permissions
 pip3 install "glean_parser==3.5" --user
 pip3 install pyhumps --user
 pip3 install pyyaml --user
 python3 scripts/generate_glean.py
-
-brew install go
-# wget -nc https://golang.org/dl/go1.16.9.darwin-amd64.tar.gz
-# tar -xzf go1.16.9.darwin-amd64.tar.gz
-# export PATH=`pwd`/go/bin:$PATH
-# export GOROOT=`pwd`/go
-# export GOPATH=`pwd`
-
 python3 scripts/importLanguages.py -m
 
+# install go and set GOROOT using brew
+# wget is currently not supported so we need to use brew
+brew install go
+
+# create xcode.xconfig
 echo "DEVELOPMENT_TEAM = 43AQ936H96" >> xcode.xconfig
 echo "GROUP_ID_MACOS = group.org.mozilla.macos.Guardian" >> xcode.xconfig
 echo "APP_ID_MACOS = org.mozilla.macos.FirefoxVPN" >> xcode.xconfig
@@ -47,6 +52,7 @@ echo "NETEXT_ID_IOS = org.mozilla.ios.FirefoxVPN.network-extension" >> xcode.xco
 
 ./scripts/apple_compile.sh ios
 
+# use New Build System instead of legacy build system
 rm ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
 echo "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
@@ -57,8 +63,12 @@ echo "	<false/>" >> ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/Work
 echo "</dict>" >> ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
 echo "</plist>" >> ./MozillaVPN.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
 
+# use correct scheme
 rm ./MozillaVPN.xcodeproj/xcshareddata/xcschemes/MozillaVPN.xcscheme
-mv MozillaVPN.xcscheme ./MozillaVPN.xcodeproj/xcshareddata/xcschemes/
+mv ./ci_scripts/MozillaVPN.xcscheme ./MozillaVPN.xcodeproj/xcshareddata/xcschemes/
 
+# build Qt resources
+# XCode Cloud has some problem with dependencies and timing therefore we have to
+# build Qt before we call xcodebuild
 make -f MozillaVPN.xcodeproj/qt_makeqmake.mak
 make -f MozillaVPN.xcodeproj/qt_preprocess.mak
