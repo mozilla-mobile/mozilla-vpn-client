27db57d937c01b2f2dd8fb36668826185c343d80
Sarah Bird <birdsarah@mozilla.com>
2021-11-09 16:12:10 -0600

    Clean-up

--- tests/qml/tst_main.cpp
+++ tests/qml/tst_main.cpp
@@ -5,6 +5,8 @@
 #include <QQmlEngine>
 #include <QQmlContext>
 
+// For info on the slots we can use
+// https://doc.qt.io/qt-5/qtquicktest-index.html#executing-c-before-qml-tests
 class Setup : public QObject {
   Q_OBJECT
 
@@ -18,6 +20,9 @@ class Setup : public QObject {
   }
 };
 
-QUICK_TEST_MAIN_WITH_SETUP(mytest, Setup)
+// The name qml_tests is arbitrary
+QUICK_TEST_MAIN_WITH_SETUP(qml_tests, Setup)
 
+// I don't understand why we need this line, but the docs say we do:
+// https://doc.qt.io/qt-5/qtquicktest-index.html#executing-c-before-qml-tests
 #include "tst_main.moc"
