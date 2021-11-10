f82f664670ca30b2998195f16d9f523aa96bd760
Marcus Kestler <mkestler@mozilla.com>
2021-11-10 12:45:01 -0500

    Debug is now working, so reenabling this
    Add debug back in to the configure scripts.

--- scripts/Qt5_static_compile.bat
+++ scripts/Qt5_static_compile.bat
@@ -52,7 +52,7 @@ IF NOT EXIST configure.bat (
 
 ECHO Configuring build...
 
-CALL configure -static -opensource -release -no-dbus -no-feature-qdbus -confirm-license -strip -silent -no-compile-examples -nomake tests -make libs -no-sql-psql -qt-sqlite -skip qt3d -skip webengine -skip qtmultimedia -skip qtserialport -skip qtsensors -skip qtgamepad -skip qtwebchannel -skip qtandroidextras -feature-imageformat_png -qt-libpng -qt-zlib -recheck-all -openssl-linked -I %BUILDDIR%\include -L %BUILDDIR%\lib -prefix %BUILDDIR% --verbose
+CALL configure -static -opensource -debug-and-release -no-dbus -no-feature-qdbus -confirm-license -strip -silent -no-compile-examples -nomake tests -make libs -no-sql-psql -qt-sqlite -skip qt3d -skip webengine -skip qtmultimedia -skip qtserialport -skip qtsensors -skip qtgamepad -skip qtwebchannel -skip qtandroidextras -feature-imageformat_png -qt-libpng -qt-zlib -recheck-all -openssl-linked -I %BUILDDIR%\include -L %BUILDDIR%\lib -prefix %BUILDDIR% --verbose
 
 rem This check is disabled, since configure returns non-zero due to QDoc build warnings.  It will fail later...
 rem IF %ERRORLEVEL% NEQ 0 (
