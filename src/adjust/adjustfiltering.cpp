51e7bbfd97dae11af49cd272894db21827ca92b8
Andrea Marchesini <amarchesini@mozilla.com>
2021-11-11 16:18:33 +0100

    No unknown params for adjust SDK (#2255)

--- src/adjust/adjustfiltering.cpp
+++ src/adjust/adjustfiltering.cpp
@@ -14,16 +14,22 @@ const QVector<QString> allowList{
     "att_status",
     "attribution_deeplink",
     "bundle_id",
+    "created_at",
     "device_type",
     "environment",
     "event_token",
+    "google_play_instant",
     "gps_adid",
     "idfv",
     "needs_response_details",
     "os_name",
     "os_version",
     "package_name",
+    "referrer",
+    "referrer_api",
     "reference_tag",
+    "sent_at",
+    "source",
     "tracking_enabled",
     "zone_offset",
 };
@@ -34,6 +40,7 @@ struct DenyParam {
 };
 
 const QVector<DenyParam> denyList{
+    {"api_level", "29"},
     {"app_name", "default"},
     {"app_version", "2"},
     {"app_version_short", "2"},
@@ -47,6 +54,7 @@ const QVector<DenyParam> denyList{
     {"engagement_type", "0"},
     {"event_buffering_enabled", "0"},
     {"event_cost_id", "xxxxx"},
+    {"gps_adid_attempt", "1"},
     {"gps_adid_lower_md5", "0"},
     {"gps_adid_lower_sha1", "0"},
     {"gps_adid_upper_md5", "0"},
@@ -57,6 +65,7 @@ const QVector<DenyParam> denyList{
     {"manufacturer", "default"},
     {"mcc", "0"},
     {"mnc", "0"},
+    {"network_type", "0"},
     {"nonce", "0"},
     {"os_build", "xxxxx"},
     {"platform", "default"},
@@ -65,10 +74,13 @@ const QVector<DenyParam> denyList{
     {"screen_density", "0"},
     {"screen_format", "0"},
     {"screen_size", "0"},
+    {"session_count", "1"},
+    {"session_length", "0"},
     {"store_name", "xxxxx"},
     {"terms_signed", "0"},
     {"time_spent", "0"},
     {"tracker_token", "xxxxx"},
+    {"ui_mode", "0"},
 };
 
 struct MirrorParam {
@@ -79,6 +91,8 @@ struct MirrorParam {
 
 const QVector<MirrorParam> mirrorList{
     {"device_name", "device_type", Constants::PLATFORM_NAME},
+    {"installed_at", "created_at", "0"},
+    {"updated_at", "created_at", "0"},
 };
 
 // static
@@ -146,7 +160,6 @@ QUrlQuery AdjustFiltering::filterParameters(QUrlQuery& parameters,
       continue;
     }
 
-    newParameters.addQueryItem(parameter.first, parameter.second);
     unknownParameters.append(parameter.first);
   }
 
