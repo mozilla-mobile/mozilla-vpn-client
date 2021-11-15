aec29b371013802987ee256c9251d0acfc96a707
Marcus Kestler <mkestler@mozilla.com>
2021-11-15 10:16:31 -0500

    Review cleanup

--- glean/glean.h
+++ glean/glean.h
@@ -1,12 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef GLEAN_H
 #define GLEAN_H
 
 #include <QQmlEngine>
 
-class Glean
+class Glean final
 {
 private:
-    Glean(){};
+    Glean() = default;
 public:
 static void Initialize(QQmlEngine * engine);
 };
--- nebula/nebula.h
+++ nebula/nebula.h
@@ -1,12 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef NEBULA_H
 #define NEBULA_H
 
 #include <QQmlEngine>
 
-class Nebula
+class Nebula final
 {
 private:
-    Nebula(){};
+    Nebula() = default;
 public:
     static void Initialize(QQmlEngine * engine);
 };
