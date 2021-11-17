71b08ae9ec12f1a9d808bee0df5e3575d3ac01bf
Sarah Bird <birdsarah@mozilla.com>
2021-11-17 13:37:06 -0600

    Begin code clean-up

--- tests/qml/main.cpp
+++ tests/qml/main.cpp
@@ -1,11 +1,21 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "helper.h"
 #include <QtQuickTest>
 #include "../../src/settingsholder.h"
 
-SettingsHolder settingsHolder;
-
-// The name qml_tests is arbitrary
-QUICK_TEST_MAIN_WITH_SETUP(qml_tests, TestHelper)
+int main(int argc, char* argv[]) {
+  // In order to be able to call our TestHelper constructor how we want,
+  // we do not follow the exact instructions here
+  // https://doc.qt.io/qt-5/qtquicktest-index.html#executing-c-before-qml-tests
+  // but instead call the components of the very simple macro ourselves.
+  // Macro source code:
+  // https://github.com/qt/qtdeclarative/blob/5.15.2/src/qmltest/quicktest.h#L69
+  QTEST_SET_MAIN_SOURCE_PATH
+  SettingsHolder settingsHolder;
+  TestHelper testHelper;
+  quick_test_main_with_setup(argc, argv, "qml_tests", QUICK_TEST_SOURCE_DIR,
+                             &testHelper);
+}
