dcb4fddd89d3f707a982a76bd8f850e7bffe33cd
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2021-12-01 09:46:27 +0100

    Add new fields to lists

--- src/adjust/adjustfiltering.h
+++ src/adjust/adjustfiltering.h
@@ -21,9 +21,11 @@ class AdjustFiltering final {
       "att_status",
       "attribution_deeplink",
       "bundle_id",
+      "created_at",
       "device_type",
       "environment",
       "event_token",
+      "google_play_instant",
       "gps_adid",
       "idfv",
       "needs_response_details",
@@ -31,11 +33,16 @@ class AdjustFiltering final {
       "os_version",
       "package_name",
       "reference_tag",
+      "referrer",
+      "referrer_api",
+      "sent_at",
+      "source",
       "tracking_enabled",
       "zone_offset",
   };
 
   QMap<QString, QString> denyList{
+      {"api_level", "29"},
       {"app_name", "default"},
       {"app_version", "2"},
       {"app_version_short", "2"},
@@ -49,6 +56,7 @@ class AdjustFiltering final {
       {"engagement_type", "0"},
       {"event_buffering_enabled", "0"},
       {"event_cost_id", "xxxxx"},
+      {"gps_adid_attempt", "1"},
       {"gps_adid_lower_md5", "0"},
       {"gps_adid_lower_sha1", "0"},
       {"gps_adid_upper_md5", "0"},
@@ -59,6 +67,7 @@ class AdjustFiltering final {
       {"manufacturer", "default"},
       {"mcc", "0"},
       {"mnc", "0"},
+      {"network_type", "0"},
       {"nonce", "0"},
       {"os_build", "xxxxx"},
       {"platform", "default"},
@@ -67,10 +76,13 @@ class AdjustFiltering final {
       {"screen_density", "0"},
       {"screen_format", "0"},
       {"screen_size", "0"},
+      {"session_count", "1"},
+      {"session_length", "0"},
       {"store_name", "xxxxx"},
       {"terms_signed", "0"},
       {"time_spent", "0"},
       {"tracker_token", "xxxxx"},
+      {"ui_mode", "0"},
   };
 
   struct MirrorParam {
@@ -79,7 +91,9 @@ class AdjustFiltering final {
   };
 
   QMap<QString, MirrorParam> mirrorList{
-      {"device_name", {"device_type", Constants::PLATFORM_NAME}},
+      {"device_name", "device_type", Constants::PLATFORM_NAME},
+      {"installed_at", "created_at", "0"},
+      {"updated_at", "created_at", "0"},
   };
 
   static AdjustFiltering* instance();
