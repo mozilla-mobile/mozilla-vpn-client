c014486d2a9eb05c3069c6bafa2bd22fe768326e
Florian Zia <fzia@mozilla.com>
2021-12-07 13:58:56 +0100

    fix: MAC feature link

--- src/features/featuremultiaccountcontainers.h
+++ src/features/featuremultiaccountcontainers.h
@@ -26,10 +26,10 @@ class FeatureMultiAccountContainers final : public Feature {
             "qrc:/ui/resources/features/"
             "multi-account-containers-preview.png",  // ImagePath
             "qrc:/ui/resources/features/multi-account-containers-icon.svg",  // IconPath
-            "https://addons.mozilla.org/firefox/"
-            "addon?utm_source=mozilla.vpn.client&utm_medium=in-app-link&utm_"
-            "content=whats-new-panel&utm_campaign=vpn-better-together"
-            "multi-account-containers",  // link URL
+            "https://addons.mozilla.org/firefox/addon/multi-account-containers/"
+            "?utm_source=mozilla.vpn.client&utm_medium=in-app-link&utm_"
+            "content=whats-new-panel&utm_campaign=vpn-better-together",  // link
+                                                                         // URL
             "2.7",
             false  // Can be enabled in devmode
         ) {}
