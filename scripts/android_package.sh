b45e246b1bb9a17ed633f4bcf82742fad355f88a
Anders Kaseorg <andersk@mit.edu>
2021-12-22 10:36:00 -0800

    Enable Qt Quick Compiler for all platforms
    Fixes #2487.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- scripts/android_package.sh
+++ scripts/android_package.sh
@@ -164,7 +164,6 @@ if [[ "$RELEASE" ]]; then
   $QTPATH/bin/qmake -spec android-clang \
     VERSION=$SHORTVERSION \
     BUILD_ID=$VERSIONCODE \
-    CONFIG+=qtquickcompiler \
     CONFIG-=debug \
     CONFIG-=debug_and_release \
     CONFIG+=release \
