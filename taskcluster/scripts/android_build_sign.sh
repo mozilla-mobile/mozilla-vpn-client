4d14f1a3763e87eb3245f98e18f7711ddb93a217
Sebastian Streich <sstreich@mozilla.com>
2022-01-05 16:40:40 +0100

    Playing with secrets!

---
+++ taskcluster/scripts/android_build_sign.sh
@@ -0,0 +1,25 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# This script is used in the Android Sign (universal) build task
+git submodule init
+git submodule update
+# glean
+./scripts/generate_glean.py
+# translations
+./scripts/importLanguages.py
+
+
+# Get Secrets for building
+
+./taskcluster/scripts/get-secret.py -s project/mozillavpn/tokens -k adjust -f adjust_token
+
+cat adjust_token
+
+./scripts/android_package.sh -R $QTPATH --adjusttoken $(cat adjust_token.pri)
+
+# Artifacts should be placed here!
+mkdir -p /builds/worker/artifacts/
+cp .tmp/src/android-build/mozillavpn.apk  /builds/worker/artifacts/
