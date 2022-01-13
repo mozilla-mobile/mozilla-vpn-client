85655305463131b2d12fa4dc4f1bf87ff8736cf6
Sebastian Streich <sstreich@mozilla.com>
2022-01-13 18:49:47 +0100

    remove debugpy stuff

--- taskcluster/mozillavpn_taskgraph/transforms/build.py
+++ taskcluster/mozillavpn_taskgraph/transforms/build.py
@@ -14,16 +14,6 @@ from taskgraph.transforms.base import TransformSequence
 transforms = TransformSequence()
 
 
-import debugpy
-
-# 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-#debugpy.listen(5678)
-print("Waiting for debugger attach")
-#debugpy.wait_for_client()
-#debugpy.breakpoint()
-print('break on this line')
-
-
 # Add build-type info to the Task. Defaults to taskname
 @transforms.add
 def checkRequiredLevel(config, tasks):
