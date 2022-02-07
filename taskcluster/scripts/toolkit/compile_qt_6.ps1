0c9bef99fd5768075a9d5ebe754d2a4a7315f134
Sebastian Streich <sstreich@mozilla.com>
2022-02-07 14:37:59 +0100

    debugging stuff

--- taskcluster/scripts/toolkit/compile_qt_6.ps1
+++ taskcluster/scripts/toolkit/compile_qt_6.ps1
@@ -4,10 +4,19 @@
 ./$PSScriptRoot/enter_dev_shell.ps1
 
 $BUILD_TOOLS_DIR = "C:\MozillaVPNBuild"
-$QT_SRC_URL = "https://download.qt.io/archive/qt/6.2/6.2.3/single/qt-everywhere-src-6.2.3.zip"
 $QT_SRC_SUBDIR = "qt-everywhere-src-6.2.3"
 
 $env:Path += ";$env:PERL_PATH"
+
+
+# Debugging stuff for basti 
+write-output("-- ENV --")
+gci env:* | sort-object name
+write-output("-- ENV --")
+write-output("-- DIR --")
+Get-ChildItem .
+Get-ChildItem fetches
+write-output("-- DIR --")
 
 # Enter QT source directory
 Set-Location $env:QT_PATH
