d0d5ddbf72cccf848d83a54d2ce0527e285d79f0
Moritz Birghan <mb@mbirghan.com>
2022-02-28 14:30:13 +0100

    enable macos tasckluster

--- scripts/macosx_build.sh
+++ scripts/macosx_build.sh
@@ -3,12 +3,43 @@
 # Dependencies
 python3 scripts/tooltool.py --url http://taskcluster/tooltool.mozilla-releng.net fetch -m macos/qt-dependencies.tt.manifest
 export PATH="`pwd`/qt/bin:$PATH"
-# TODO: internal pypi mirror, if it doesn't get used by default?
-pip3 install glean_parser==3.5
+# install xcodeproj which is needed by xcode_patcher.rb
+# use --user-install for permissions
+gem install xcodeproj --user-install
 
-cd macos/gobridge
-go list -m golang.zx2c4.com/wireguard | sed -n 's/.*v\([0-9.]*\).*/#define WIREGUARD_GO_VERSION "\1"/p') > macos/gobridge/wireguard-go-version.h
-cd ../..
+# make sure submodules are up to date
+# should already be done by XCode cloud cloning but just to make sure
+git submodule init
+git submodule update
+
+# generate qt_static_macos
+auth_header="$(git config --local --get http.https://github.com/.extraheader)"
+git clone https://github.com/mozilla-mobile/qt_static_macos
+cd qt_static_macos
+cat x* > qt_static.tar.gz
+tar xf qt_static.tar.gz
+cd ..
+export QT_MACOS_BIN=`pwd`/qt_static_macos/qt/bin
+export PATH=`pwd`/qt_static_macos/qt/bin:$PATH
+
+export PATH="~/Library/Python/3.6/bin:~/.gem/ruby/2.6.0/bin:$PATH"
+
+# install python packages
+# use --user for permissions
+pip3 install "glean_parser==3.5" --user
+pip3 install pyhumps --user
+pip3 install pyyaml --user
+
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+export PYTHONIOENCODING="UTF-8"  
+
+python3 scripts/generate_glean.py
+python3 scripts/importLanguages.py -m
+
+curl -O https://dl.google.com/go/go1.17.6.darwin-amd64.tar.gz
+tar -xzf go1.17.6.darwin-amd64.tar.gz
+export PATH="`pwd`/go/bin:$PATH"
 
 # Xcode config - maybe we should just maintain a full Xcode config somewhere instead of replacing things here?
 SHORTVERSION=$(cat version.pri | grep VERSION | grep defined | cut -d= -f2 | tr -d \ )
@@ -22,40 +53,23 @@ echo "LOGIN_ID_MACOS = org.mozilla.macos.FirefoxVPN.login" >> xcode.xconfig
 echo "GROUP_ID_IOS = group.org.mozilla.ios.Guardian" >> xcode.xconfig
 echo "APP_ID_IOS = org.mozilla.ios.FirefoxVPN" >> xcode.xconfig
 echo "NETEXT_ID_IOS = org.mozilla.ios.FirefoxVPN.network-extension" >> xcode.xconfig
-qmake \
-  VERSION=$SHORTVERSION \
-  BUILD_ID=$FULLVERSION \
-  -spec macx-xcode \
-  CONFIG-=debug \
-  CONFIG-=debug_and_release \
-  CONFIG+=release \
-  CONFIG+=production \
-  QTPLUGIN+=qsvg \
-  CONFIG-=static \
-  CONFIG+=balrog \
-  MVPN_MACOS=1 \
-  src/src.pro
-ruby scripts/macos/utils/xcode_patcher.rb \
-  "MozillaVPN.xcodeproj" \
-  "$SHORTVERSION" \
-  "$FULLVERSION" \
-  macos \
-  Qt5
+./scripts/apple_compile.sh macos
 
-# Build
-python3 scripts/utils/generate_glean.py
-python3 scripts/utils/import_languages.py -m
+make -f MozillaVPN.xcodeproj/qt_makeqmake.mak
+make -f MozillaVPN.xcodeproj/qt_preprocess.mak
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -project MozillaVPN.xcodeproj
 
 # Package
-python3 ./scripts/macos/import_pkg_resources.py
-# Full path of the app in a temp folder
-mkdir -p BUILD
-cp -r Release/Mozilla\ VPN.app BUILD
-# Copy the script
-cp -r ./macos/pkg/scripts BUILD
-cp -r ./macos/pkg/Distribution BUILD
-cp -r ./macos/pkg/Resources BUILD
+python3 ./scripts/import_macos_pkg_resources.py
 
-cd BUILD
-zip -r ../build/src/artifacts/unsigned.zip .
+export BUILD="../../build/src/artifacts/"
+# Full path of the app in a temp folder
+mkdir -p $BUILD
+cp -r Release/Mozilla\ VPN.app $BUILD
+# Copy the script
+cp -r ./macos/pkg/scripts $BUILD
+cp -r ./macos/pkg/Distribution $BUILD
+cp -r ./macos/pkg/Resources $BUILD
+
+cd $BUILD
+zip -r unsigned.zip .
