1aa18c93ea98964961e5799ef27bbd760fc4efe6
Florian Zia <fzia@mozilla.com>
2022-03-13 18:44:24 +0100

    chore: Add What’s new panel for speed test

--- src/features/featureconnectioninfo.h
+++ src/features/featureconnectioninfo.h
@@ -12,19 +12,23 @@ constexpr const char* FEATURE_CONNECTION_INFO = "connectionInfo";
 class FeatureConnectionInfo final : public Feature {
  public:
   FeatureConnectionInfo()
-      : Feature(FEATURE_CONNECTION_INFO, "Connection info",
-                false,               // Is Major Feature
-                L18nStrings::Empty,  // Display name
-                L18nStrings::Empty,  // Description
-                L18nStrings::Empty,  // LongDescr
-                "",                  // ImagePath
-                "",                  // IconPath
-                "",                  // link URL
-                "2.8",               // released
-                true                 // Can be enabled in devmode
+      : Feature(
+            FEATURE_CONNECTION_INFO, "Connection info",
+            true,  // Is Major Feature
+            L18nStrings::WhatsNewReleaseNotesConnectionInfoDisplayName,  // Display
+                                                                         // name
+            L18nStrings::
+                WhatsNewReleaseNotesConnectionInfoDescriptionShort,  // Description
+            L18nStrings::
+                WhatsNewReleaseNotesConnectionInfoDescription,  // LongDescr
+            "qrc:/ui/resources/features/connection-info-preview.png",  // ImagePath
+            "qrc:/ui/resources/features/connection-info-icon.svg",  // IconPath
+            "",                                                     // link URL
+            "2.8",                                                  // released
+            true  // Can be enabled in devmode
         ) {}
 
-  bool checkSupportCallback() const override { return false; }
+  bool checkSupportCallback() const override { return true; }
 
   static const FeatureConnectionInfo* instance() {
     return static_cast<const FeatureConnectionInfo*>(
