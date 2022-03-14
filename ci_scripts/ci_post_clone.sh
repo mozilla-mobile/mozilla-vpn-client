0b7a4609b23bf0c9fa7a665405bf6b573736f19a
Moritz Birghan <mb@mbirghan.com>
2022-03-14 14:44:46 +0100

    install clang_64

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -39,7 +39,7 @@ else
   #echo `ls -la /opt/6.2.3`
   #cd ..
   pip3 install aqtinstall
-  aqt install-qt -O /Volumes/workspace/repository/qt_ios mac ios 6.2.3 -m qtcharts qtwebsockets qt5compat
+  aqt install-qt -O /Volumes/workspace/repository/qt_ios mac ios clang_64 6.2.3 -m qtcharts qtwebsockets qt5compat
   export QT_IOS_BIN=`pwd`/qt_ios/6.2.3/ios/bin
   export PATH=`pwd`/qt_ios/6.2.3/ios/bin:`pwd`/qt_ios/6.2.3/macos/bin:`pwd`/qt_ios/6.2.3/clang_64/bin:$PATH
 fi
