a965a372fd5602c028a34847e940e9ce5464bb32
Heitor Neiva <hneiva@mozilla.com>
2022-03-15 15:59:19 -0700

    RELENG-851 - Pull taskgraph from pypi

--- taskcluster/mozillavpn_taskgraph/parameters.py
+++ taskcluster/mozillavpn_taskgraph/parameters.py
@@ -2,19 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 import os
-import re
 
-from six import text_type
 from taskgraph.parameters import extend_parameters_schema
 from voluptuous import All, Any, Range, Required
 
-extend_parameters_schema({
-    Required("pull_request_number"): Any(All(int, Range(min=1)), None),
-    Required("version"): text_type,
-})
+extend_parameters_schema(
+    {
+        Required("pull_request_number"): Any(All(int, Range(min=1)), None),
+        Required("version"): str,
+    }
+)
 
 
 def get_decision_parameters(graph_config, parameters):
