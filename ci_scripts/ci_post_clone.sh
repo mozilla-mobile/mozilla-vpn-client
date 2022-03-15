f7a0194aeff2a5789e85abded18d7954194fd110
Moritz Birghan <32091959+mbirghan@users.noreply.github.com>
2022-03-15 02:18:20 +0100

    Remove qt5 from xcode cloud (#2999)
    * Remove qt5 from xcode cloud
    
    * use /opt
    
    * add outputs
    
    * try usign aqt
    
    * add python to path
    
    * look at qt version
    
    * add clang_64 o path
    
    * install clang_64
    
    * instatll macos
    
    * fix path
    
    * remove unnecccessary lines

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,31 +12,28 @@ cd /Volumes/workspace/repository
 git submodule init
 git submodule update
 
+# add necessary directories to path
+export PATH=/Users/local/.gem/ruby/2.6.0/bin:/Users/local/Library/Python/3.8/bin:$PATH
+
 if [ $CI_PRODUCT_PLATFORM == 'macOS' ]
 then
   # generate qt_static_macos
   auth_header="$(git config --local --get http.https://github.com/.extraheader)"
   git clone https://github.com/mozilla-mobile/qt_static_macos
   cd qt_static_macos
-  cat x* > qt_static.tar.gz
+  cat qt6* > qt_static.tar.gz
   tar xf qt_static.tar.gz
   cd ..
-  export QT_MACOS_BIN=`pwd`/qt_static_macos/qt/bin
-  export PATH=`pwd`/qt_static_macos/qt/bin:$PATH
+  export QT_MACOS_BIN=`pwd`/qt_static_macos/qt6/bin
+  export PATH=`pwd`/qt_static_macos/qt6/bin:$PATH
 else
-  # generate qt_ios
-  git clone https://github.com/mozilla-mobile/qt_ios
-  cd qt_ios
-  cat qt5* > qt_static.tar.gz
-  tar xf qt_static.tar.gz
-  cd ..
-  export QT_IOS_BIN=`pwd`/qt_ios/ios/bin
-  export PATH=`pwd`/qt_ios/ios/bin:$PATH
+  pip3 install aqtinstall
+  aqt install-qt -O /Volumes/workspace/repository/qt_ios mac desktop 6.2.3 -m qtcharts qtwebsockets qt5compat
+  aqt install-qt -O /Volumes/workspace/repository/qt_ios mac ios 6.2.3 -m qtcharts qtwebsockets qt5compat
+  mv /Volumes/workspace/repository/qt_ios/6.2.3/macos /Volumes/workspace/repository/qt_ios/6.2.3/clang_64
+  export QT_IOS_BIN=`pwd`/qt_ios/6.2.3/ios/bin
+  export PATH=`pwd`/qt_ios/6.2.3/ios/bin:`pwd`/qt_ios/6.2.3/macos/bin:`pwd`/qt_ios/6.2.3/clang_64/bin:$PATH
 fi
-
-
-# add necessary directories to path
-export PATH=/Users/local/.gem/ruby/2.6.0/bin:/Users/local/Library/Python/3.8/bin:$PATH
 
 # install xcodeproj which is needed by xcode_patcher.rb
 # use --user-install for permissions
@@ -65,6 +62,9 @@ GROUP_ID_IOS = group.org.mozilla.ios.Guardian
 APP_ID_IOS = org.mozilla.ios.FirefoxVPN
 NETEXT_ID_IOS = org.mozilla.ios.FirefoxVPN.network-extension
 EOF
+which qmake
+qmake -v
+$QT_IOS_BIN/qmake -v
 
 if [ $CI_PRODUCT_PLATFORM == 'macOS' ]
 then
