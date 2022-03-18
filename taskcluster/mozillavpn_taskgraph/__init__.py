79038ac2ef71465dce449bf09837104f7161cab3
Heitor Neiva <hneiva@mozilla.com>
2022-03-18 10:05:19 -0700

    RELENG-851 - Pull taskgraph from pypi (#3059)

--- taskcluster/mozillavpn_taskgraph/__init__.py
+++ taskcluster/mozillavpn_taskgraph/__init__.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 from importlib import import_module
 
 
@@ -12,12 +10,14 @@ def register(graph_config):
     Import all modules that are siblings of this one, triggering decorators in
     the process.
     """
-    _import_modules([
-        "job",
-        "parameters",
-        "target_tasks",
-        "worker_types",
-    ])
+    _import_modules(
+        [
+            "job",
+            "parameters",
+            "target_tasks",
+            "worker_types",
+        ]
+    )
 
 
 def _import_modules(modules):
