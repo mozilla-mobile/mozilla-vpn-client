fa02b337b762407bb4a2cef12602a6fe1d81f353
Sebastian Streich <streich.mobile@gmail.com>
2022-04-10 23:58:00 +0200

    Global config && dynamic wg_tools path

---
+++ android/buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies{
+
+}
---
+++ android/buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,7 @@
+object Config {
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 24
+    const val targetSdkVersion = 30
+    const val ndkVersion ="23.1.7779620"
+}
