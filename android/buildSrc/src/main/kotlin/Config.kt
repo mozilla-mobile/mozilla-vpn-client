82ce7a4904168d57b66807bba61eb7cdb8156efd
Sebastian Streich <streich.mobile@gmail.com>
2022-04-19 12:59:18 +0200

    Licence for build files

--- android/buildSrc/src/main/kotlin/Config.kt
+++ android/buildSrc/src/main/kotlin/Config.kt
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 object Config {
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.3"
--- android/buildSrc/src/main/kotlin/Dependencies.kt
+++ android/buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 object Dependencies {
     const val org_jetbrains_kotlin_kotlin_serialization = "org.jetbrains.kotlin:kotlin-serialization:1.4.30-M1"
     const val org_jetbrains_kotlin_kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
