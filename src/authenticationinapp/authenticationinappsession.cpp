6f5bb622b3d44b71da3148ec907c4c23751b8848
Andrea Marchesini <amarchesini@mozilla.com>
2022-04-28 07:05:47 +0200

    Pass the original login email in case of redirects in the in-app auth flow - VPN-2124 (#3387)

--- src/authenticationinapp/authenticationinappsession.cpp
+++ src/authenticationinapp/authenticationinappsession.cpp
@@ -238,8 +238,9 @@ void AuthenticationInAppSession::signIn(const QString& unblockCode) {
 
 void AuthenticationInAppSession::signInInternal(const QString& unblockCode) {
   NetworkRequest* request = NetworkRequest::createForFxaLogin(
-      m_task, m_emailAddressCaseFix, generateAuthPw(), unblockCode,
-      m_fxaParams.m_clientId, m_fxaParams.m_deviceId, m_fxaParams.m_flowId,
+      m_task, m_emailAddressCaseFix, generateAuthPw(),
+      m_originalLoginEmailAddress, unblockCode, m_fxaParams.m_clientId,
+      m_fxaParams.m_deviceId, m_fxaParams.m_flowId,
       m_fxaParams.m_flowBeginTime);
 
   connect(request, &NetworkRequest::requestFailed, this,
@@ -266,6 +267,7 @@ void AuthenticationInAppSession::signInInternal(const QString& unblockCode) {
                   logger.error()
                       << "Failed to sign in for email case issues. New email:"
                       << logger.sensitive(email);
+                  m_originalLoginEmailAddress = m_emailAddressCaseFix;
                   m_emailAddressCaseFix = email;
                   signInInternal(unblockCode);
                   return;
