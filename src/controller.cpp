061b7cfacf7081187d78907c11702834d68a5202
Sarah Bird <birdsarah@mozilla.com>
2022-05-06 15:16:31 -0500

    Add glean recording step

--- src/controller.cpp
+++ src/controller.cpp
@@ -609,6 +609,8 @@ void Controller::setState(State state) {
     return;
   }
   logger.debug() << "Setting state:" << state;
+  emit MozillaVPN::instance()->recordGleanEventWithExtraKeys(
+      GleanSample::appStep, {{"state", QVariant::fromValue(state).toString()}});
   m_state = state;
   emit stateChanged();
 }
