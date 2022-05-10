e3f5f3361305b5d6e6a53e2b2eb09228b587a0f8
Andrew Halberstadt <ahal@mozilla.com>
2022-05-10 10:26:30 -0400

    Convert build-qt6 and build-openssl tasks to toolchains

--- taskcluster/scripts/toolchain/compile_qt_6_mac.sh
+++ taskcluster/scripts/toolchain/compile_qt_6_mac.sh
@@ -4,7 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
+set -x
+pushd vcs
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 export PATH=$PWD/homebrew/bin:$PATH
 
@@ -16,12 +17,13 @@ brew install ninja
 
 echo "Download QT $QT_VERSION"
 curl -o qt.zip -L https://download.qt.io/archive/qt/$QT_MAJOR/$QT_VERSION/single/qt-everywhere-src-$QT_VERSION.zip
-unzip -q -aa qt.zip || true 
+unzip -q -aa qt.zip || true
 
 echo "Building QT"
-./scripts/utils/qt6_compile.sh qt-everywhere-src-$QT_VERSION qt_dist 
+./scripts/utils/qt6_compile.sh qt-everywhere-src-$QT_VERSION qt_dist
 echo "Build Qt- Creating dist artifact"
-ls 
+ls
 echo $PWD
 mv qt-everywhere-src-$QT_VERSION/qt_dist/* qt_dist
-zip -r artifacts/qt6_mac.zip qt_dist/*
+mkdir -p ../../public/build
+zip -qr ../../public/build/qt6_mac.zip qt_dist/*
