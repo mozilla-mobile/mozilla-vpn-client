114392e3b248d97837688cda345033ec8ba5e367
Andrea Marchesini <amarchesini@mozilla.com>
2022-06-01 13:09:00 +0200

    Add extra checks for the VPN-2307 currency issue (#3649)

--- src/localizer.cpp
+++ src/localizer.cpp
@@ -322,6 +322,10 @@ QString Localizer::localizeCurrency(double value,
   }
 
   QString symbol = retrieveCurrencySymbolFallback(currencyIso4217, locale);
+  if (symbol.isEmpty()) {
+    return locale.toCurrencyString(value, currencyIso4217);
+  }
+
   return locale.toCurrencyString(value, symbol);
 }
 
