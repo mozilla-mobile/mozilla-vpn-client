f8c189d1af728708ca527cafca8269c6d663baa2
Andrea Marchesini <amarchesini@mozilla.com>
2022-06-01 09:01:29 +0200

    Expose a way to localize currency values from iso4217 - VPN-2307

--- tests/unit/testlocalizer.cpp
+++ tests/unit/testlocalizer.cpp
@@ -39,4 +39,23 @@ void TestLocalizer::systemLanguage() {
   QCOMPARE(l.previousCode(), "en");
 }
 
+void TestLocalizer::localizeCurrency() {
+  SettingsHolder settings;
+  Localizer l;
+  l.setCode("en_GB");
+
+  // Invalid iso4217 values
+  QCOMPARE(l.localizeCurrency(123.123, "FOOBAR"), "FOOBAR123.12");
+  QCOMPARE(l.localizeCurrency(123.123, "F"), "F123.12");
+
+  // Happy path
+  QCOMPARE(l.localizeCurrency(123.123, "GBP"), "£123.12");
+
+  // Let's guess - invalid currency
+  QCOMPARE(l.localizeCurrency(123.123, "AAA"), "AAA123.12");
+
+  // Let's guess - valid currency
+  QVERIFY(l.localizeCurrency(123.123, "EUR").contains("€"));
+}
+
 static TestLocalizer s_testLocalizer;
