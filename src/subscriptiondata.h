ae14e9f21b6d29df911188a3570a85b3b3ae278e
Florian Zia <zia.florian@gmail.com>
2022-06-08 13:25:24 +0200

    chore: Directly return sub data values

--- src/subscriptiondata.h
+++ src/subscriptiondata.h
@@ -14,23 +14,23 @@ class SubscriptionData final : public QObject {
   Q_PROPERTY(bool initialized READ initialized NOTIFY changed)
 
   // Subscription
-  Q_PROPERTY(int createdAt READ createdAt CONSTANT)
-  Q_PROPERTY(int expiresOn READ expiresOn CONSTANT)
-  Q_PROPERTY(bool isCancelled READ isCancelled CONSTANT)
-  Q_PROPERTY(QString status READ status CONSTANT)
-  Q_PROPERTY(QString type READ type CONSTANT)
+  Q_PROPERTY(int createdAt MEMBER m_createdAt CONSTANT)
+  Q_PROPERTY(int expiresOn MEMBER m_expiresOn CONSTANT)
+  Q_PROPERTY(bool isCancelled MEMBER m_isCancelled CONSTANT)
+  Q_PROPERTY(QString status MEMBER m_status CONSTANT)
+  Q_PROPERTY(QString type MEMBER m_type CONSTANT)
 
   // Plan
-  Q_PROPERTY(int planAmount READ planAmount CONSTANT)
-  Q_PROPERTY(QString planCurrency READ planCurrency CONSTANT)
-  Q_PROPERTY(int planIntervalCount READ planIntervalCount CONSTANT)
+  Q_PROPERTY(int planAmount MEMBER m_planAmount CONSTANT)
+  Q_PROPERTY(QString planCurrency MEMBER m_planCurrency CONSTANT)
+  Q_PROPERTY(int planIntervalCount MEMBER m_planIntervalCount CONSTANT)
 
   // Payment
-  Q_PROPERTY(QString paymentType READ paymentType CONSTANT)
-  Q_PROPERTY(QString creditCardBrand READ creditCardBrand CONSTANT)
-  Q_PROPERTY(QString creditCardLast4 READ creditCardLast4 CONSTANT)
-  Q_PROPERTY(int creditCardExpMonth READ creditCardExpMonth CONSTANT)
-  Q_PROPERTY(int creditCardExpYear READ creditCardExpYear CONSTANT)
+  Q_PROPERTY(QString paymentType MEMBER m_paymentType CONSTANT)
+  Q_PROPERTY(QString creditCardBrand MEMBER m_creditCardBrand CONSTANT)
+  Q_PROPERTY(QString creditCardLast4 MEMBER m_creditCardLast4 CONSTANT)
+  Q_PROPERTY(int creditCardExpMonth MEMBER m_creditCardExpMonth CONSTANT)
+  Q_PROPERTY(int creditCardExpYear MEMBER m_creditCardExpYear CONSTANT)
 
  public:
   SubscriptionData();
@@ -40,22 +40,6 @@ class SubscriptionData final : public QObject {
   Q_INVOKABLE bool fromJson(const QByteArray& json);
 
   bool initialized() const { return !m_rawJson.isEmpty(); }
-
-  int createdAt() const { return m_createdAt; }
-  int expiresOn() const { return m_expiresOn; }
-  bool isCancelled() const { return m_isCancelled; }
-  QString status() const { return m_status; }
-  QString type() const { return m_type; }
-
-  int planAmount() const { return m_planAmount; }
-  QString planCurrency() const { return m_planCurrency; }
-  int planIntervalCount() const { return m_planIntervalCount; }
-
-  QString paymentType() const { return m_paymentType; }
-  QString creditCardBrand() const { return m_creditCardBrand; }
-  QString creditCardLast4() const { return m_creditCardLast4; }
-  int creditCardExpMonth() const { return m_creditCardExpMonth; }
-  int creditCardExpYear() const { return m_creditCardExpYear; }
 
  signals:
   void changed();
