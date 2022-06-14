4f9846f7dc36f0eacd836baf037218783a56622b
Andrea Marchesini <amarchesini@mozilla.com>
2022-06-15 01:51:09 +0200

    Fix wasm

--- scripts/wasm/compile.sh
+++ scripts/wasm/compile.sh
@@ -57,9 +57,6 @@ python3 scripts/utils/import_languages.py || die "Failed to import languages"
 print Y "Generating glean samples..."
 python3 scripts/utils/generate_glean.py || die "Failed to generate glean samples"
 
-print Y "Generating addons..."
-python3 ./scripts/addon/generate_all.py || die "Failed to generate addons"
-
 print Y "Merge addons..."
 (
   cd addons/generated/addons
