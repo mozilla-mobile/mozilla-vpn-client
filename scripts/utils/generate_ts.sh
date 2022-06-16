b3f9e59f9eb407aefe022ec3ab664c916f677005
Andrea Marchesini <amarchesini@mozilla.com>
2022-06-16 06:31:16 +0200

    Fix an error in the translation string script

--- scripts/utils/generate_ts.sh
+++ scripts/utils/generate_ts.sh
@@ -67,9 +67,9 @@ for branch in $(git branch -r | grep origin/releases); do
     for f in $ts_files
     do
       ts_name=$(basename "$f")
-      if [ -f "addon_ts/${ts_name}"]; then
+      if [ -f "addon_ts/${ts_name}" ]; then
         printn Y "File ${ts_name} exists, updating with branch strings..."
-        lconvert -i "cache_rs/${ts_name}" "addons/generated/addons/${ts_name}" -o tmp.ts || die
+        lconvert -i "addon_ts/${ts_name}" "addons/generated/addons/${ts_name}" -o tmp.ts || die
         mv tmp.ts "addon_ts/${ts_name}"
       else
         printn Y "File ${ts_name} does not exist, copying over..."
@@ -82,6 +82,7 @@ done
 
 printn Y "Remove cache... "
 rm -rf cache || die
+rm -rf addon_ts || die
 print G "done."
 
 if [ "$BRANCHNAME" ]; then
