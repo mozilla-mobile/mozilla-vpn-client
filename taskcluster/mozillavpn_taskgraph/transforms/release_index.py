210d24e2ea8b8191f157d93c0bd5530c265f4800
Andrew Halberstadt <ahal@mozilla.com>
2022-06-23 09:19:41 -0400

    ci: add task type to branch index route
    This will allow us to add other types of tasks to this route in the
    future without namespace collisions.
    
    This also does a minor refactor of the 'add_branch_index' transform.

--- taskcluster/mozillavpn_taskgraph/transforms/release_index.py
+++ taskcluster/mozillavpn_taskgraph/transforms/release_index.py
@@ -4,21 +4,31 @@
 
 
 from taskgraph.transforms.base import TransformSequence
+
 transforms = TransformSequence()
 
 _GIT_REFS_HEADS_PREFIX = "refs/heads/"
+
+BRANCH_INDEX_ROUTES = (
+    "index.mozillavpn.v2.mozilla-vpn-client.branch.{branch}.latest.{task_type}.{name}",
+)
 
 
 @transforms.add
 def add_branch_index(config, tasks):
     for task in tasks:
-        if "add-branch-index" in task:
-            release_index = task.pop("add-branch-index")
-            if release_index and int(config.params["level"]) == 3:
-                name = task['name'].split("/")[0]
-                git_branch = config.params["head_ref"]
-                if git_branch.startswith(_GIT_REFS_HEADS_PREFIX):
-                    git_branch = git_branch[len(_GIT_REFS_HEADS_PREFIX):]
-                route = f"index.mozillavpn.v2.mozilla-vpn-client.branch.{git_branch}.latest.{name}"
-                task.setdefault("routes", []).append(route)
+        task_type = task.pop("add-branch-index", None)
+        if task_type and int(config.params["level"]) == 3:
+            name = task["name"].split("/")[0]
+            branch = config.params["head_ref"]
+            if branch.startswith(_GIT_REFS_HEADS_PREFIX):
+                branch = branch[len(_GIT_REFS_HEADS_PREFIX):]
+
+            task.setdefault("routes", [])
+            for route in BRANCH_INDEX_ROUTES:
+                task["routes"].append(route.format(
+                    branch=branch,
+                    task_type=task_type,
+                    name=name,
+                ))
         yield task
