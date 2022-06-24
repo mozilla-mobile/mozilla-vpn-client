7ca35f2feb7fb6ff5951fcb3223223253e1741b1
Andrew Halberstadt <ahal@mozilla.com>
2022-06-24 10:35:15 -0400

    ci: add an index route for 'github-release' events

--- taskcluster/mozillavpn_taskgraph/transforms/release_index.py
+++ taskcluster/mozillavpn_taskgraph/transforms/release_index.py
@@ -12,23 +12,36 @@ _GIT_REFS_HEADS_PREFIX = "refs/heads/"
 BRANCH_INDEX_ROUTES = (
     "index.mozillavpn.v2.mozilla-vpn-client.branch.{branch}.latest.{task_type}.{name}",
 )
+RELEASE_INDEX_ROUTES = (
+    "index.mozillavpn.v2.mozilla-vpn-client.release.{version}.latest.{task_type}.{name}",
+)
 
 
 @transforms.add
-def add_branch_index(config, tasks):
+def add_index_routes(config, tasks):
     for task in tasks:
-        task_type = task.pop("add-branch-index", None)
-        if task_type and int(config.params["level"]) == 3:
-            name = task["name"].split("/")[0]
+        task_type = task.pop("add-index-routes", None)
+        if not task_type or int(config.params["level"]) != 3:
+            yield task
+            continue
+
+        routes = []
+        context = {
+            "name": task["name"].split("/")[0],
+            "task_type": task_type,
+        }
+        if config.params["tasks_for"] == "github-push":
+            routes = BRANCH_INDEX_ROUTES
             branch = config.params["head_ref"]
             if branch.startswith(_GIT_REFS_HEADS_PREFIX):
                 branch = branch[len(_GIT_REFS_HEADS_PREFIX):]
+            context["branch"] = branch
 
-            task.setdefault("routes", [])
-            for route in BRANCH_INDEX_ROUTES:
-                task["routes"].append(route.format(
-                    branch=branch,
-                    task_type=task_type,
-                    name=name,
-                ))
+        elif config.params["tasks_for"] == "github-release":
+            routes = RELEASE_INDEX_ROUTES
+            context["version"] = config.params["head_tag"]
+
+        task.setdefault("routes", []).extend(
+            [route.format(**context) for route in routes]
+        )
         yield task
