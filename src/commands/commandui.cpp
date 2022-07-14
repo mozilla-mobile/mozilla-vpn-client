3d6ce13a74d8947711c2b66be9b7f2ace63a280d
Andrea Marchesini <amarchesini@mozilla.com>
2022-07-14 12:39:57 -0700

    Quit the app asap without calling the DTOR

--- src/commands/commandui.cpp
+++ src/commands/commandui.cpp
@@ -529,8 +529,13 @@ int CommandUI::run(QStringList& tokens) {
           return obj;
         });
 
+#if MVPN_IOS && QT_VERSION >= 0x060000 && QT_VERSION < 0x060300
+    QObject::connect(qApp, &QCoreApplication::aboutToQuit, &vpn,
+                     &MozillaVPN::quit);
+#else
     QObject::connect(qApp, &QCoreApplication::aboutToQuit, &vpn,
                      &MozillaVPN::aboutToQuit);
+#endif
 
     QObject::connect(
         qApp, &QGuiApplication::commitDataRequest, &vpn,
