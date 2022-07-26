2146eadb50f38cf4e6fda6b6ab447129be1cf153
Sebastian Streich <streich.mobile@gmail.com>
2022-07-26 12:38:15 +0200

    fixes

--- taskcluster/scripts/addons/build.sh
+++ taskcluster/scripts/addons/build.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 set -e
 
-# Fetch strings
-auth_header="$(git config --local --get http.https://github.com/.extraheader)"
 git submodule sync --recursive
-git -c "http.extraheader=$auth_header" -c protocol.version=2 submodule update --init --force --recursive --depth=1
+git submodule update --init --force --recursive --depth=1
+
 
 # Reqs
 pip3 install -r requirements.txt
