b1c45657c3bb7919797b162c45406c11198eb8a5
Beatriz Rizental <beatriz.rizental@gmail.com>
2022-07-27 17:11:37 +0200

    BUGFIX: Apply various fixes to MacOS cmake configuration (#4030)
    * Include network dependencies in macos.cmake
    
    These changes were necessary due to the changes introduced in
    https://github.com/mozilla-mobile/mozilla-vpn-client/pull/3651/files
    
    * Guarantee cmake overwrites CMAKE_OSX_DEPLOYMENT_TARGET variable
    
    I am not completely sure why, but with caching this variable was not set
    which prevented the Balrog enabling step and icon generation steps to
    run for the MacOS build.
    
    I attempted to move the `set` call for this variable to before the
    `project` call as advised in
    https://cmake.org/cmake/help/latest/variable/CMAKE_OSX_DEPLOYMENT_TARGET.html,
    but that did not work.
    
    * EXTRA: Add doc about how to open app from terminal

--- src/cmake/macos.cmake
+++ src/cmake/macos.cmake
@@ -26,11 +26,13 @@ find_library(FW_SYSTEMCONFIG SystemConfiguration)
 find_library(FW_SERVICEMGMT ServiceManagement)
 find_library(FW_SECURITY Security)
 find_library(FW_COREWLAN CoreWLAN)
+find_library(FW_NETWORK Network)
 
 target_link_libraries(mozillavpn PRIVATE ${FW_SYSTEMCONFIG})
 target_link_libraries(mozillavpn PRIVATE ${FW_SERVICEMGMT})
 target_link_libraries(mozillavpn PRIVATE ${FW_SECURITY})
 target_link_libraries(mozillavpn PRIVATE ${FW_COREWLAN})
+target_link_libraries(mozillavpn PRIVATE ${FW_NETWORK})
 
 qt6_import_qml_plugins(mozillavpn)
 
@@ -75,6 +77,8 @@ target_sources(mozillavpn PRIVATE
     platforms/macos/macosnetworkwatcher.h
     platforms/macos/macosutils.mm
     platforms/macos/macosutils.h
+    platforms/ios/iosnetworkwatcher.mm
+    platforms/ios/iosnetworkwatcher.h
 )
 
 include(cmake/osxtools.cmake)
