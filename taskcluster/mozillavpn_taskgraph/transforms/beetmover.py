3afaa0f3d0453eb7c22c6ac23aea18c47bd5234d
Gabriel Bustamante <gbustamante@mozilla.com>
2022-08-03 15:56:20 -0500

    Fix archive link in beetmover task description

--- taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
+++ taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
@@ -39,7 +39,7 @@ def add_beetmover_worker_config(config, tasks):
         )
         destination_paths = [candidates_path]
         archive_url = "https://ftp.stage.mozaws.net/" if config.params["level"] != 3 else "https://ftp.mozilla.org/"
-        task_description = f"This {worker_type} task will upload a release candidate for v{app_version} of the {app_name} on {build_os} to {archive_url}{candidates_path}"
+        task_description = f"This {worker_type} task will upload a {build_os} release candidate for {app_name} v{app_version} to {archive_url}{candidates_path}/"
         branch = config.params["head_ref"]
 
         def get_artifact_path(path):
