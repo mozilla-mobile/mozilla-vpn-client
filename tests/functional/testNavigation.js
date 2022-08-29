d57fda654b62db4d5cba1b7a6baa2f35406fc99d
Lesley Norton <lesley@mozilla.com>
2022-08-28 22:26:01 -0500

    Start navbar functional tests

--- tests/functional/testNavigation.js
+++ tests/functional/testNavigation.js
@@ -3,50 +3,200 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const assert = require('assert');
+const {waitForElementProperty} = require('./helper.js');
 const vpn = require('./helper.js');
 
-const assert = require('assert');
-const vpn = require('./helper.js');
-
-describe('Settings', function() {
+describe('Navigation bar', async function() {
   this.timeout(60000);
 
-  /* Nav bar is...
-    ... not visible over initial screen
-    ... not visible over mobile onboarding
-    ... not visible during in-app auth
-    ... not visible during browser auth
-    ... not visible over telemetry screen
-    ... not visible over post-auth screen
-    ... not visible over subscription needed
-    ... not visible over reauth screen
-
-    - visible over home screens
-    - visible over settings screens
-    - visible over messaging screens
-    - visible over get help screens
-
-    - clicking settings icon opens settings screen
-    - clicking home icon opens home screen
-    - clicking messages icon opens messages screen
-
-    - double clicking settings icon unwinds settings stack
-    - double clicking home icon unwinds home stack
-    - double clicking messaging icon unwinds messaging stack
+  async function navigationBarVisible() {
+    await vpn.waitForElement('navigationLayout/navButton-home');
+    return await vpn.getElementProperty(
+        'navigationLayout/navButton-home', 'visible');
+  }
 
 
-    - selected navbar correctly reflects screen opens from systray
+  it('Is not visible over initial screen', async () => {
+    await vpn.wait();
+    assert(await navigationBarVisible() === 'false');
+  });
 
+
+  it('Is not visible over pre-authentication Get help menu', async () => {
+    await vpn.clickOnElement('getHelpLink');
+    await vpn.waitForElement('getHelpBack');
+    await vpn.waitForElementProperty('getHelpBack', 'visible', 'true');
+    await vpn.wait();
+    assert(await navigationBarVisible() === 'false');
+  });
+
+
+  it('Is not visible over desktop onboarding', async () => {
+    await vpn.waitForElement('learnMoreLink')
+        await vpn.clickOnElement('learnMoreLink');
+    assert(await navigationBarVisible() === 'false');
+  });
+
+
+  it('Is not visible during browser authentication', async () => {
+    await vpn.waitForMainView();
+    await vpn.clickOnElement('getStarted');
+
+    if (!this.ctx.wasm) {
+      await vpn.waitForCondition(async () => {
+        const url = await vpn.getLastUrl();
+        return url.includes('/api/v2/vpn/login');
+      });
+    }
+
+    await vpn.waitForElement('authenticatingView');
+    await vpn.waitForElementProperty('authenticatingView', 'visible', 'true');
+    assert(await navigationBarVisible() === 'false');
+  });
+
+
+  it('Is not visible over telemetry screen', async () => {
+    await vpn.authenticateInApp(true, false);
+    await vpn.waitForElement('telemetryPolicyButton');
+    assert(await navigationBarVisible() === 'false');
+  });
+
+
+  it('Is visible over the Home screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-home');
+    await vpn.waitForElementProperty(
+        'navigationLayout/navButton-home', 'visible', 'true');
+    assert(await navigationBarVisible() === 'true');
+  });
+
+
+  it('Has Settings, Messaging, and Home buttons', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-home');
+    await vpn.waitForElementProperty(
+        'navigationLayout/navButton-home', 'visible', 'true');
+
+    await vpn.waitForElement('navigationLayout/navButton-messages');
+    await vpn.waitForElementProperty(
+        'navigationLayout/navButton-messages', 'visible', 'true');
+
+    await vpn.waitForElement('navigationLayout/navButton-settings');
+    await vpn.waitForElementProperty(
+        'navigationLayout/navButton-settings', 'visible', 'true');
+  });
+
+
+  it('Clicking the Settings button opens settings screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-settings');
+    await vpn.clickOnElement('navigationLayout/navButton-settings');
+    await vpn.waitForElement('settingsView');
+    assert(await vpn.getElementProperty('settingsView', 'visible') === 'true');
+  });
+
+
+  it('Clicking the Messages button opens messaging screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-messages');
+    await vpn.clickOnElement('navigationLayout/navButton-messages');
+    await vpn.waitForElement('messagingView');
+    assert(await vpn.getElementProperty('messagingView', 'visible') === 'true');
+  });
+
+
+  it('Clicking the Home button opens home screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-messages');
+    await vpn.clickOnElement('navigationLayout/navButton-messages');
+    await vpn.waitForElement('messagingView');
+
+    await vpn.clickOnElement('navigationLayout/navButton-home');
+    await vpn.waitForElement('serverListButton');
+
+    assert(
+        await vpn.getElementProperty('serverListButton', 'visible') === 'true');
+  });
+
+
+  it('Clicking the Homes button opens home screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-messages');
+    await vpn.clickOnElement('navigationLayout/navButton-messages');
+    await vpn.waitForElement('messagingView');
+
+    await vpn.clickOnElement('navigationLayout/navButton-home');
+    await vpn.waitForElement('serverListButton');
+
+    assert(
+        await vpn.getElementProperty('serverListButton', 'visible') === 'true');
+  });
+
+
+  it('Clicking a selected Home button reloads the Home screen', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('serverListButton');
+    await vpn.clickOnElement('serverListButton');
+    await vpn.waitForElement('serverListBackButton');
+    await vpn.waitForElementProperty('serverListBackButton', 'visible', 'true');
+
+    await vpn.clickOnElement('navigationLayout/navButton-home');
+    assert(
+        await vpn.getElementProperty('serverListButton', 'visible') === 'true');
+  });
+
+
+  it('Clicking a selected Settings button reloads the Settings screen',
+     async () => {
+       await vpn.authenticateInApp(true, true);
+       await vpn.wait();
+       await vpn.waitForElement('navigationLayout/navButton-settings')
+           await vpn.clickOnElement('navigationLayout/navButton-settings');
+
+       await vpn.waitForElement('settingsGetHelp');
+       await vpn.waitForElementProperty('settingsGetHelp', 'visible', 'true');
+       await vpn.clickOnElement('settingsGetHelp');
+
+       await vpn.waitForElement('getHelpBack');
+       await vpn.waitForElementProperty('getHelpBack', 'visible', 'true');
+
+       await vpn.clickOnElement('navigationLayout/navButton-settings');
+
+       assert(
+           await vpn.getElementProperty('settingsGetHelp', 'visible') ===
+           'true');
+     });
+
+
+  it('Is visible over the post-auth Get help menu', async () => {
+    await vpn.authenticateInApp(true, true);
+    await vpn.wait();
+    await vpn.waitForElement('navigationLayout/navButton-settings')
+        await vpn.clickOnElement('navigationLayout/navButton-settings');
+
+    await vpn.waitForElement('settingsGetHelp');
+    await vpn.waitForElementProperty('settingsGetHelp', 'visible', 'true');
+    await vpn.clickOnElement('settingsGetHelp');
+
+    await vpn.waitForElement('getHelpBack');
+    await vpn.waitForElementProperty('getHelpBack', 'visible', 'true');
+
+    await vpn.clickOnElement('navigationLayout/navButton-settings');
+
+    assert(await navigationBarVisible() === 'true');
+  });
+
+
+  /* TODOS...
+    - selected navbar icon correctly reflects screen opens from systray...
+    - not visible during reauth screen...
     what else?
-  */
-
-  /* Menu bar
-    ... has no back button on initial views
-    ... is not visible if menu.title === ""
-    ... accessibleName aligns with image
-  */
-
-  /*
-
   */
 });
