dd0802e47e70d5f3531b18c2251ba2129b786f9e
Sebastian Streich <streich.mobile@gmail.com>
2022-08-30 20:43:15 +0200

    formatting

--- tools/wasm_chrome/branch_selector.mjs
+++ tools/wasm_chrome/branch_selector.mjs
@@ -3,145 +3,148 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-const octokit = new Octokit({ });
+const octokit = new Octokit({});
 
-const static_branch_info = fetch("./branch_runs.json").then(r=>r.json());
-
+const static_branch_info = fetch("./branch_runs.json").then((r) => r.json());
 
 /**
- * 
  * <branch-selector></branch-selector>
- * 
+ *
  * BranchSelector is a custom element, extendins a <select> element, that
  * allows the user to select a branch from a list of branches, from the Mozilla-VPN repo
- * 
+ *
  * currently caches the branches for 24h ... needs an api key to get around that
- * 
+ *
  * fires a "change" event when the user selects a branch
  * the current branch is stored in the "value" property of the element
  */
-class BranchSelector extends HTMLElement{
-    /** @type {ShadowRoot} */
-    #dom = null;
+class BranchSelector extends HTMLElement {
+  /** @type {ShadowRoot} */
+  #dom = null;
 
-    #data = [];
+  #data = [];
 
-    value = "";
+  value = "";
 
-    #firedOnload= false;
+  #firedOnload = false;
 
-    constructor(){
-        super();
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    this.#dom = this.attachShadow({ mode: "open" });
+    this.render();
+    this.#data = await this.getData();
+    this.update();
+  }
+
+  async getData() {
+    const maybeStored = localStorage.getItem("branches");
+
+    if (maybeStored) {
+      const stored = JSON.parse(maybeStored);
+      const stored_age = new Date() - new Date(stored.date);
+      // The branches should be cached for 1 hour
+      if (stored_age < 1000 * 60 * 60 * 1) {
+        console.log("Using Cached Data");
+        return stored.data;
+      }
     }
-
-
-    async connectedCallback(){
-        this.#dom = this.attachShadow({mode: 'open'});
-        this.render();
-        this.#data = await this.getData();
-        this.update();
+    // We can't use the stored ones >:c - Too old
+    const response = await octokit.request(
+      "GET /repos/{owner}/{repo}/branches",
+      {
+        owner: "mozilla-mobile",
+        repo: "mozilla-vpn-client",
+        per_page: 100,
+      },
+    );
+    let branch_info = [];
+    try {
+      branch_info = await static_branch_info; // Make Sure this is ready
+    } catch (error) {
+      console.error(error);
     }
+    const unfiltered_branches = response.data;
+    // Filter the branches we just got from github:
+    // if we know the head-sha of the breanch does not have
+    // a taskcluster run (because too told), drop it.
+    const data = unfiltered_branches.filter((branch) => {
+      const branch_metadata = branch_info[branch.commit.sha];
+      if (!branch_metadata) {
+        // We don't know anything about that branch, add it to final list
+        return true;
+      }
+      if (branch_metadata.name === "main") {
+        // It's okay if we did not have valid info about main
+        // on build time, as gh pages task could have been faster
+        // as the taskcluster-wasm task
+        return true;
+      }
+      // It's ok if it has a running or completed task attached.
+      return branch_metadata.task_status == "ok";
+    });
 
+    console.log(`RateLimit -> ${response.headers["x-ratelimit-remaining"]}`);
 
-    async getData(){
-        const maybeStored = localStorage.getItem("branches");
+    localStorage.setItem(
+      "branches",
+      JSON.stringify({
+        date: new Date(),
+        data,
+      }),
+    );
+    return data;
+  }
 
-        if(maybeStored){
-            const stored = JSON.parse(maybeStored);
-            const stored_age = new Date() - new Date(stored.date) ;
-            // The branches should be cached for 1 hour
-            if(stored_age < 1000 * 60 * 60 * 1){
-                console.log("Using Cached Data");
-                return stored.data;
-            }
-        }
-        // We can't use the stored ones >:c - Too old
-        const response = await octokit.request('GET /repos/{owner}/{repo}/branches', {
-            owner: 'mozilla-mobile',
-            repo: 'mozilla-vpn-client',
-            per_page: 100
-        });
-        let branch_info = []
-        try {
-            branch_info = await static_branch_info; // Make Sure this is ready
-        } catch (error) {
-            console.error(error);
-        }
-        const unfiltered_branches = response.data;
-        // Filter the branches we just got from github:
-        // if we know the head-sha of the breanch does not have 
-        // a taskcluster run (because too told), drop it. 
-        const data = unfiltered_branches.filter((branch) => {
-            const branch_metadata = branch_info[branch.commit.sha]
-            if(!branch_metadata){
-                // We don't know anything about that branch, add it to final list
-                return true;
-            }
-            if(branch_metadata.name === "main"){
-                // It's okay if we did not have valid info about main
-                // on build time, as gh pages task could have been faster
-                // as the taskcluster-wasm task
-                return true;
-            }
-            // It's ok if it has a running or completed task attached.
-            return branch_metadata.task_status == "ok";
-        })
-        
-
-        console.log(`RateLimit -> ${response.headers["x-ratelimit-remaining"]}`)
-
-        localStorage.setItem("branches", JSON.stringify({
-            date: new Date(),
-            data,
-        }));
-        return data;
+  render() {
+    if (this.#dom.innerHTML != "") {
+      this.update();
     }
-
-    render(){
-        if(this.#dom.innerHTML != ''){
-            this.update();
-        }
-        this.#dom.innerHTML = `
+    this.#dom.innerHTML = `
             <select id="selector">
                     <option value="">Select a VPN</option>
             </select>
         `;
-        const selector = this.#dom.querySelector("#selector");
-        selector.addEventListener("change", e => {
-            // Forward the event to the parent element;
-            this.value = selector.value;
-            this.name = selector.options[selector.selectedIndex].text;
-            this.dispatchEvent(new CustomEvent("change", e));
-        });
-    }
-    update(){
-        const selector = this.#dom.querySelector("#selector");
-        selector.innerHTML = `
+    const selector = this.#dom.querySelector("#selector");
+    selector.addEventListener("change", (e) => {
+      // Forward the event to the parent element;
+      this.value = selector.value;
+      this.name = selector.options[selector.selectedIndex].text;
+      this.dispatchEvent(new CustomEvent("change", e));
+    });
+  }
+  update() {
+    const selector = this.#dom.querySelector("#selector");
+    selector.innerHTML = `
             <option value="">Select a VPN-Branch</option>
-            ${this.#data.map((e)=>{
-                return `<option value="${e.commit.sha}">${e.name}</option>`	
-            })};
+            ${
+      this.#data.map((e) => {
+        return `<option value="${e.commit.sha}">${e.name}</option>`;
+      })
+    };
         `;
 
-        if(!this.#firedOnload){
-            requestIdleCallback(()=>{
-                const url = new URL(window.location);
-                let name = url.searchParams.get('branch');
-                if(!name){
-                    name="main";
-                }
-                const option = Array.from(selector.querySelectorAll(`option`)).find(e => e.text == name);
-                if(option){
-                    option.selected = true;
-                    this.value = option.value;
-                    this.name = option.text;
-                    this.dispatchEvent(new CustomEvent("change", {}));
-                }
-            });
+    if (!this.#firedOnload) {
+      requestIdleCallback(() => {
+        const url = new URL(window.location);
+        let name = url.searchParams.get("branch");
+        if (!name) {
+          name = "main";
         }
-
-        
+        const option = Array.from(selector.querySelectorAll(`option`)).find(
+          (e) => e.text == name,
+        );
+        if (option) {
+          option.selected = true;
+          this.value = option.value;
+          this.name = option.text;
+          this.dispatchEvent(new CustomEvent("change", {}));
+        }
+      });
     }
+  }
 }
 
 customElements.define("branch-selector", BranchSelector);
