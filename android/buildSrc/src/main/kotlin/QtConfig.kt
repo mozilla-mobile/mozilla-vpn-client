e55107d2a34265385bad90a16aec553c444c451b
Sebastian Streich <streich.mobile@gmail.com>
2022-08-30 19:32:33 +0200

    Cleanup of build.gradle

--- android/buildSrc/src/main/kotlin/QtConfig.kt
+++ android/buildSrc/src/main/kotlin/QtConfig.kt
@@ -2,29 +2,28 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.CopySpec
 import org.gradle.api.file.DuplicatesStrategy
-import org.gradle.internal.impldep.com.google.gson.JsonArray
 import org.gradle.kotlin.dsl.provideDelegate
 import org.json.JSONArray
-import org.json.JSONObject
-import java.io.File
-import javax.inject.Inject
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
-
+import javax.inject.Inject
 
 // This Plugin will help in finding the right paths for QT.
 //
 class QtConfig : Plugin<Project> {
-    private val qtAndroidDir ="~/Qt/6.2.4" // Just a dummy, will be overwritten hopefully
+    private val qtAndroidDir = "~/Qt/6.2.4" // Just a dummy, will be overwritten hopefully
 
     override fun apply(project: Project) {
-        if(!project.hasProperty("mozillavpn_qt_android_dir")){
-            error("mozillavpn_qt_android_dir is not set as gradle property \n "+
+        if (!project.hasProperty("mozillavpn_qt_android_dir")) {
+            error(
+                "mozillavpn_qt_android_dir is not set as gradle property \n " +
                     "Please add it to gradle.properties or set is as env"
             )
         }
-        if(!project.hasProperty("mozillavpn_qt_host_dir")){
-            error("mozillavpn_qt_host_dir is not set as gradle property \n "+
+        if (!project.hasProperty("mozillavpn_qt_host_dir")) {
+            error(
+                "mozillavpn_qt_host_dir is not set as gradle property \n " +
                     "Please add it to gradle.properties or set is as env"
             )
         }
@@ -32,90 +31,72 @@ class QtConfig : Plugin<Project> {
     }
 }
 
-
 open class QTConfigurationExtension
 @Inject constructor(
     private val project: Project
-){
+) {
     @Suppress("unused")
-    val host :String by lazy {
+    val host: String by lazy {
         val dir = File(project.properties["mozillavpn_qt_host_dir"].toString())
-        if(!dir.exists()){
+        if (!dir.exists()) {
             error("You need to define a QT host dir")
         }
         dir.absolutePath.toString()
     }
     @Suppress("unused")
-    val qtAndroidRootDir :String by lazy{
+    val qtAndroidRootDir: String by lazy {
         File(anyAndroid).parentFile.absolutePath
     }
 
-
     @Suppress("unused")
-    val arm64:String by lazy { getPath(QT_arm64) }
+    val arm64: String by lazy { getPath(QT_arm64) }
     @Suppress("unused")
-    val armv7:String by lazy { getPath(QT_armv7) }
-    val x86:String by lazy { getPath(QT_x84) }
+    val armv7: String by lazy { getPath(QT_armv7) }
+    val x86: String by lazy { getPath(QT_x84) }
     @Suppress("unused")
-    val x64:String by lazy { getPath(QT_x64) }
+    val x64: String by lazy { getPath(QT_x64) }
     @Suppress("unused")
-    val anyAndroid :String by lazy{
-        listOf(x64,x86,arm64,armv7).first { it.isNotEmpty() }
+    val anyAndroid: String by lazy {
+        listOf(x64, x86, arm64, armv7).first { it.isNotEmpty() }
     }
-
 
     @Suppress("unused")
     val abis: String by lazy {
-        var out = listOf(x86,arm64,x64,armv7).filter { it.isNotEmpty() }.map {
-            qt_archMap[File(it).name];
+        var out = listOf(x86, arm64, x64, armv7).filter { it.isNotEmpty() }.map {
+            qt_archMap[File(it).name]
         }
         // Massage the string a bit so gradle takes it as a valid abi-filter value
-       val b = out.toString().replace("[","").replace("]","").replace(" ", "")
+        val b = out.toString().replace("[", "").replace("]", "").replace(" ", "")
         println(b)
         b
     }
     @Suppress("unused")
-    val hasArm64 :Boolean  by lazy { arm64.isNotEmpty() }
+    val hasArm64: Boolean by lazy { arm64.isNotEmpty() }
     @Suppress("unused")
-    val hasArmv7 :Boolean  by lazy { armv7.isNotEmpty() }
+    val hasArmv7: Boolean by lazy { armv7.isNotEmpty() }
     @Suppress("unused")
-    val hasX86 : Boolean  by lazy { x86.isNotEmpty() }
+    val hasX86: Boolean by lazy { x86.isNotEmpty() }
     @Suppress("unused")
-    val hasX64 : Boolean  by lazy { x64.isNotEmpty() }
+    val hasX64: Boolean by lazy { x64.isNotEmpty() }
 
     @Suppress("unused")
-    val bindingsFolder : String by lazy { "$anyAndroid/src/android/java/src/org/qtproject/qt/android/bindings/" }
+    val bindingsFolder: String by lazy { "$anyAndroid/src/android/java/src/org/qtproject/qt/android/bindings/" }
 
-
-    private fun getPath(QT_ABI:String) : String{
+    private fun getPath(QT_ABI: String): String {
         val androidDir = File(project.properties["mozillavpn_qt_android_dir"].toString())
-        if(!androidDir.exists()){
+        if (!androidDir.exists()) {
             error("Android Qt Directory not existing")
         }
-        val maybeABI = androidDir.resolve(QT_ABI);
-        if(!maybeABI.exists()){
+        val maybeABI = androidDir.resolve(QT_ABI)
+        if (!maybeABI.exists()) {
             return ""
         }
         return maybeABI.absolutePath
     }
 
-
-    // Generates Gradle Copy Tasks for the given Android-Plattform-Plugin for a given ABI
-    fun copyAndroidPlugin(abi:String, target:String):CopySpec{
-        val pluginPath = File("$abi/plugins/platforms")
-        if(!pluginPath.exists()){
-            return project.copySpec()
-        }
-        return project.copySpec {
-            this.into(target)
-
-            this.from(pluginPath)
-        }
-    }
-
-    fun importQtPlugin(name:String,abi:String,target:String):CopySpec{
+    private fun importQtPlugin(name: String, abi: String, target: String): CopySpec {
         val pluginPath = File("$abi/plugins/$name")
-        if(!pluginPath.exists()){
+        if (!pluginPath.exists()) {
             return project.copySpec()
         }
         return project.copySpec {
@@ -123,35 +104,51 @@ open class QTConfigurationExtension
             this.from(pluginPath)
         }
     }
+    fun importQtPlugin(name: String): CopySpec {
+        return project.copySpec {
+            this.with(importQtPlugin(name, x64, NDK_x64))
+            this.with(importQtPlugin(name, arm64, NDK_arm64))
+            this.with(importQtPlugin(name, x86, NDK_x86))
+            this.with(importQtPlugin(name, armv7, NDK_armv7))
+        }
+    }
 
-    fun copyQMLCompat(abi:String, target:String):CopySpec{
+    fun copyQMLCompat(abi: String, target: String): CopySpec {
         val copyTask = project.copySpec()
-        val path = getPath(abi)+"/qml/Qt5Compat/GraphicalEffects/private"
+        val path = getPath(abi) + "/qml/Qt5Compat/GraphicalEffects/private"
         val Qt5CompatFolder = File(path)
         if (!Qt5CompatFolder.exists()) {
-            return copyTask;
+            return copyTask
         }
-        copyTask.from(path){
+        copyTask.from(path) {
             include("*.so")
         }
         copyTask.into(target)
-        copyTask.duplicatesStrategy=DuplicatesStrategy.EXCLUDE
-        return copyTask;
+        copyTask.duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        return copyTask
+    }
+    fun copyQMLCompat(): CopySpec {
+        return project.copySpec {
+            this.with(copyQMLCompat(x64, NDK_x64))
+            this.with(copyQMLCompat(arm64, NDK_arm64))
+            this.with(copyQMLCompat(x86, NDK_x86))
+            this.with(copyQMLCompat(armv7, NDK_armv7))
+        }
     }
 
     // Generates Gradle Copy Tasks to for our QML imports for the given ABI
-    fun copyQMLPlugin(abi:String, target:String):CopySpec{
+    fun copyQMLPlugin(abi: String, target: String): CopySpec {
         val imports = getQMLImports(abi)
-        if(imports.isEmpty){
+        if (imports.isEmpty) {
             // No imports for that abi
             return project.copySpec()
         }
         val copyTask = project.copySpec()
         copyTask.into(target)
 
-        for(i in 0 until imports.length()){
-            val import = imports.getJSONObject(i);
-            if(import.has("path")){ // Qt has found a static qml dependency
+        for (i in 0 until imports.length()) {
+            val import = imports.getJSONObject(i)
+            if (import.has("path")) { // Qt has found a static qml dependency
                 val path = import.getString("path")
                 copyTask.from(path) {
                     include("*.so")
@@ -159,6 +156,14 @@ open class QTConfigurationExtension
             }
         }
         return copyTask
+    }
+    fun copyQMLPlugin(): CopySpec {
+        return project.copySpec {
+            this.with(copyQMLPlugin(x64, NDK_x64))
+            this.with(copyQMLPlugin(arm64, NDK_arm64))
+            this.with(copyQMLPlugin(x86, NDK_x86))
+            this.with(copyQMLPlugin(armv7, NDK_armv7))
+        }
     }
 
     // Runs QMLImportscanner from qt_host against the
@@ -171,11 +176,11 @@ open class QTConfigurationExtension
         val repo_path = project.rootProject.projectDir.parent
         val qmlPath = File("$repo_path/src/ui")
         val qtQMLPath = File("$abi/qml")
-        if(!qmlPath.exists()){
+        if (!qmlPath.exists()) {
             return JSONArray()
         }
         val process =
-            Runtime.getRuntime().exec("$importScanner -rootPath $qmlPath -importPath $qtQMLPath");
+            Runtime.getRuntime().exec("$importScanner -rootPath $qmlPath -importPath $qtQMLPath")
         val processOutput = StringBuilder()
         BufferedReader(
             InputStreamReader(process.inputStream)
@@ -190,8 +195,7 @@ open class QTConfigurationExtension
         return JSONArray(processOutput.toString())
     }
 
-
-     companion object {
+    companion object {
         const val NDK_arm64 = "arm64-v8a"
         const val NDK_armv7 = "armeabi-v7a"
         const val NDK_x86 = "x86"
@@ -202,14 +206,11 @@ open class QTConfigurationExtension
         const val QT_x84 = "android_x86"
         const val QT_x64 = "android_x86_64"
 
-         val qt_archMap = mapOf(
-             QT_arm64 to NDK_arm64,
-             QT_x64 to NDK_x64,
-             QT_x84 to NDK_x86,
-             QT_armv7 to NDK_armv7
-         )
+        val qt_archMap = mapOf(
+            QT_arm64 to NDK_arm64,
+            QT_x64 to NDK_x64,
+            QT_x84 to NDK_x86,
+            QT_armv7 to NDK_armv7
+        )
     }
 }
-
-
-
--- android/buildSrc/src/main/kotlin/VPNVersion.kt
+++ android/buildSrc/src/main/kotlin/VPNVersion.kt
@@ -2,9 +2,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.provideDelegate
 import java.io.File
-import javax.inject.Inject
 import java.util.*
-
+import javax.inject.Inject
 
 class VPNVersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -15,13 +14,13 @@ class VPNVersionPlugin : Plugin<Project> {
 open class VPNVersionExtension
 @Inject constructor(
     private val project: Project
-){
+) {
     @Suppress("unused")
     // The version code is a TimeStamp where we drop the last 3 digits,
     // so we roughly get a new code every 20 min
-    val versionCode : Int by lazy {
+    val versionCode: Int by lazy {
         var timestamp = Date()
-        if(project.hasProperty("VPN_PIN_TIMESTAMP")){
+        if (project.hasProperty("VPN_PIN_TIMESTAMP")) {
             timestamp = Date(project.properties["VPN_PIN_TIMESTAMP"].toString())
         }
         val timeString = timestamp.time.toString()
@@ -29,9 +28,9 @@ open class VPNVersionExtension
         slice.toInt()
     }
     @Suppress("unused")
-    val versionName : String by lazy{
+    val versionName: String by lazy {
         val cmake = File("${project.rootDir.absolutePath}/../CMakeLists.txt")
-        if(!cmake.exists()){
+        if (!cmake.exists()) {
             error("Could not find the Root cmake under ${cmake.absolutePath}")
         }
         val lines = cmake.readLines()
@@ -39,9 +38,6 @@ open class VPNVersionExtension
             it.startsWith("project(")
         }.orEmpty()
         val tokens = project.split(" ")
-        tokens[tokens.indexOf("VERSION")+1]
+        tokens[tokens.indexOf("VERSION") + 1]
     }
 }
-
-
-
