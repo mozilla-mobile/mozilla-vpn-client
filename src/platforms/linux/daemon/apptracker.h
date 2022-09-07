d62611b223212f462df29e72ff8581ad8599b980
Owen Kirby <oskirby@gmail.com>
2022-09-07 15:00:47 -0700

    Support split tunneling with cgroups v2 (#3303)
    * Refactor AppTracker to monitor for cgroupv2 slices.
    * Maintain a list of cgroup scopes to detect changes.
    * Add simple appId -> cgroup matching using the gtkLaunchEvent
    * Perform cgroupv2 matching with xt_cgroup
    * Implement crude split tunneling and fallback to cgroupsv1
    * Tidy up logger creation in netfilter.go
    * Add xt_cgroup kernel module dependency to start mozillavpn service.
    * Require Gnome shell version 3.34 for split tunnelling.

--- src/platforms/linux/daemon/apptracker.h
+++ src/platforms/linux/daemon/apptracker.h
@@ -5,27 +5,64 @@
 #ifndef APPTRACKER_H
 #define APPTRACKER_H
 
-#include <QDBusPendingCallWatcher>
 #include <QDBusObjectPath>
+#include <QFileSystemWatcher>
+#include <QString>
+
+#include "leakdetector.h"
+
+class QDBusInterface;
+
+class AppData {
+ public:
+  AppData(const QString& path) : cgroup(path) { MVPN_COUNT_CTOR(AppData); }
+  ~AppData() { MVPN_COUNT_DTOR(AppData); }
+
+  QList<int> pids() const;
+
+  const QString cgroup;
+  QString appId;
+  int rootpid = 0;
+};
 
 class AppTracker final : public QObject {
   Q_OBJECT
   Q_DISABLE_COPY_MOVE(AppTracker)
 
  public:
-  explicit AppTracker(QObject* parent);
+  explicit AppTracker(QObject* parent = nullptr);
   ~AppTracker();
 
+  void userCreated(uint userid, const QDBusObjectPath& path);
+  void userRemoved(uint userid, const QDBusObjectPath& path);
+
+  QHash<QString, AppData*>::iterator begin() { return m_runningApps.begin(); }
+  QHash<QString, AppData*>::iterator end() { return m_runningApps.end(); }
+  AppData* find(const QString& cgroup) { return m_runningApps.value(cgroup); }
+
  signals:
-  void appLaunched(const QString& name, int rootpid);
+  void appLaunched(const QString& cgroup, const QString& appId, int rootpid);
+  void appTerminated(const QString& cgroup, const QString& appId);
 
  private slots:
-  void userListCompleted(QDBusPendingCallWatcher* call);
-  void userCreated(uint uid, const QDBusObjectPath& path);
-  void userRemoved(uint uid, const QDBusObjectPath& path);
   void gtkLaunchEvent(const QByteArray& appid, const QString& display,
                       qlonglong pid, const QStringList& uris,
                       const QVariantMap& extra);
+
+  void cgroupsChanged(const QString& directory);
+
+ private:
+  void appHeuristicMatch(AppData* data);
+
+ private:
+  // Monitoring of the user's control groups.
+  QString m_cgroupMount;
+  QFileSystemWatcher m_cgroupWatcher;
+
+  // The set of applications that we have tracked.
+  QHash<QString, AppData*> m_runningApps;
+  QString m_lastLaunchName;
+  int m_lastLaunchPid;
 };
 
 #endif  // APPTRACKER_H
