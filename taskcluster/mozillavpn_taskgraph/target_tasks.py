553364a81070f0182c1018b1fa47fdcf7c56f2bb
Gabriel Bustamante <gbustamante@mozilla.com>
2022-09-23 16:18:27 -0500

    A bit of clean up for vpn taskgraph (#4532)
    * Simplify release-promotion target_task methods
    
    * Delete un-used build_number parameter from release-promotion action
    
    * Move addons release-artifacts generation to a separate transform

--- taskcluster/mozillavpn_taskgraph/target_tasks.py
+++ taskcluster/mozillavpn_taskgraph/target_tasks.py
@@ -5,11 +5,11 @@
 from taskgraph.target_tasks import _target_task
 
 
-@_target_task("promote-addons")
-def target_tasks_promote_addons(full_task_graph, parameters, graph_config):
+@_target_task("addons-target-tasks")
+def addons_target_tasks(full_task_graph, parameters, graph_config):
     def filter(task):
         if (
-            task.attributes.get("shipping-phase") == "promote-addons"
+            task.attributes.get("shipping-phase") == parameters["shipping_phase"]
             and task.attributes.get("build-type") == "addons/opt"
         ):
             return True
@@ -17,55 +17,11 @@ def target_tasks_promote_addons(full_task_graph, parameters, graph_config):
     return [label for label, task in full_task_graph.tasks.items() if filter(task)]
 
 
-@_target_task("ship-addons")
-def target_tasks_ship_addons(full_task_graph, parameters, graph_config):
-    filtered_for_candidates = target_tasks_promote_addons(
-        full_task_graph,
-        parameters,
-        graph_config,
-    )
-
-    def filter(task):
-        # Include promotion tasks; these will be optimized out
-        if task.label in filtered_for_candidates:
-            return True
-
-        if (
-            task.attributes.get("shipping-phase") == "ship-addons"
-            and task.attributes.get("build-type") == "addons/opt"
-        ):
-            return True
-
-    return [label for label, task in full_task_graph.tasks.items() if filter(task)]
-
-
-@_target_task("promote-client")
-def target_tasks_promote_client(full_task_graph, parameters, graph_config):
+@_target_task("client-target-tasks")
+def client_target_tasks(full_task_graph, parameters, graph_config):
     def filter(task):
         if (
-            task.attributes.get("shipping-phase") == "promote-client"
-            and task.attributes.get("build-type") != "addons/opt"
-        ):
-            return True
-
-    return [label for label, task in full_task_graph.tasks.items() if filter(task)]
-
-
-@_target_task("ship-client")
-def target_tasks_ship_client(full_task_graph, parameters, graph_config):
-    filtered_for_candidates = target_tasks_promote_client(
-        full_task_graph,
-        parameters,
-        graph_config,
-    )
-
-    def filter(task):
-        # Include promotion tasks; these will be optimized out
-        if task.label in filtered_for_candidates:
-            return True
-
-        if (
-            task.attributes.get("shipping-phase") == "ship-client"
+            task.attributes.get("shipping-phase") == parameters["shipping_phase"]
             and task.attributes.get("build-type") != "addons/opt"
         ):
             return True
