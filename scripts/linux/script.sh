a72b6a30707e7eef402b79d480e153b9aaa19e67
Owen Kirby <oskirby@gmail.com>
2022-10-25 09:07:22 -0700

    Add support for Ubuntu 22.10/Kinetic

--- scripts/linux/script.sh
+++ scripts/linux/script.sh
@@ -63,7 +63,7 @@ while [[ $# -gt 0 ]]; do
     shift
     ;;
   --source)
-    RELEASE="bionic focal jammy fedora"
+    RELEASE="bionic focal jammy kinetic fedora"
     SOURCEONLY=Y
     shift
     ;;
