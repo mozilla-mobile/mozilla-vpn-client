a85ea3049b379b7d1161e931148dc7d6fc18d6cd
Sebastian Streich <streich.mobile@gmail.com>
2022-11-14 17:40:53 +0100

    formatting

--- src/platforms/android/androidcontroller.cpp
+++ src/platforms/android/androidcontroller.cpp
@@ -46,11 +46,11 @@ AndroidController::AndroidController() {
       activity, &AndroidVPNActivity::eventInitialized, this,
       [this](const QString& parcelBody) {
         // We might get multiple Init events as widgets, or fragments
-        // might query this. 
-        if(m_init){
+        // might query this.
+        if (m_init) {
           return;
         }
-        m_init=true;
+        m_init = true;
         auto doc = QJsonDocument::fromJson(parcelBody.toUtf8());
         emit initialized(true, doc.object()["connected"].toBool(),
                          QDateTime::fromMSecsSinceEpoch(
