62fc9b2bace3e151f1148687602a99e96aaa25b3
Raphael Okafor Jr <rokafor@mozilla.com>
2022-11-14 10:32:06 -0500

    unit and linter fixes

--- src/addons/manager/addonmanager.cpp
+++ src/addons/manager/addonmanager.cpp
@@ -78,10 +78,13 @@ void AddonManager::initialize() {
   }
 
   // Listen for updates in the addons list
-  connect(&m_addonIndex, &AddonIndex::indexUpdated, this,
+  connect(&m_addonIndex, &AddonIndex::indexUpdated, this, 
           &AddonManager::updateAddonsList);
-  connect(SettingsHolder::instance(), &SettingsHolder::addonCustomServerChanged, this, &AddonManager::refreshAddons);
-  connect(SettingsHolder::instance(), &SettingsHolder::addonCustomServerAddressChanged, this, &AddonManager::refreshAddons);
+  connect(SettingsHolder::instance(), &SettingsHolder::addonCustomServerChanged, 
+          this, &AddonManager::refreshAddons);
+  connect(SettingsHolder::instance(), 
+          &SettingsHolder::addonCustomServerAddressChanged, this, 
+          &AddonManager::refreshAddons);
 }
 
 void AddonManager::updateIndex(const QByteArray& index,
