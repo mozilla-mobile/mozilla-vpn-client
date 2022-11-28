e85fde9d7f0459adb16f6ffb1a1f286e42a64b79
Owen Kirby <oskirby@gmail.com>
2022-11-28 12:17:10 -0800

    Experiment: CMake support for iOS (#4709)
    * Initial experiments building for iOS with CMake
    * Add Info.plist template
    * Build network extension for iOS
    * Add Adjust support with -DBUILD_ADJUST_SDK_TOKEN=<token>
    * Add some notes to the README.md
    * Remove duplicate wireguard-go-version.h
    * Fix cmake dependencies for glean
    * Use find_program to locate golang build tool.
    * Fix ios app entitlements
    * XCode: disable auto-generation of build schemes for every target
    * Add bundling of .xcasset catalog
    * Revisit codesigning for non-Xcode generators.
    * Ensure we use a consistent python for config and build
    * Fix cross-compilation bugs for golang libraries
    * Add workaround for iOS simulator builds on M1/M2 macbooks.
    * Fix bug in macos app entitlements
    * Split gleansample.h generation to fix Xcode dependency bug.
    * Update ci_post_clone.sh for Xcode cloud
    * Fix dependency loop between vpnglean and mozillavpn targets
    * README: Correct capitalization of Xcode
    * Fix vpnglean builds for iOS on M1 macs
    * vpnglean: Force linking with /usr/bin/cc when building in Xcode
    * qmake: Force python3 path for glean at config time
    * Update README with instructions for simulation
    * Fix missing vpnglean generated directory error
    
    Co-authored-by: Owen Kirby <osk@Owens-MBP-2.lan>
    Co-authored-by: brizental <beatriz.rizental@gmail.com>

--- scripts/utils/make_template.py
+++ scripts/utils/make_template.py
@@ -2,8 +2,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This program attempts to emulate CMake's `configure_file()` command
+# as closely as possible, but can be run as independetly from cmake as
+# a script, or as a build command.
 
 import sys
+import re
 import argparse
 
 # Parse arguments to determine what to do.
@@ -41,26 +46,40 @@ for keyfile in args.keyfile:
 # to any already-transformed text.
 def transform(text):
     start = 0
+    ac_regex = re.compile('@[\w.]+@') ## Autoconf style match: @VARNAME@
+    cm_regex = re.compile('\${[\w.]+}') ## CMake style match: ${VARNAME}
+    xc_regex = re.compile('\$\([\w.]+\)') ## Xcode style match: $(VARNAME)
     while start < len(text):
-        # Find the next matching keyword, if any.
-        matchIdx = -1
-        matchKey = ""
-        for key in keywords:
-            x = text.find(key, start)
-            if (x < 0):
-                continue
-            if (matchIdx < 0) or (x < matchIdx):
-                matchIdx = x
-                matchKey = key
+        ## Search for the next variable to substitute.
+        ac_match = ac_regex.search(text, start)
+        ac_start = ac_match.start() if ac_match else len(text)
+        cm_match = cm_regex.search(text, start)
+        cm_start = cm_match.start() if cm_match else len(text)
+        xc_match = xc_regex.search(text, start)
+        xc_start = xc_match.start() if xc_match else len(text)
 
-        # If there are no matches, we can return.
-        if matchIdx < 0:
+        if ac_match and (ac_start < cm_start) and (ac_start < xc_start):
+            match = ac_match
+            name = ac_match.group(0)[1:-1]
+        elif cm_match and (cm_start < ac_start) and (cm_start < xc_start):
+            match = cm_match
+            name = cm_match.group(0)[2:-1]
+        elif xc_match and (xc_start < ac_start) and (xc_start < cm_start):
+            match = xc_match
+            name = xc_match.group(0)[2:-1]
+        else:
+            # If there are no matches, we can return
             break
 
+        ## If there is no value, skip the match and keep searching.
+        if name not in keywords:
+            start = match.end()
+            continue
+
         # Substitute the keyword and adjust the start.
-        value = keywords[matchKey]
-        start = matchIdx + len(value)
-        text = text[0:matchIdx] + value + text[matchIdx+len(matchKey):]
+        value = keywords[name]
+        start = match.start() + len(value)
+        text = text[0:match.start()] + value + text[match.end():]
 
     return text
 
