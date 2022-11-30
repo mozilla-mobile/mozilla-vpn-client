412b28368eda7dc1b1eeacd944fb421d52790108
Andrea Marchesini <amarchesini@mozilla.com>
2022-11-30 12:48:29 +0100

    Test for the settings-rollback location (#5065)

--- tests/functional/testAddons.js
+++ tests/functional/testAddons.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const assert = require('assert');
+const {navBar, settingsScreen, homeScreen} = require('./elements.js');
 const vpn = require('./helper.js');
 
 describe('Addons', function() {
@@ -54,5 +55,69 @@ describe('Addons', function() {
                  await vpn.getElementProperty('VPNAddonManager', 'count'),
                  10) === 1;
     });
+  });
+
+  it('Settings rollback - location', async () => {
+    // Loading the custom tutorial
+    await vpn.resetAddons('05_settings_rollback');
+
+    await vpn.waitForCondition(async () => {
+      return parseInt(
+                 await vpn.getElementProperty('VPNAddonManager', 'count'),
+                 10) === 1;
+    });
+
+    const exitCityName =
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCityName');
+    const exitCountryCode =
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCountryCode');
+
+    // Let's start the tutorial
+    await vpn.waitForElementAndClick(navBar.SETTINGS);
+    await vpn.waitForElementAndClick(settingsScreen.TIPS_AND_TRICKS);
+    await vpn.waitForElementAndClick(homeScreen.TUTORIAL_LIST_HIGHLIGHT);
+
+    // Confirmation dialog for settings-rollback
+    await vpn.waitForElementProperty(
+        homeScreen.TUTORIAL_POPUP_PRIMARY_BUTTON, 'visible', 'true');
+    assert(
+        (await vpn.getElementProperty(
+            homeScreen.TUTORIAL_POPUP_PRIMARY_BUTTON, 'text')) === 'Continue');
+    await vpn.waitForElementAndClick(homeScreen.TUTORIAL_POPUP_PRIMARY_BUTTON);
+
+    await vpn.waitForCondition(async () => {
+      return await vpn.getElementProperty(
+                 'VPNCurrentServer', 'exitCityName') === 'Vienna';
+    });
+
+    assert(
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCityName') ===
+        'Vienna');
+    assert(
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCountryCode') ===
+        'at');
+
+    await vpn.waitForElementAndClick('serverListButton-btn');
+
+    // Final dialog
+    await vpn.waitForElementProperty(
+        homeScreen.TUTORIAL_POPUP_PRIMARY_BUTTON, 'visible', 'true');
+    assert(
+        (await vpn.getElementProperty(
+            homeScreen.TUTORIAL_POPUP_PRIMARY_BUTTON, 'text')) === 'Let’s go!');
+    await vpn.waitForElementAndClick(
+        homeScreen.TUTORIAL_POPUP_SECONDARY_BUTTON);
+
+    await vpn.waitForCondition(async () => {
+      return await vpn.getElementProperty(
+                 'VPNCurrentServer', 'exitCityName') === exitCityName;
+    });
+
+    assert(
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCityName') ===
+        exitCityName);
+    assert(
+        await vpn.getElementProperty('VPNCurrentServer', 'exitCountryCode') ===
+        exitCountryCode);
   });
 });
