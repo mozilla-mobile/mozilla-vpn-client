74bc40028fb00b720c5027b6f9f70cfe8921abcf
Owen Kirby <oskirby@gmail.com>
2022-12-08 14:49:39 -0600

    Use a timestamp for release candidate suffixes (#5177)
    * Use a timestamp for release candidate suffixes
    * We don't really need a full checkout anymore
    * Use UTC time

--- scripts/linux/script.sh
+++ scripts/linux/script.sh
@@ -118,14 +118,9 @@ if [[ "$GITREF" =~ ^refs/pull/([0-9]+)/merge ]]; then
 elif [[ "$GITREF" =~ ^refs/tags/v([0-9a-z.]+) ]]; then
   SHORTVERSION=${BASH_REMATCH[1]}
 elif [[ "$GITREF" =~ ^refs/heads/releases/([0-9][^/]*) ]]; then
-  if [[ $(git rev-parse --is-shallow-repository) == "true" ]]; then
-    git fetch --unshallow             # Unshallow the release branch
-    git fetch origin main:origin/main # Fetch the main branch
-  fi
-  RCVERSION="~rc$(git rev-list --count --first-parent origin/main..HEAD)"
-  SHORTVERSION="${BASH_REMATCH[1]}${RCVERSION}"
+  SHORTVERSION="${BASH_REMATCH[1]}~rc$(date -u +%Y%m%d%H%M%S)"
 elif [[ "$GITREF" == "refs/heads/main" ]]; then
-  SHORTVERSION="${SHORTVERSION}~nightly$(date +%Y%m%d)"
+  SHORTVERSION="${SHORTVERSION}~nightly$(date -u +%Y%m%d)"
 fi
 WORKDIR=mozillavpn-${SHORTVERSION}
 print G "${SHORTVERSION}"
