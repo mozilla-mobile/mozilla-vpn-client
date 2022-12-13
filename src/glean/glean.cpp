928c85387ce2a969e24590efb13e016cc9af075b
brizental <beatriz.rizental@gmail.com>
2022-12-13 10:27:22 +0100

    Address rebase mistakes and use relativd path to import glean header

--- src/glean/glean.cpp
+++ src/glean/glean.cpp
@@ -2,7 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "glean/glean.h"
+// TODO: Stop using the relative path once Glean.js is fully removed from the codebase.
+// Until then we need the relative path,
+// otherwise XCode is confused about what is being imported.
+#include "./glean.h"
 
 #include "glean/generated/metrics.h"
 #include "glean/generated/pings.h"
@@ -88,7 +91,7 @@ void VPNGlean::initialize() {
 #if defined(UNIT_TEST)
     glean_test_reset_glean(uploadEnabled, dataPath.toLocal8Bit());
 #elif defined(MVPN_IOS)
-    new IOSGleanBridge(uploadEnabled, QString(appChannel).toNSString());
+    new IOSGleanBridge(uploadEnabled, appChannel);
 #elif not(defined(MVPN_WASM) || defined(BUILD_QMAKE))
     glean_initialize(uploadEnabled, dataPath.toLocal8Bit(), appChannel);
 #endif
