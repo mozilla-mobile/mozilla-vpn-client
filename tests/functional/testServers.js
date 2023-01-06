2a2c90e1bdeeed7b838c910cd782d2fe6261676e
Andrea Marchesini <amarchesini@mozilla.com>
2023-01-06 23:38:23 +0100

    Fix the wasm client functional test: testServers.js

--- tests/functional/testServers.js
+++ tests/functional/testServers.js
@@ -137,8 +137,6 @@ describe('Server list', function() {
 
     await vpn.waitForQueryAndClick(
         queries.screenHome.serverListView.BACK_BUTTON.visible());
-    await vpn.waitForQueryAndClick(
-        queries.screenHome.SERVER_LIST_BUTTON.visible());
 
     await vpn.activate();
 
