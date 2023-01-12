3d86e7f31f0cafd0a31d801c3e65c17113b522b6
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2023-01-12 13:29:48 -0500

    retrieve browserstack secrets

--- taskcluster/scripts/appium/upload.sh
+++ taskcluster/scripts/appium/upload.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-curl -u $BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY \
+# Get Secrets for building
+echo "Fetching Tokens!"
+./taskcluster/scripts/get-secret.py -s project/mozillavpn/level-1/browserstack -k user -f user
+./taskcluster/scripts/get-secret.py -s project/mozillavpn/level-1/browserstack -k pass -f pass
+
+curl -u $(cat user):$(cat pass) \
     -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
     -F "file=@//$MOZ_FETCHES_DIR/mozillavpn-arm64-v8a-debug.apk" \
     -F "custom_id=mozillavpn-arm64-v8a-debug"
