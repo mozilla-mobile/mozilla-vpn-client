a7a72d1adf48d66d53b14de31e321df304fbecd0
Owen Kirby <oskirby@gmail.com>
2023-01-12 14:17:08 -0800

    Try setting the PR number for build tasks

--- taskcluster/mozillavpn_taskgraph/transforms/build.py
+++ taskcluster/mozillavpn_taskgraph/transforms/build.py
@@ -67,3 +67,12 @@ def resolve_keys_scope(config, tasks):
             },
         )
         yield task
+
+
+@transforms.add
+def add_pr_number(config, tasks):
+    for task in tasks:
+        if config.params["pull_request_number"]:
+            task["env"]["PULL_REQUEST_NUMBER"] = str(config.params["pull_request_number"])
+
+        yield task
