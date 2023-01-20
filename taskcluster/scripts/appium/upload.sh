032beb0426c99634202bd8c137db206eaa82f4ac
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2023-01-20 10:33:17 -0500

    specify artifact dirs

--- taskcluster/scripts/appium/upload.sh
+++ taskcluster/scripts/appium/upload.sh
@@ -5,7 +5,8 @@ echo "Fetching Tokens!"
 ./taskcluster/scripts/get-secret.py -s project/mozillavpn/level-1/browserstack -k user -f user
 ./taskcluster/scripts/get-secret.py -s project/mozillavpn/level-1/browserstack -k pass -f pass
 
-curl -u $(cat user):$(cat pass) \
+mkdir -p /builds/worker/artifacts/
+
+curl -u -s $(cat user):$(cat pass) \
     -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
-    -F "file=@//$MOZ_FETCHES_DIR/mozillavpn-arm64-v8a-debug.apk" \
-    -F "custom_id=mozillavpn-arm64-v8a-debug"
+    -F "file=@//$MOZ_FETCHES_DIR/mozillavpn-arm64-v8a-debug.apk" > /builds/worker/artifacts/text.txt
