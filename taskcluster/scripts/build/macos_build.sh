e07fe3e3cb529d07f3f0ae4935bc9dc376c19b2e
Sebastian Streich <streich.mobile@gmail.com>
2023-01-24 18:32:25 +0100

    don't use wget?

--- taskcluster/scripts/build/macos_build.sh
+++ taskcluster/scripts/build/macos_build.sh
@@ -34,7 +34,7 @@ export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
 print Y "Installing conda env..."
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ${TASK_HOME}/miniconda
 source ${TASK_HOME}/bin/activate
 
