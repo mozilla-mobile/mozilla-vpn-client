b22137af6f86bf88c766d43a0b851d5f81460c73
Owen Kirby <oskirby@gmail.com>
2023-01-27 16:52:33 -0800

    Enable MacOS universal builds in Taskcluster

--- taskcluster/scripts/build/macos_build.sh
+++ taskcluster/scripts/build/macos_build.sh
@@ -79,10 +79,12 @@ if [[ "$RELEASE" ]]; then
         -DCMAKE_PREFIX_PATH=${MOZ_FETCHES_DIR}/qt_dist/lib/cmake \
         -DSENTRY_DSN=$SENTRY_DSN \
         -DSENTRY_ENVELOPE_ENDPOINT=$SENTRY_ENVELOPE_ENDPOINT \
+        -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
         -DCMAKE_BUILD_TYPE=Release
 else 
     cmake -S . -B ${MOZ_FETCHES_DIR}/build -GNinja \
         -DCMAKE_PREFIX_PATH=${MOZ_FETCHES_DIR}/qt_dist/lib/cmake \
+        -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
         -DCMAKE_BUILD_TYPE=Release
 fi 
 
