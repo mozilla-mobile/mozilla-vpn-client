4acadd09adbac4b3d2b57e875db5ad0e85780221
Beatriz Rizental <beatriz.rizental@gmail.com>
2023-02-02 15:18:17 +0100

    Attempt to install all modules for WASM build

--- taskcluster/scripts/build/wasm.sh
+++ taskcluster/scripts/build/wasm.sh
@@ -13,7 +13,7 @@ pip3 install -r requirements.txt
 
 export PATH="$QTPATH/wasm_32/bin:$PATH"
 mkdir build
-$QTPATH/wasm_32/bin/qt-cmake -S . -B build -DQT_HOST_PATH=/$QTPATH/gcc_64 -DQT_HOST_PATH_CMAKE_DIR=/$QTPATH/gcc_64/lib/cmake
+$QTPATH/wasm_32/bin/qt-cmake -S . -B build -DQT_HOST_PATH=/$QTPATH/gcc_64 -DQT_HOST_PATH_CMAKE_DIR=/$QTPATH/gcc_64/lib/cmake -DQT_WASM_EXTRA_EXPORTED_METHODS=ENV
 cmake --build build -j8
 cp build/src/mozillavpn.wasm wasm
 cp build/src/mozillavpn.js wasm
