ac94c25a32c2bf73ce3e7adda4ee1f51afb3e542
Andrea Marchesini <amarchesini@mozilla.com>
2023-02-06 12:20:05 -0400

    Exclude non-vpn apps when generating strings for translations - VPN-4076

--- scripts/utils/generate_ts.sh
+++ scripts/utils/generate_ts.sh
@@ -29,14 +29,20 @@ printn Y "Generating a dummy PRO file... "
 mkdir -p translations/generated || die
 cat > translations/generated/dummy.pro << EOF
 HEADERS += l18nstrings.h
+HEADERS += \$\$files(../../src/shared/*.h, true)
+HEADERS += \$\$files(../../src/apps/vpn/*.h, true)
+HEADERS += \$\$files(../../nebula/*.h, true)
+
 SOURCES += l18nstrings_p.cpp
 SOURCES += ../l18nstrings.cpp
-TRANSLATIONS += translations.ts
-HEADERS += \$\$files(../../src/*.h, true)
-SOURCES += \$\$files(../../src/*.cpp, true)
-RESOURCES += \$\$files(../../src/*.qrc, true)
-HEADERS += \$\$files(../../nebula/*.h, true)
+SOURCES += \$\$files(../../src/shared/*.cpp, true)
+SOURCES += \$\$files(../../src/apps/vpn/*.cpp, true)
 SOURCES += \$\$files(../../nebula/*.cpp, true)
+
+TRANSLATIONS += translations.ts
+
+RESOURCES += \$\$files(../../src/shared/*.qrc, true)
+RESOURCES += \$\$files(../../src/apps/vpn/*.qrc, true)
 RESOURCES += \$\$files(../../nebula/*.qrc, true)
 EOF
 print G "done"
