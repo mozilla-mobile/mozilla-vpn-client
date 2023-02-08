f67e8962621730ff1fed85a5db0527a7540e942d
Raphael Okafor Jr <94625718+mozrokafor@users.noreply.github.com>
2023-02-08 13:35:27 -0500

    add tests to verify search functionality in serverlist views (#5773)
    * add tests to verify search functionality in serverlist views
    
    * removing additional addon server
    
    * test adjustments
    
    * adding no results and multi results
    
    * verify multi results
    
    * assertion update
    
    * confirming countries are not shown in invalid search

--- tests/functional/helper.js
+++ tests/functional/helper.js
@@ -189,6 +189,11 @@ module.exports = {
     await this.clickOnQuery(id);
   },
 
+  async waitForQueryAndWriteInTextField(id, value){
+    await this.waitForQuery(id)
+    await this.setQueryProperty(id, 'text', value)
+  },
+
   async clickOnNotification() {
     const json = await this._writeCommand('click_notification');
     assert(
