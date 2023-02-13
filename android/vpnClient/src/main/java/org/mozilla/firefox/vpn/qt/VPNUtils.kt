6431dcee4287d0a9ee9edfa75e2e1392542ed7b6
Beatriz Rizental <beatriz.rizental@gmail.com>
2023-02-13 13:50:07 +0100

    Fix Glean on Android

--- android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNUtils.kt
+++ android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNUtils.kt
@@ -16,8 +16,12 @@ import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import java.io.IOException
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
+import java.util.Calendar
+import mozilla.telemetry.glean.config.Configuration
+import mozilla.telemetry.glean.Glean
+import mozilla.telemetry.glean.BuildInfo
 
 // Companion for AndroidUtils.cpp
 object VPNUtils {
@@ -40,11 +44,12 @@ object VPNUtils {
         // Find the right volume to use:
         val collection = MediaStore.Downloads.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
         val dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("y-mm-dd-H-m-ss"))
-        val fileMetaData = ContentValues().apply {
-            put(MediaStore.Downloads.MIME_TYPE, "text/plain")
-            put(MediaStore.Downloads.DISPLAY_NAME, "MozillaVPN_Logs_$dateTime")
-            put(MediaStore.Downloads.IS_PENDING, 1)
-        }
+        val fileMetaData =
+                ContentValues().apply {
+                    put(MediaStore.Downloads.MIME_TYPE, "text/plain")
+                    put(MediaStore.Downloads.DISPLAY_NAME, "MozillaVPN_Logs_$dateTime")
+                    put(MediaStore.Downloads.IS_PENDING, 1)
+                }
         // Create the File and get the URI
         val fileURI: Uri? = resolver.insert(collection, fileMetaData)
         if (fileURI == null) {
@@ -102,11 +107,16 @@ object VPNUtils {
         context.startActivity(intent)
     }
 
-    @SuppressLint("Unused")
+    @SuppressLint("NewApi")
     @JvmStatic
-    private external fun recordGleanEvent(metricName: String)
-
-    @SuppressLint("Unused")
-    @JvmStatic
-    private external fun recordGleanEventWithExtraKeys(metricName: String, keysJSON: String)
+    fun initializeGlean(ctx: Context, isTelemetryEnabled: Boolean, channel: String) {
+        Glean.initialize(
+                applicationContext = ctx.applicationContext,
+                uploadEnabled = isTelemetryEnabled,
+                // GleanBuildInfo can only be generated for application,
+                // We are in a library so we have to build it ourselves.
+                buildInfo = BuildInfo(BuildConfig.VERSIONCODE, BuildConfig.SHORTVERSION, Calendar.getInstance()),
+                configuration = Configuration(channel = channel)
+        )
+    }
 }
