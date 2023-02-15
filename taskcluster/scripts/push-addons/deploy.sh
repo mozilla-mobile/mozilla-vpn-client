b81b33724867fef6319f23f245c2e57ef251b692
Sarah Bird <birdsarah@mozilla.com>
2023-02-15 09:39:32 -0600

    Update deploy.sh to match too

--- taskcluster/scripts/push-addons/deploy.sh
+++ taskcluster/scripts/push-addons/deploy.sh
@@ -5,15 +5,14 @@
 
 set -e
 
-git submodule sync --recursive
-git submodule update --init --force --recursive --depth=1
 # Reqs
+git submodule update --init --recursive --depth=1
 pip3 install -r requirements.txt
 
 # Get Secrets for building
 echo "Fetching Tokens!"
 ./taskcluster/scripts/get-secret.py -s project/mozillavpn/tokens -k addons_key -f addons_key.pem
-ls 
+ls
 cd /builds/worker/fetches/
 ls
 openssl dgst -sha256 -sign /builds/worker/checkouts/vcs/addons_key.pem -out addons/manifest.json.sig addons/manifest.json
