ebfc807597ad3df8704639bfc0f1235783321cfb
Sarah Bird <birdsarah@mozilla.com>
2023-02-23 21:34:12 -0600

    Explicitly listen on LocalHost IPv4 and IPv6

--- src/apps/vpn/tasks/authenticate/desktopauthenticationlistener.cpp
+++ src/apps/vpn/tasks/authenticate/desktopauthenticationlistener.cpp
@@ -21,7 +21,7 @@ DesktopAuthenticationListener::DesktopAuthenticationListener(QObject* parent)
     : AuthenticationListener(parent) {
   MZ_COUNT_CTOR(DesktopAuthenticationListener);
 
-  m_server = new QOAuthHttpServerReplyHandler(QHostAddress::Any, this);
+  m_server = new QOAuthHttpServerReplyHandler(this);
   connect(m_server, &QAbstractOAuthReplyHandler::callbackReceived, this,
           [this](const QVariantMap& values) {
             logger.debug() << "DesktopAuthenticationListener data received";
@@ -53,7 +53,8 @@ void DesktopAuthenticationListener::start(Task* task,
                                    emailAddress));
 
   if (!m_server->isListening()) {
-    m_server->listen(QHostAddress::Any);
+    m_server->listen(QHostAddress::LocalHost);
+    m_server->listen(QHostAddress::LocalHostIPv6);
   }
 
   if (!m_server->isListening()) {
