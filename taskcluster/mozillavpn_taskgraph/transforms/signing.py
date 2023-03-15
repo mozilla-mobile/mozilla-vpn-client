mozilla-releng/staging-mozilla-vpn-client

ci: turn linux signing tasks back on
For now this also removes Linux from production signing, so we'll only
use the 'dep' key. Once we prove out some infrastructure for testing and
are confident that the tasks are working as expected, we can re-enable
production signing.

taskcluster/mozillavpn_taskgraph/transforms/signing.py
@@ -17,7 +17,6 @@ PRODUCTION_SIGNING_BUILD_TYPES = [
     "android-x86/release",
     "android-arm64/release",
     "android-armv7/release",
-    "linux/opt",
     "macos/opt",
     "windows/opt",
     "addons/opt",
@@ -28,6 +27,7 @@ SIGNING_BUILD_TYPES = PRODUCTION_SIGNING_BUILD_TYPES + [
     # android builds. Contact releng if you need it :)
     # "android-debug",
     # "addons/opt",  # TODO: Add addons debug builds? We have the infra to debug sign them.
+    "linux/source",  # Needs testing / validation
 ]
 
 

mozilla-mobile/mozilla-vpn-client

taskcluster/mozillavpn_taskgraph/transforms/signing.py
@@ -17,7 +17,6 @@ PRODUCTION_SIGNING_BUILD_TYPES = [
     "android-x86/release",
     "android-arm64/release",
     "android-armv7/release",
-    "linux/opt",
     "macos/opt",
     "windows/opt",
     "addons/opt",
@@ -28,6 +27,7 @@ SIGNING_BUILD_TYPES = PRODUCTION_SIGNING_BUILD_TYPES + [
     # android builds. Contact releng if you need it :)
     # "android-debug",
     # "addons/opt",  # TODO: Add addons debug builds? We have the infra to debug sign them.
+    "source/vpn",  # Needs testing / validation
 ]
 
 