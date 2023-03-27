fa6ee4ec3b60f065c12adc10cccdee065c4385b4
Gela <gmalekpour@mozilla.com>
2023-03-26 19:56:27 -0700

    Add connection for controller in singleton

--- src/apps/vpn/subscriptionmonitor.cpp
+++ src/apps/vpn/subscriptionmonitor.cpp
@@ -8,12 +8,16 @@
 #include <QMetaMethod>
 #include <QMetaObject>
 
+#include "appconstants.h"
+#include "controller.h"
 #include "dnshelper.h"
 #include "leakdetector.h"
 #include "logger.h"
 #include "mozillavpn.h"
+#include "networkrequest.h"
 #include "settingsholder.h"
 #include "tasks/controlleraction/taskcontrolleraction.h"
+#include "tasks/function/taskfunction.h"
 #include "taskscheduler.h"
 
 namespace {
@@ -29,46 +33,40 @@ Logger logger("SubscriptionMonitor");
 SubscriptionMonitor::SubscriptionMonitor(QObject* parent) : QObject(parent) {
   MZ_COUNT_CTOR(SubscriptionMonitor);
 
-//   SettingsHolder* settingsHolder = SettingsHolder::instance();
+/**
+ * Here we need to setup 2 connections:
+ * connection health: to monitor no signal state)
+ * controller: to monitor if/when user turns their VPN off 
+ *             so we can ping guardian and then show the sub expired page.
+ */
+ 
 
-// #define CONNECT(x)                                  \
-//   connect(settingsHolder, &SettingsHolder::x, this, \
-//           &SubscriptionMonitor::maybeServerSwitch);
+  connect(MozillaVPN::instance()->controller(), &Controller::stateChanged, this,
+          [this]() {
+            Controller::State state =
+                MozillaVPN::instance()->controller()->state();
+            // m_operationRunning is set to true when the Controller is in
+            // StateOn. So, if we see a change, it means that the new settings
+            // values have been taken in consideration. We are ready to
+            // schedule a new TaskControllerAction if needed.
+            if (state == Controller::StateOff) {
+              logger.debug() << "User has toggled the VPN off";
+              m_operationRunning = false;
 
-//   CONNECT(captivePortalAlertChanged);
-//   CONNECT(protectSelectedAppsChanged);
-//   CONNECT(vpnDisabledAppsChanged);
+              // Check the subscription status
+              // MozillaVPN::instance()->controller()->setState(Controller::StateCheckSubscription);
+              TaskFunction* task = new TaskFunction([]() {});
+              NetworkRequest* request = new NetworkRequest(task, 200);
+              request->auth(MozillaVPN::authorizationHeader());
+              request->get(AppConstants::apiUrl(AppConstants::Account));
 
-// #undef CONNECT
-
-// #define DNS_CONNECT(x)                                                  \
-//   connect(settingsHolder, &SettingsHolder::x, this, [this]() {          \
-//     SettingsHolder* settingsHolder = SettingsHolder::instance();        \
-//     if (settingsHolder->dnsProviderFlags() != SettingsHolder::Custom || \
-//         DNSHelper::validateUserDNS(settingsHolder->userDNS())) {        \
-//       maybeServerSwitch();                                              \
-//     }                                                                   \
-//   });
-
-//   DNS_CONNECT(dnsProviderFlagsChanged);
-//   DNS_CONNECT(userDNSChanged);
-
-// #undef DNS_CONNECT
-
-//   connect(MozillaVPN::instance()->controller(), &Controller::stateChanged, this,
-//           [this]() {
-//             Controller::State state =
-//                 MozillaVPN::instance()->controller()->state();
-//             // m_operationRunning is set to true when the Controller is in
-//             // StateOn. So, if we see a change, it means that the new settings
-//             // values have been taken in consideration. We are ready to
-//             // schedule a new TaskControllerAction if needed.
-//             if (state != Controller::StateOn && state != Controller::StateOff &&
-//                 m_operationRunning) {
-//               logger.debug() << "Resetting the operation running state";
-//               m_operationRunning = false;
-//             }
-//           });
+              // connect(request, &NetworkRequest::requestFailed, this,
+              //         [this](QNetworkReply::NetworkError error, const QByteArray&) {
+              //           logger.error() << "Account request failed" << error;
+              //           REPORTNETWORKERROR(error, ErrorHandler::DoNotPropagateError,
+              //                             "PreActivationSubscriptionCheck");
+            }
+          });
 }
 
 SubscriptionMonitor::~SubscriptionMonitor() { MZ_COUNT_DTOR(SubscriptionMonitor); }
@@ -81,21 +79,5 @@ SubscriptionMonitor* SubscriptionMonitor::instance() {
   }
   return s_instance;
 }
-
-// void SubscriptionMonitor::maybeServerSwitch() {
-//   logger.debug() << "Settings changed!";
-
-//   if (MozillaVPN::instance()->controller()->state() != Controller::StateOn ||
-//       m_operationRunning) {
-//     return;
-//   }
-
-//   m_operationRunning = true;
-
-//   TaskScheduler::deleteTasks();
-//   TaskScheduler::scheduleTask(
-//       new TaskControllerAction(TaskControllerAction::eSilentSwitch,
-//                                TaskControllerAction::eServerCoolDownNotNeeded));
-// }
 
 void SubscriptionMonitor::operationCompleted() { m_operationRunning = false; }
