81b9ffae0a0e86771085731fa6a31897b2c3ea3e
Gela <gmalekpour@mozilla.com>
2023-03-27 19:35:41 -0700

    fixup! Fix functional tests

--- tests/functional/testServerSearch.js
+++ tests/functional/testServerSearch.js
@@ -181,7 +181,7 @@ describe("Server list", function () {
     assert.strictEqual(vpn.lastNotification().title, "VPN Connected");
     assert.strictEqual(
       vpn.lastNotification().message,
-      `Connected to ${currentCity}`
+      `Connected through ${currentCity}`
     );
   });
 
