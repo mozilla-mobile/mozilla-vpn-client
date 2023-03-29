3d24dc599f075cbc084e994fccb19780f2a98ea4
Matt Lichtenstein <mlichtenstein@mozilla.com>
2023-03-29 13:14:30 -0400

    add feature signal watcher condition

--- addons/tutorial_01_get_started/condition.js
+++ addons/tutorial_01_get_started/condition.js
@@ -1,4 +1,12 @@
 (function(api, condition) {
+    api.connectSignal(api.featureList.get('recommendedServers'), 'supportedChanged', () => {
+        if(!api.featureList.get('recommendedServers').isSupported) {
+            condition.enable()
+            return
+        }
+        condition.disable()
+      });
+
     if(!api.featureList.get('recommendedServers').isSupported) {
         condition.enable()
         return
