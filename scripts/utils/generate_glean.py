7c5d98a9cb5ecc4818fb433759009faf9815b43d
Andrea Marchesini <amarchesini@mozilla.com>
2023-03-31 15:20:52 +0200

    Unify glean samples cross projects (#6523)

--- scripts/utils/generate_glean.py
+++ scripts/utils/generate_glean.py
@@ -5,7 +5,6 @@
 
 import sys
 import os.path
-import yaml
 import subprocess
 import argparse
 
@@ -32,17 +31,35 @@ def camelize(string):
 
 ## Find the input Yaml files relative to the script location.
 rootpath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-metrics = os.path.join(rootpath, 'glean', 'metrics.yaml')
-pings = os.path.join(rootpath, 'glean', 'pings.yaml')
 
-yaml_file = open(metrics, 'r', encoding='utf-8')
-yaml_content = yaml.safe_load(yaml_file)
+pings_files = []
+metrics_files = []
+for project in os.listdir(os.path.join(rootpath, "src", "apps")):
+    telemetry_path = os.path.join(rootpath, "src", "apps", project, "telemetry")
+    if os.path.isdir(telemetry_path):
+        pings_file = os.path.join(telemetry_path, "pings_deprecated.yaml")
+        if os.path.isfile(pings_file):
+            pings_files.append(pings_file)
 
+        metrics_file = os.path.join(telemetry_path, "metrics_deprecated.yaml")
+        if os.path.isfile(metrics_file):
+            metrics_files.append(metrics_file)
 
-print("Parsing the metrics.yaml file...")
-if not os.path.isfile(metrics):
-    print(f'Unable to find {metrics}')
-    exit(1)
+telemetry_path = os.path.join(rootpath, "src", "shared", "telemetry")
+if os.path.isdir(telemetry_path):
+    pings_file = os.path.join(telemetry_path, "pings_deprecated.yaml")
+    if os.path.isfile(pings_file):
+        pings_files.append(pings_file)
+
+    metrics_file = os.path.join(telemetry_path, "metrics_deprecated.yaml")
+    if os.path.isfile(metrics_file):
+        metrics_files.append(metrics_file)
+
+if not metrics_files:
+    exit("No metrics found!")
+
+if not pings_files:
+    exit("No pings found!")
 
 ## Create the output directory if it doesn't already exist.
 os.makedirs(args.output, exist_ok=True)
@@ -62,9 +79,9 @@ output.close()
 
 print("Generating the JS modules...")
 try:
-    subprocess.call([sys.executable, "-m", "glean_parser", "translate", metrics, pings,
-                     "-f", "javascript", "-o", args.output, "--option", "platform=qt",
-                     "--option", "version=0.30"])
+    subprocess.call([sys.executable, "-m", "glean_parser", "translate"] + metrics_files + pings_files +
+                    ["-f", "javascript", "-o", args.output, "--option", "platform=qt",
+                    "--option", "version=0.30"])
 except Exception as e:
     print("glean_parser failed. Is it installed? Try with:\n\tpip3 install -r requirements.txt --user")
     print(e)
