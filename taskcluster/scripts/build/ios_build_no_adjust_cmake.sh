f57abfc38c38ee54af05203e689e805aa055a8bb
brizental <beatriz.rizental@gmail.com>
2023-03-31 12:03:26 +0200

    Bring back the qmake build

--- taskcluster/scripts/build/ios_build_no_adjust_cmake.sh
+++ taskcluster/scripts/build/ios_build_no_adjust_cmake.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-set -e
+set -xe
 
 
 . $(dirname $0)/../../../scripts/utils/commons.sh
@@ -15,18 +15,14 @@ print N ""
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
-print Y "Installing ruby dependencies..."
-# use --user-install for permissions
-gem install xcodeproj --user-install || die
-export PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
-
 print Y "Installing rust..."
 curl https://sh.rustup.rs -sSf | sh -s -- -y || die
 export PATH="$HOME/.cargo/bin:$PATH"
+rustup target add aarch64-apple-ios
 
 print Y "Installing go..."
-curl -O https://dl.google.com/go/go1.17.6.darwin-amd64.tar.gz
-tar -xzf go1.17.6.darwin-amd64.tar.gz
+curl -O https://dl.google.com/go/go1.18.10.darwin-amd64.tar.gz
+tar -xzf go1.18.10.darwin-amd64.tar.gz
 export PATH="`pwd`/go/bin:$PATH"
 
 print Y "Installing python dependencies..."
@@ -34,26 +30,23 @@ print Y "Installing python dependencies..."
 python3 -m pip install -r requirements.txt --user
 export PYTHONIOENCODING="UTF-8"
 
-
 print Y "Installing QT..."
 PROJECT_HOME=`pwd`
-QTVERSION=$(ls ../../fetches/qt_ios)
+QTVERSION=$(ls ${MOZ_FETCHES_DIR}/qt_ios)
 echo "Using QT:$QTVERSION"
 
-cd ../../fetches/qt_ios/$QTVERSION/ios || die
-export QT_IOS_BIN=`pwd`/bin
-export PATH=$QT_IOS_BIN:$PATH
+print Y "Installing Homebrew..."
+# We have to install it locally like this because of Taskcluster permission policies.
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+export PATH=$PATH:$(pwd)/homebrew/bin
 
-cd $PROJECT_HOME
-# So ios qmake is just a wrapper script
-# and expects to find pwd/qt_ios/mac/bin/qmake >:c
-ln -s ../../fetches/qt_ios/ qt_ios
+print Y "Installing cmake..."
+brew install cmake
+
+export PATH=$PATH:$GOROOT/bin
 
 print Y "Get the submodules..."
 git submodule update --init --depth 1 || die "Failed to init submodules"
-# Technically we do not need the following line because we later call the
-# apple compile script which calls import languages. However when we move to cmake
-# for iOS we can remove the call to import languages and this step will be necessary
 for i in src/apps/*/translations/i18n; do
   git submodule update --remote $i || die "Failed to pull latest i18n from remote ($i)"
 done
@@ -70,16 +63,22 @@ APP_ID_IOS = org.mozilla.ios.FirefoxVPN
 NETEXT_ID_IOS = org.mozilla.ios.FirefoxVPN.network-extension
 EOF
 
-# NOTE: In case we want to release this, we need to get that token, see android-release.sh
-./scripts/macos/apple_compile.sh ios -q ../../fetches/qt_ios/$QTVERSION/macos/bin -a ReallyNotAnAPIToken || die
+print Y "Building xcodeproj..."
+$MOZ_FETCHES_DIR/qt_ios/$QTVERSION/ios/bin/qt-cmake -S . -B $MOZ_FETCHES_DIR/build -GXcode \
+  -DQT_HOST_PATH="$MOZ_FETCHES_DIR/qt_ios/$QTVERSION/macos" \
+  -DCMAKE_PREFIX_PATH=$MOZ_FETCHES_DIR/qt_ios/lib/cmake \
+  -DCMAKE_OSX_ARCHITECTURES="arm64" \
+  -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="" \
+  -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED="NO" \
+  -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED="NO" \
+  -DCMAKE_BUILD_TYPE=Release
 
 print Y "Compiling..."
-# TODO: apple_compile.sh ios generated "MozillaVPN.xcodeproj"; the mac one generated "Mozilla VPN.xcodeproj"
-xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO -project MozillaVPN.xcodeproj || die
+cmake --build $MOZ_FETCHES_DIR/build --config Release || die
 
 print Y "Exporting the artifact..."
 mkdir -p tmp || die
-cp -r Release-iphoneos/* tmp || die
+cp -r $MOZ_FETCHES_DIR/build/build/mozillavpn.build/Release-iphoneos/* tmp || die
 cd tmp || die
 
 # From checkout dir to actual task base directory
