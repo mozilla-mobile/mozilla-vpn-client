e559ffd258606067c7eab2b7151ea088a4d32882
Sebastian Streich <strseb@users.noreply.github.com>
2023-04-11 14:58:44 +0200

    VPN-4476 - Fix share logs on 2.15 (#6574)
    * VPN-4476 - Fix share logs on 2.15
    
    * lint

--- src/shared/platforms/android/androidcommons.cpp
+++ src/shared/platforms/android/androidcommons.cpp
@@ -61,8 +61,7 @@ bool AndroidCommons::verifySignature(const QByteArray& publicKey,
 // static
 bool AndroidCommons::shareText(const QString& text) {
   return (bool)QJniObject::callStaticMethod<jboolean>(
-      UTILS_CLASS, "sharePlainText",
-      "(Landroid/content/Context;Ljava/lang/String;)Z", getActivity().object(),
+      UTILS_CLASS, "sharePlainText", "(Ljava/lang/String;)Z",
       QJniObject::fromString(text).object());
 }
 
