6e26547814af67acd4ff0b819b4ca659cdfbce64
Naomi Kirby <oskirby@gmail.com>
2023-04-20 14:50:25 -0700

    Attempt universal building with Conda

--- taskcluster/scripts/build/macos_build.sh
+++ taskcluster/scripts/build/macos_build.sh
@@ -100,7 +100,8 @@ cmake -S . -B ${MOZ_FETCHES_DIR}/build -GNinja \
         -DCMAKE_PREFIX_PATH=${MOZ_FETCHES_DIR}/qt_dist/lib/cmake \
         -DSENTRY_DSN=$SENTRY_DSN \
         -DSENTRY_ENVELOPE_ENDPOINT=$SENTRY_ENVELOPE_ENDPOINT \
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
 
 
 print Y "Building the client..."
