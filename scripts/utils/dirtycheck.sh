4ee42499cc2ed44b73a03fb140d186eba9798192
Naomi Kirby <oskirby@gmail.com>
2023-05-08 11:31:09 -0700

    Check submodules too

--- scripts/utils/dirtycheck.sh
+++ scripts/utils/dirtycheck.sh
@@ -10,3 +10,8 @@ GITDIR=$(cd $(dirname $0) && git rev-parse --show-toplevel)
 # List all untracked and modified files and print them to stderr
 # We permit the build directories to exist as per CMake build instructions.
 git -C $GITDIR ls-files -m -o -x '.tmp/' -x 'build/' -x 'build-*/' >&2
+
+## Do the same for submodules.
+for SUBMODULE in $(git submodule foreach --recursive --quiet 'echo $sm_path'); do
+    git -C $GITDIR/$SUBMODULE ls-files -m -o | xargs -d '\n' --replace=filename echo "$SUBMODULE/filename" >&2
+done
