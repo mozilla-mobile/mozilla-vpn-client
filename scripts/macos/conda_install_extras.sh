3c2bb598a49b30eff50bb53992317e937c776512
Sebastian Streich <streich.mobile@gmail.com>
2023-05-09 14:50:39 +0200

    fix channel installatioN

--- scripts/macos/conda_install_extras.sh
+++ scripts/macos/conda_install_extras.sh
@@ -4,4 +4,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-conda install -y cctools=973.0.1
+conda install -y -c conda-forge cctools=973.0.1
