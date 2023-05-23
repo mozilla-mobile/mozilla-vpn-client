e5d81def26576aa136f04ce42bdeff096cd14613
Naomi Kirby <oskirby@gmail.com>
2023-05-23 07:20:12 -0700

    VPN-4291: Build addons with CMake (#6832)
    * Initial addon support in CMake
    * Convert addons to a standalone project for easier building
    * Build addon bundle on Takscluster with CMake
    * Support addon builds on CMake < 3.20 with a little bit of python
    * Fix addon build on Windows
    * Build test addons with CMake too
    * PROD_ADDONS_PATH is no longer used, remove it
    * Update addon.js server for flattened test addons dir
    * Build functional test addons once and share with test runners
    * Remove generate_all.py addon script
    * Ignore build directories from clang linter
    * Add better messaging when test addons need generation

---
+++ scripts/cmake/addons.cmake
@@ -0,0 +1,96 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+get_filename_component(ADDON_SCRIPT_DIR ${CMAKE_CURRENT_LIST_DIR}/../addon REALPATH)
+
+### Helper function to build a VPN Addon
+#
+# This function takes one mandatory argument: ADDON_DIR which provides
+# the path to the location where the addon's manifest.json can be found.
+#
+function(add_addon_target NAME)
+    cmake_parse_arguments(ADDON
+        ""
+        "SOURCE_DIR;OUTPUT_DIR;I18N_DIR"
+        "SOURCES"
+        ${ARGN})
+
+    # Fix Ninja dependency tracking when dealing with absolute paths. 
+    cmake_policy(PUSH)
+    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.20)
+        cmake_policy(SET CMP0116 NEW)
+    endif()
+
+    if(NOT ADDON_OUTPUT_DIR)
+        set(ADDON_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
+    else()
+        file(MAKE_DIRECTORY ${ADDON_OUTPUT_DIR})
+    endif()
+
+    # Get the path to the Qt host tools.
+    get_target_property(QT_QMLLINT_EXECUTABLE Qt6::qmllint LOCATION)
+    get_filename_component(QT_TOOL_BIN_PATH ${QT_QMLLINT_EXECUTABLE} PATH)
+    get_target_property(QT_RCC_EXECUTABLE Qt6::rcc LOCATION)
+    get_filename_component(QT_TOOL_LIBEXEC_PATH ${QT_RCC_EXECUTABLE} PATH)
+
+    # Scan for addon manifests if a source directory was provided.
+    if(ADDON_SOURCE_DIR)
+        file(GLOB_RECURSE SCANNED_MANIFEST_FILES CONFIGURE_DEPENDS ${ADDON_SOURCE_DIR}/manifest.json)
+        foreach(FILENAME ${SCANNED_MANIFEST_FILES})
+            if(${FILENAME} MATCHES "generated")
+                continue()
+            endif()
+            list(APPEND ADDON_SOURCES ${FILENAME})
+        endforeach()
+    endif()
+
+    # Prepare some common addon build arguments.
+    set(ADDON_BUILD_ARGS -q ${QT_TOOL_BIN_PATH} -q ${QT_TOOL_LIBEXEC_PATH})
+    if (ADDON_I18N_DIR)
+        list(APPEND ADDON_BUILD_ARGS -i ${ADDON_I18N_DIR})
+    endif()
+
+    # Add commands to build the addons
+    foreach(MANIFEST_FILE ${ADDON_SOURCES})
+        # Parse the manifest to get the addon ID.
+        execute_process(OUTPUT_VARIABLE ADDON_ID OUTPUT_STRIP_TRAILING_WHITESPACE
+            COMMAND ${PYTHON_EXECUTABLE} -c "import json; print(json.load(open('${MANIFEST_FILE}', encoding='utf-8'))['id'])")
+
+        if((CMAKE_GENERATOR MATCHES "Ninja") OR
+           (CMAKE_GENERATOR MATCHES "Makefiles" AND (CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)))
+            ## Depfiles are great, but they only work for some generators.
+            file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_deps)
+            add_custom_command(
+                OUTPUT ${ADDON_OUTPUT_DIR}/${ADDON_ID}.rcc
+                DEPENDS ${MANIFEST_FILE}
+                DEPFILE ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_deps/${ADDON_ID}.d
+                COMMAND ${PYTHON_EXECUTABLE} ${ADDON_SCRIPT_DIR}/build.py
+                            -d ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_deps/${ADDON_ID}.d
+                            ${ADDON_BUILD_ARGS} ${MANIFEST_FILE} ${ADDON_OUTPUT_DIR}
+            )
+        else()
+            add_custom_command(
+                OUTPUT ${ADDON_OUTPUT_DIR}/${ADDON_ID}.rcc
+                DEPENDS ${MANIFEST_FILE}
+                COMMAND ${PYTHON_EXECUTABLE} ${ADDON_SCRIPT_DIR}/build.py
+                            ${ADDON_BUILD_ARGS} ${MANIFEST_FILE} ${ADDON_OUTPUT_DIR}
+            )
+        endif()
+        list(APPEND ADDON_OUTPUT_FILES ${ADDON_OUTPUT_DIR}/${ADDON_ID}.rcc)
+    endforeach()
+
+    ## Reset our policy changes
+    cmake_policy(POP)
+
+    # Add a command to generate the addon index.
+    add_custom_command(
+        OUTPUT ${ADDON_OUTPUT_DIR}/manifest.json
+        DEPENDS ${ADDON_OUTPUT_FILES}
+        COMMAND ${PYTHON_EXECUTABLE} ${ADDON_SCRIPT_DIR}/index.py
+                    -o ${ADDON_OUTPUT_DIR}/manifest.json ${ADDON_OUTPUT_FILES}
+    )
+
+    # Add a custom target to gather the dependencies together.
+    add_custom_target(${NAME} DEPENDS ${ADDON_OUTPUT_FILES} ${ADDON_OUTPUT_DIR}/manifest.json)
+endfunction(add_addon_target)
