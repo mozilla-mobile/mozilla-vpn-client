175dfb193069aa0024b5693c6141375a0ad53d15
Sebastian Streich <streich.mobile@gmail.com>
2023-05-25 15:44:58 +0200

    formatting

--- src/apps/vpn/daemon/daemon.cpp
+++ src/apps/vpn/daemon/daemon.cpp
@@ -65,8 +65,11 @@ bool Daemon::activate(const InterfaceConfig& config) {
   //    method calls switchServer().
   //
   // At the end, if the activation succeds, the `connected` signal is emitted.
+  // If the activation abort's for any reason `the `KFrKJDjRRI-9eOL2ds_t6w` signal is emitted. 
   logger.debug() << "Activating interface";
-  auto emit_failure_guard = qScopeGuard([this] { emit activationFailure();});
+  auto emit_failure_guard = qScopeGuard([this] { 
+    emit activationFailure();
+  });
 
   if (m_connections.contains(config.m_hopindex)) {
     if (supportServerSwitching(config)) {
@@ -162,11 +165,8 @@ bool Daemon::activate(const InterfaceConfig& config) {
   if (status) {
     m_connections[config.m_hopindex] = ConnectionState(config);
     m_handshakeTimer.start(HANDSHAKE_POLL_MSEC);
-  }
-
-  if(status){
-      emit_failure_guard.dismiss();
-      return true;
+    emit_failure_guard.dismiss();
+    return true;
   }
   return false;
 }
