b251fa486d447c7529cec52507df2d0f56a7f478
brizental <beatriz.rizental@gmail.com>
2023-06-06 14:51:44 +0200

    Sync telemetry enabled value with iOS Network Extension

--- src/shared/platforms/ios/iosgleanbridge.swift
+++ src/shared/platforms/ios/iosgleanbridge.swift
@@ -6,19 +6,49 @@ import IOSGlean
 
 public class IOSGleanBridgeImpl : NSObject {
   @objc init(telemetryEnabled isTelemetryEnabled: Bool, channel appChannel: String) {
-    Glean.shared.registerPings(GleanMetrics.Pings.self)
-    Glean.shared.initialize(
-      uploadEnabled: isTelemetryEnabled,
-      configuration: Configuration.init(channel: appChannel),
-      buildInfo: GleanMetrics.GleanBuild.info
+      super.init()
+      
+      Glean.shared.registerPings(GleanMetrics.Pings.self)
+      Glean.shared.initialize(
+          uploadEnabled: isTelemetryEnabled,
+          configuration: Configuration.init(channel: appChannel),
+          buildInfo: GleanMetrics.GleanBuild.info
       )
-
-    // This logs an error like so:
-    // "[User Defaults] Couldn't read values in CFPrefsPlistSource<0x2821ced00>..."
-    // This is just a warning and can be ignored.
-    let defaults = UserDefaults(suiteName: Constants.appGroupIdentifier)
-    defaults!.set(isTelemetryEnabled, forKey: Constants.UserDefaultKeys.telemetryEnabled)
-    defaults!.set(appChannel, forKey: Constants.UserDefaultKeys.appChannel)
-    defaults!.synchronize()
+      
+      withDefaults { defaults in
+          defaults.set(isTelemetryEnabled, forKey: Constants.UserDefaultKeys.telemetryEnabled)
+          defaults.set(appChannel, forKey: Constants.UserDefaultKeys.appChannel)
+          defaults.synchronize()
+      }
   }
+
+  @objc func syncTelemetryEnabled(telemetryEnabled isTelemetryEnabled: Bool) {
+      withDefaults { defaults in
+          defaults.set(isTelemetryEnabled, forKey: Constants.UserDefaultKeys.telemetryEnabled)
+      }
+      
+      do {
+            Logger.global?.log(message: "Sending telemetry state change message.");
+          try TunnelManager.session!.sendProviderMessage(
+              TunnelMessage.telemetryEnabledChanged(isTelemetryEnabled).encode()
+          ) { _ in
+              Logger.global?.log(message: "Telemetry state change message sent.")
+          }
+      } catch {
+          Logger.global?.log(message: "Error sending telemetry state change message: \(error)");
+      }
+  }
+    
+    private func withDefaults(_ f: (_ defaults: UserDefaults) -> Void) {
+        // This logs an error like so:
+        // "[User Defaults] Couldn't read values in CFPrefsPlistSource<0x2821ced00>..."
+        // This is just a warning and can be ignored.
+        let defaults = UserDefaults(suiteName: Constants.appGroupIdentifier)
+        if (defaults == nil) {
+            Logger.global?.log(message: "Attempted to access UserDefaults, but it's not available.")
+            return
+        }
+        
+        f(defaults!)
+    }
 }
