e7716a066dfdd0bd9f1b5f885335cbc02130f5aa
Sebastian Streich <streich.mobile@gmail.com>
2023-06-20 17:13:44 +0200

    first attempt, packin

---
+++ taskcluster/scripts/toolchain/conda_pack_windows.ps1
@@ -0,0 +1,39 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+$REPO_ROOT_PATH =resolve-path "$PSScriptRoot/../../../"
+$TASK_WORKDIR =resolve-path "$REPO_ROOT_PATH/../../"
+$FETCHES_PATH =resolve-path "$TASK_WORKDIR/fetches"
+
+New-Item -ItemType Directory -Force -Path "$TASK_WORKDIR/conda"
+$CONDA_DIR =resolve-path "$TASK_WORKDIR/conda"
+# Prep Env:
+
+## Install MiniConda 
+Start-Process "$FETCHES_PATH/miniconda_installer.exe" -Wait -ArgumentList @('/S',"/D=$CONDA_DIR")
+# We don't have the ability to do conda init - as that need's admin rights.
+# So let's just do that ourselves :3
+$Env:CONDA_EXE = "$CONDA_DIR\Scripts\conda.exe"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "$CONDA_DIR"
+$Env:_CONDA_EXE = "$CONDA_DIR\Scripts\conda.exe"
+$CondaModuleArgs = @{ChangePs1 = $False}
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
+
+$env:PATH ="$CONDA_DIR;$FETCHES_PATH;$QTPATH;$env:PATH"
+
+## Conda is now ready - let's enable the env
+conda env create --force -f $REPO_ROOT_PATH/env.yml -n VPN
+
+conda info --envs
+
+conda activate VPN
+. "$REPO_ROOT_PATH\scripts\windows\conda_setup_win_sdk.ps1" # <- This download's all sdk things we need :3 
+. "$REPO_ROOT_PATH\scripts\windows\conda_install_extras.ps1" # <- Downloads gcc. 
+conda deactivate
+conda activate VPN
+
+conda install conda-pack
+conda-pack --name VPN -o $UPLOAD_DIR/conda-windows.tar.gz
