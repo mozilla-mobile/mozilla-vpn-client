6919f9d7a3ee43a3b4932b73e4be2b00c14c4da1
brizental <beatriz.rizental@gmail.com>
2023-06-23 12:28:14 +0200

    Set the values for the Info.plist file for IOSGlean

--- src/shared/platforms/ios/iosgleanbridge.swift
+++ src/shared/platforms/ios/iosgleanbridge.swift
@@ -13,39 +13,4 @@ public class IOSGleanBridgeImpl : NSObject {
       buildInfo: GleanMetrics.GleanBuild.info
       )
   }
-<<<<<<< HEAD
-=======
-
-  @objc func syncTelemetryEnabled(telemetryEnabled isTelemetryEnabled: Bool) {
-      withDefaults { defaults in
-          defaults.set(isTelemetryEnabled, forKey: Constants.UserDefaultKeys.telemetryEnabled)
-      }
-      
-      if let session = TunnelManager.session {
-        do {
-            Logger.global?.log(message: "Attempting to send telemetry state change message.");
-          try session.sendProviderMessage(
-              TunnelMessage.telemetryEnabledChanged(isTelemetryEnabled).encode()
-          ) { _ in
-              Logger.global?.log(message: "Telemetry state change message sent.")
-          }
-        } catch {
-            Logger.global?.log(message: "Error sending telemetry state change message: \(error)");
-        }
-      }
-  }
-
-    private func withDefaults(_ f: (_ defaults: UserDefaults) -> Void) {
-        // This logs an error like so:
-        // "[User Defaults] Couldn't read values in CFPrefsPlistSource<0x2821ced00>..."
-        // This is just a warning and can be ignored.
-        let defaults = UserDefaults(suiteName: Constants.appGroupIdentifier)
-        if (defaults == nil) {
-            Logger.global?.log(message: "Attempted to access UserDefaults, but it's not available.")
-            return
-        }
-        
-        f(defaults!)
-    }
->>>>>>> 6b1373492 (VPN-5035 - Always execute TunnelManager.initialize completion handler (#7256) (#7281))
 }
