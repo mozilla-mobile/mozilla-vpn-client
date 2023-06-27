c78284aaaaeb39274e9da3ae07eeea1a60fbb1c0
Sebastian Streich <streich.mobile@gmail.com>
2023-06-27 16:29:13 +0200

    Ohhh okay parcour

--- src/shared/cmake/sentry.cmake
+++ src/shared/cmake/sentry.cmake
@@ -90,8 +90,8 @@ if( ${_SUPPORTED} GREATER -1 )
     elseif(LINUX)
         target_compile_definitions(shared-sources INTERFACE SENTRY_BUILD_STATIC)
         # Compile Static for apple and link to libsentry.a
-        target_link_libraries(shared-sources INTERFACE libsentry.a)
-        target_link_libraries(shared-sources INTERFACE breakpad_client.a)
+        target_link_libraries(shared-sources INTERFACE libsentry)
+        target_link_libraries(shared-sources INTERFACE breakpad_client)
         # We are using breakpad as a backend - in process stackwalking is never the best option ... however!
         # this is super easy to link against and we do not need another binary shipped with the client.
         SET(SENTRY_ARGS
