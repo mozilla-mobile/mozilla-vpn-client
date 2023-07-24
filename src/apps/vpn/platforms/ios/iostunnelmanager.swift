9799c21f81a2437f9a39effde530f2c66aca3d81
MCleinman <9295855+mcleinman@users.noreply.github.com>
2023-07-24 11:38:02 -0700

    Refactor some Swift in iOS Tunnel Manager (#7566)
    * remove some force unwraps, make more swift-y
    
    * remove fatalError

--- src/apps/vpn/platforms/ios/iostunnelmanager.swift
+++ src/apps/vpn/platforms/ios/iostunnelmanager.swift
@@ -5,6 +5,10 @@
 import Foundation
 import NetworkExtension
 
+class TunnelError: Error {
+
+}
+
 // Singleton that manages the VPN tunnel and exposes functions
 // for interacting with it.
 class TunnelManager {
@@ -14,22 +18,14 @@ class TunnelManager {
 
     private static let instance = TunnelManager()
 
-    private var tunnel: Result<NETunnelProviderManager, Error>?
+    private var tunnel: NETunnelProviderManager?
     
     static var session: NETunnelProviderSession? {
-        get {
-            return withTunnel { tunnel in
-                return (tunnel.connection as? NETunnelProviderSession)!
-            } as! NETunnelProviderSession?
-        }
+            return TunnelManager.instance.tunnel?.connection as? NETunnelProviderSession
     }
     
     static var protocolConfiguration: NETunnelProviderProtocol? {
-        get {
-            return withTunnel { tunnel in
-                return (tunnel.protocolConfiguration as? NETunnelProviderProtocol)!
-            } as! NETunnelProviderProtocol?
-        }
+        return TunnelManager.instance.tunnel?.protocolConfiguration as? NETunnelProviderProtocol
     }
 
     private init() {}
@@ -40,40 +36,35 @@ class TunnelManager {
         
         logger.debug(message: "Attempting to initialize VPN tunnel")
         
-        TunnelManager.instance.setTunnel { 
-            switch TunnelManager.instance.tunnel {
-            case .failure(let error):
-                logger.error(message: "Error initializing tunnel: \(error).")
-                return completionHandler(error, nil)
-            case .success(let tunnel):
-                logger.info(message: "Tunnel initialized succesfully")
-                return completionHandler(nil, tunnel)
-            case .none:
-                fatalError("IMPOSSIBLE: Attempted to set the VPN tunnel, but didn't get an error nor a tunnel.")
+        TunnelManager.instance.setTunnel {
+            guard let tunnel = TunnelManager.instance.tunnel else {
+                completionHandler(TunnelError(), nil)
+                return
             }
-            
-        };
+
+            logger.info(message: "Tunnel initialized succesfully")
+            completionHandler(nil, tunnel)
+        }
     }
     
     static func withTunnel(_ f: (_ tunnel: NETunnelProviderManager) throws -> Any) -> Any? {
-        switch TunnelManager.instance.tunnel {
-        case .success(let tunnel):
-            do {
-                let result = try f(tunnel)
-                return result
-            } catch {
-                logger.error(message: "Error executing callback: \(error).")
-                return nil
-            }
-        case .failure, .none:
+        guard let tunnel = TunnelManager.instance.tunnel else {
             logger.error(message: "Attempted to use the VPN tunnel, but it's not available.")
+            return nil
+        }
+
+        do {
+            let result = try f(tunnel)
+            return result
+        } catch {
+            logger.error(message: "Error executing callback: \(error).")
             return nil
         }
     }
 
     private func setTunnel(_ completionHandler: @escaping () -> Void) {
         NETunnelProviderManager.loadAllFromPreferences { [weak self] managers, error in
-            if self == nil {
+            guard let self = self else {
                 TunnelManager.logger.debug(message: "We are shutting down.")
                 completionHandler();
                 return
@@ -88,17 +79,16 @@ class TunnelManager {
             let nsManagers = managers ?? []
             TunnelManager.logger.debug(message: "We have received \(nsManagers.count) managers.")
 
-            let tunnel = nsManagers.first(where: TunnelManager.isOurManager(_:))
-            if tunnel == nil {
+            guard let tunnel = nsManagers.first(where: TunnelManager.isOurManager(_:)) else {
                 TunnelManager.logger.debug(message: "Creating the tunnel")
-                self!.tunnel = .success(NETunnelProviderManager())
+                self.tunnel = NETunnelProviderManager()
                 completionHandler();
                 return
             }
 
             TunnelManager.logger.debug(message: "Tunnel already exists")
-            self!.tunnel = .success(tunnel!)
-            completionHandler();
+            self.tunnel = tunnel
+            completionHandler()
         }
     }
 
@@ -111,17 +101,17 @@ class TunnelManager {
             return false
         }
 
-        if (tunnelProto.providerBundleIdentifier == nil) {
+        guard let bundleIdentifier = tunnelProto.providerBundleIdentifier else {
             logger.debug(message: "Ignoring manager because the bundle identifier is null.")
             return false
         }
 
-        if (tunnelProto.providerBundleIdentifier != vpnBundleId) {
+        if (bundleIdentifier != vpnBundleId) {
             logger.debug(message: "Ignoring manager because the bundle identifier doesn't match.")
             return false;
         }
 
-        logger.debug(message: "Found the manager with the correct bundle identifier: \(tunnelProto.providerBundleIdentifier!)")
+        logger.debug(message: "Found the manager with the correct bundle identifier: \(bundleIdentifier)")
         return true
     }
 }
