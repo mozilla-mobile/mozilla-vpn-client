575f53a3d04e7450215456767a8195d27fa8d30c
Gela <gmalekpour@mozilla.com>
2023-07-26 14:01:17 -0700

    Copy over ctor

--- src/apps/vpn/connectionmanager.cpp
+++ src/apps/vpn/connectionmanager.cpp
@@ -17,6 +17,20 @@ Logger logger("Connection Manager");
 ConnectionManager::ConnectionManager() {
   MZ_COUNT_CTOR(ConnectionManager);
   
+  m_connectingTimer.setSingleShot(true);
+  m_handshakeTimer.setSingleShot(true);
+
+  connect(&m_timer, &QTimer::timeout, this, &ConnectionManager::timerTimeout);
+
+  connect(&m_connectingTimer, &QTimer::timeout, this, [this]() {
+    m_enableDisconnectInConfirming = true;
+    emit enableDisconnectInConfirmingChanged();
+  });
+
+  connect(&m_handshakeTimer, &QTimer::timeout, this,
+          &ConnectionManager::handshakeTimeout);
+
+//  LogHandler::instance()->registerLogSerializer(this);
 }
 
 ConnectionManager::~ConnectionManager() {
@@ -25,4 +39,54 @@ ConnectionManager::~ConnectionManager() {
 
 void ConnectionManager::initialize() {
   logger.debug() << "Initializing the connection manager";
+}
+
+qint64 ConnectionManager::time() const {
+  if (m_connectedTimeInUTC.isValid()) {
+    return m_connectedTimeInUTC.secsTo(QDateTime::currentDateTimeUtc());
+  }
+  return 0;
+}
+
+void ConnectionManager::timerTimeout() {
+  Q_ASSERT(m_state == StateOn);
+  emit timeChanged();
+}
+
+void ConnectionManager::handshakeTimeout() {
+  logger.debug() << "Timeout while waiting for handshake";
+
+//  MozillaVPN* vpn = MozillaVPN::instance();
+//  Q_ASSERT(!m_activationQueue.isEmpty());
+//
+//  // Block the offending server and try again.
+//  InterfaceConfig& hop = m_activationQueue.first();
+//  vpn->serverLatency()->setCooldown(
+//      hop.m_serverPublicKey, AppConstants::SERVER_UNRESPONSIVE_COOLDOWN_SEC);
+//
+//  emit handshakeFailed(hop.m_serverPublicKey);
+//
+//  if (m_nextStep != None) {
+//    deactivate();
+//    return;
+//  }
+//
+//  // Try again, again if there are sufficient retries left.
+//  ++m_connectionRetry;
+//  emit connectionRetryChanged();
+//  logger.info() << "Connection attempt " << m_connectionRetry;
+//  if (m_connectionRetry == 1) {
+//    logger.info() << "Connection Attempt: Using Port 53 Option this time.";
+//    // On the first retry, opportunisticly try again using the port 53
+//    // option enabled, if that feature is disabled.
+//    activateInternal(ForceDNSPort, RandomizeServerSelection);
+//    return;
+//  } else if (m_connectionRetry < CONNECTION_MAX_RETRY) {
+//    activateInternal(DoNotForceDNSPort, RandomizeServerSelection);
+//    return;
+//  }
+//
+//  // Otherwise, the give up and report the location as unavailable.
+//  logger.error() << "Connection retries exhausted, giving up";
+//  serverUnavailable();
 }
--- src/apps/vpn/connectionmanager.h
+++ src/apps/vpn/connectionmanager.h
@@ -5,8 +5,9 @@
 #ifndef CONNECTIONMANAGER_H
 #define CONNECTIONMANAGER_H
 
-// #include "constants.h"
+#include <QDateTime>
 #include <QObject>
+#include <QTimer>
 
 class MozillaVPN;
 
@@ -28,7 +29,7 @@ public:
 //  Q_ENUM(ConnectionState)
   
   // These are temporarily copied from the controller state to mirror them, but will eventually be removed by ConnectionState above 
-  enum ControllerState {
+  enum State {
     StateInitializing,
     StateOff,
     StateCheckSubscription,
@@ -39,23 +40,30 @@ public:
     StateSilentSwitching,
     StateSwitching,
   };
-  Q_ENUM(ControllerState)
+  Q_ENUM(State)
   
   enum Reason {
     ReasonNone = 0,
     ReasonSwitching,
     ReasonConfirming,
   };
+
+public:
+  qint64 time() const;
+  
+  bool enableDisconnectInConfirming() const {
+    return m_enableDisconnectInConfirming;
+  }
   
 private:
- Q_PROPERTY(State state READ state NOTIFY stateChanged)
+// Q_PROPERTY(State state READ state NOTIFY stateChanged)
  Q_PROPERTY(qint64 time READ time NOTIFY timeChanged)
- Q_PROPERTY(
-     int connectionRetry READ connectionRetry NOTIFY connectionRetryChanged);
+// Q_PROPERTY(
+//     int connectionRetry READ connectionRetry NOTIFY connectionRetryChanged);
  Q_PROPERTY(bool enableDisconnectInConfirming READ enableDisconnectInConfirming
                 NOTIFY enableDisconnectInConfirmingChanged);
- Q_PROPERTY(bool silentServerSwitchingSupported READ
-                silentServerSwitchingSupported CONSTANT);
+// Q_PROPERTY(bool silentServerSwitchingSupported READ
+//                silentServerSwitchingSupported CONSTANT);
 
 #ifdef MZ_DUMMY
  // This is just for testing purposes. Not exposed in prod.
@@ -63,13 +71,29 @@ private:
                 currentServerChanged);
 #endif
   
+private slots:
+  void timerTimeout();
+  void handshakeTimeout();
+
+signals:
+  void timeChanged();
+  void enableDisconnectInConfirmingChanged();
+  
 public:
   ConnectionManager();
  ~ConnectionManager();
 
  void initialize();
 
-
+private:
+  QTimer m_timer;
+  QTimer m_connectingTimer;
+  QTimer m_handshakeTimer;
+  
+  QDateTime m_connectedTimeInUTC;
+  
+  State m_state = StateInitializing;
+  bool m_enableDisconnectInConfirming = false;
 };  // namespace ConnectionManager
 
 #endif  // CONNECTIONMANAGER_H
