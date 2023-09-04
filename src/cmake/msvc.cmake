fd05a6c120474157ad731f81bdaad56702468a94
Sebastian Streich <streich.mobile@gmail.com>
2023-09-04 15:08:24 +0200

    Fix Args for Clang CL

--- src/cmake/msvc.cmake
+++ src/cmake/msvc.cmake
@@ -2,8 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-add_compile_options(/MP /Zc:preprocessor)
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang$")
+    # We're using clang in msvc mode so we can only use 
+    # msvc style compile flags
+else()
+    # These compile flags are only supported on cl.exe 
+    # they do nothing on clang. 
+    add_compile_options(/MP /Zc:preprocessor)
+endif()
+
 
 set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} /MANIFEST:NO")
 set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /MANIFEST:NO")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /MANIFEST:NO")
