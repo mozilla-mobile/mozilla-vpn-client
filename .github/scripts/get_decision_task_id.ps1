24a9d8228cdd4bc932624fc8041d1d6a02d7aa05
Sebastian Streich <streich.mobile@gmail.com>
2023-09-07 19:00:39 +0200

    okay lets debug

--- .github/scripts/get_decision_task_id.ps1
+++ .github/scripts/get_decision_task_id.ps1
@@ -5,14 +5,18 @@ $script:Task_ID=""
 while (1){
     $(gh pr checks $env:PR_NUMBER).Split([Environment]::NewLine) | ForEach-Object{ 
         if(!$_.contains("Decision Task")){
+            Write-Output "no task"
             return;
         }
         if(!$_.contains("pass")){
+            Write-Output "not ready"
             return;
         }
+        Write-Output $_
         $url = $_.Split([char]9)[-1]
         $script:Task_ID = $url.Replace("https://firefox-ci-tc.services.mozilla.com/tasks/","")
         if($script:Task_ID -eq ""){
+            Write-Output "empty"
             return;
         }
         break;
