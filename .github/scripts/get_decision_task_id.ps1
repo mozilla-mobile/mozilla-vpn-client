fb75dcc395b94e27b2946440c9ed80c8873aff99
Sebastian Streich <streich.mobile@gmail.com>
2023-09-07 18:27:36 +0200

    maybe script is returning bullshit?

--- .github/scripts/get_decision_task_id.ps1
+++ .github/scripts/get_decision_task_id.ps1
@@ -2,20 +2,17 @@ while (1){
     $(gh pr checks $env:PR_NUMBER).Split([Environment]::NewLine) | ForEach-Object{ 
         if($_.contains("Decision Task (pull-request)")){
             if($_.contains("pass")){
-#                Write-Output "Task done!"
                 $url = $_.Split([char]9)[-1]
                 $task_ID += $url.Replace("https://firefox-ci-tc.services.mozilla.com/tasks/","")
-#                Write-Output $task_ID
-                Write-Output "taskID=$task_ID"
+               
                 break;
             }else{
-#              Write-Output "Found task, but not done."
             }
         } else {
-#         Write-Output "Not found task."
         }
         
     }
- #   Write-Output "Sleeping 30s before retry"
-    Start-Sleep -Seconds 30 
+    Start-Sleep -Seconds 60 
 }
+
+Write-Output "taskID=$task_ID"
