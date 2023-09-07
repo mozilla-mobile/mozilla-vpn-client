22e9678178e09f257ebad1ee6256be4e62dbbd64
Sebastian Streich <streich.mobile@gmail.com>
2023-09-07 19:20:45 +0200

    uuh

--- .github/scripts/get_decision_task_id.ps1
+++ .github/scripts/get_decision_task_id.ps1
@@ -1,31 +1,25 @@
 #!/usr/bin/pwsh
 
-$script:Task_ID=""
-
 while (1){
     $(gh pr checks $env:PR_NUMBER).Split([Environment]::NewLine) | ForEach-Object{ 
         if(!$_.contains("Decision Task")){
-            Write-Output "no task"
+            #Write-Output "no task"
             return;
         }
         if(!$_.contains("pass")){
-            Write-Output "not ready"
+            #Write-Output "not ready"
             return;
         }
-        Write-Output $_
+        #Write-Output $_
         $id = $_.Split("/")[-1]    
-        Write-Output $url
+        #Write-Output $url
         if($id -eq ""){
-            Write-Output "empty"
+            #Write-Output "empty"
             return;
         }
         Write-Output "taskID=${id}"
-        $script:Task_ID=$id
-        Write-Output "$script:Task_ID"
         break;
     }
     Start-Sleep -Seconds 30 
 }
 
-Write-Output "taskID=$script:Task_ID"
-return 0
