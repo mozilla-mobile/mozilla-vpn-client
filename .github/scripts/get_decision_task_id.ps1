d2c2509f3b27139b8af11c6abdffe9d39258010f
Sebastian Streich <streich.mobile@gmail.com>
2023-09-07 18:52:16 +0200

    fix ?

--- .github/scripts/get_decision_task_id.ps1
+++ .github/scripts/get_decision_task_id.ps1
@@ -1,20 +1,23 @@
 #!/usr/bin/pwsh
 
+$script:Task_ID=""
+
 while (1){
-    $(gh pr checks $env:PR_NUMBER).Split([Environment]::NewLine) | ForEach-Object{ 
-        if($_.contains("Decision Task (pull-request)")){
-            if($_.contains("pass")){
-                $url = $_.Split([char]9)[-1]
-                $task_ID += $url.Replace("https://firefox-ci-tc.services.mozilla.com/tasks/","")
-               
-                break;
-            }else{
-            }
-        } else {
+    $(gh pr checks ).Split([Environment]::NewLine) | ForEach-Object{ 
+        if(!$_.contains("Decision Task")){
+            return;
         }
-        
+        if(!$_.contains("pass")){
+            return;
+        }
+        $url = $_.Split([char]9)[-1]
+        $script:Task_ID = $url.Replace("https://firefox-ci-tc.services.mozilla.com/tasks/","")
+        if($script:Task_ID -eq ""){
+            return;
+        }
+        break;
     }
     Start-Sleep -Seconds 60 
 }
 
-Write-Output "taskID=$task_ID"
+Write-Output "taskID=$script:Task_ID"
