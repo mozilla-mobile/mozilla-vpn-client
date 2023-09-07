6d19732e231aa6913080f849af38404bc4d4efdf
Sebastian Streich <streich.mobile@gmail.com>
2023-09-07 18:19:25 +0200

    use an extra script?

--- .github/scripts/get_decision_task_id.ps1
+++ .github/scripts/get_decision_task_id.ps1
@@ -1,17 +1,21 @@
 while (1){
-    $(gh pr checks).Split([Environment]::NewLine) | ForEach-Object{ 
-        Write-Output $_
+    $(gh pr checks $env:PR_NUMBER).Split([Environment]::NewLine) | ForEach-Object{ 
         if($_.contains("Decision Task (pull-request)")){
             if($_.contains("pass")){
+#                Write-Output "Task done!"
                 $url = $_.Split([char]9)[-1]
                 $task_ID += $url.Replace("https://firefox-ci-tc.services.mozilla.com/tasks/","")
-                Write-Output $task_ID
-                Write-Output "taskID=$task_ID" | Out-File -FilePath $env:GITHUB_OUTPUT
+#                Write-Output $task_ID
+                Write-Output "taskID=$task_ID"
                 break;
+            }else{
+#              Write-Output "Found task, but not done."
             }
+        } else {
+#         Write-Output "Not found task."
         }
         
     }
-    Write-Output "Decision Task not found, sleeping 1s"
-    Start-Sleep -Seconds 1 
+ #   Write-Output "Sleeping 30s before retry"
+    Start-Sleep -Seconds 30 
 }
