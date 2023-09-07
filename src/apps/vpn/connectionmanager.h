a0f9a4ef9cb837ce289cf7b8981468238856c74f
Gela <gmalekpour@mozilla.com>
2023-09-06 17:00:26 -0700

    TODO

--- src/apps/vpn/connectionmanager.h
+++ src/apps/vpn/connectionmanager.h
@@ -47,7 +47,7 @@ class ConnectionManager : public QObject, public LogSerializer {
   //  Q_ENUM(ConnectionState)
 
   /* TODO: The states below are copied from the controller state to mirror the
-  controller, but they will eventually be removed by ConnectionState above. */
+  controller, but they will eventually be removed by ConnectionState above. (VPN-3575) */
 
   enum State {
     StateInitializing,
