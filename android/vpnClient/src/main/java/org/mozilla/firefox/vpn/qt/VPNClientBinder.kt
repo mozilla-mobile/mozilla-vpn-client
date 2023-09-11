5c6b32c08c503bfb016cb9c2fb48cdd4171f70c0
Sebastian Streich <streich.mobile@gmail.com>
2023-09-11 19:02:56 +0200

    Try to fetch android notification permissions

--- android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNClientBinder.kt
+++ android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNClientBinder.kt
@@ -6,15 +6,20 @@ package org.mozilla.firefox.vpn
 import android.os.Binder
 import android.os.Parcel
 import org.mozilla.firefox.vpn.qt.VPNActivity
+import android.util.Log
 
-const val permissionRequired = 6
+const val vpnPermissionRequired = 6
+const val notificationPermissionRequired= 8
 
 class VPNClientBinder() : Binder() {
 
     override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
-        if (code == permissionRequired) {
+        if (code == vpnPermissionRequired) {
             VPNActivity.getInstance().onPermissionRequest(code, data)
             return true
+        }else if(code == notificationPermissionRequired){
+            Log.e("VPNActivity", "ASKING FOR PERMISSIOOONS")
+            VPNActivity.getInstance().onNotificationPermissionRequest()
         }
 
         val buffer = data.createByteArray()
