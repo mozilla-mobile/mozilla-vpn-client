3517d16e0a4210a8b8e6eab133ccc28eb626af49
Sebastian Streich <streich.mobile@gmail.com>
2023-09-12 15:21:31 +0200

    Yeees, fixed it

---
+++ android/common/src/main/java/org/mozilla/firefox/qt/common/CoreBinder.kt
@@ -0,0 +1,35 @@
+package org.mozilla.firefox.qt.common
+
+import android.os.Binder
+
+open class CoreBinder : Binder() {
+    /** The codes this Binder does accept in [onTransact] */
+    object ACTIONS {
+        const val activate = 1
+        const val deactivate = 2
+        const val registerEventListener = 3
+        const val requestStatistic = 4
+        const val requestCleanupLog = 6
+        const val resumeActivate = 7
+        const val setNotificationText = 8
+        const val recordEvent = 10
+        const val getStatus = 13
+        const val setStartOnBoot = 15
+        const val reactivate = 16
+        const val clearStorage = 17
+        const val setGleanUploadEnabled = 18
+        const val notificationPermissionFired = 19
+    }
+
+    /** The codes we Are Using in case of [dispatchEvent] */
+    object EVENTS {
+        const val init = 0
+        const val connected = 1
+        const val disconnected = 2
+        const val statisticUpdate = 3
+        const val activationError = 5
+        const val permissionRequired = 6
+        const val requestGleanUploadEnabledState = 7
+        const val requestNotificationPermission = 8
+    }
+}
--- android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNClientBinder.kt
+++ android/vpnClient/src/main/java/org/mozilla/firefox/vpn/qt/VPNClientBinder.kt
@@ -3,28 +3,28 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.firefox.vpn
-import android.os.Binder
 import android.os.Parcel
+import org.mozilla.firefox.qt.common.CoreBinder
 import org.mozilla.firefox.vpn.qt.VPNActivity
-import android.util.Log
 
-const val vpnPermissionRequired = 6
-const val notificationPermissionRequired= 8
-
-class VPNClientBinder() : Binder() {
+class VPNClientBinder() : CoreBinder() {
 
     override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
-        if (code == vpnPermissionRequired) {
-            VPNActivity.getInstance().onPermissionRequest(code, data)
-            return true
-        }else if(code == notificationPermissionRequired){
-            Log.e("VPNActivity", "ASKING FOR PERMISSIOOONS")
-            VPNActivity.getInstance().onNotificationPermissionRequest()
+        when (code) {
+            EVENTS.permissionRequired -> {
+                VPNActivity.getInstance().onPermissionRequest(code, data)
+                return true
+            }
+            EVENTS.requestNotificationPermission -> {
+                VPNActivity.getInstance().onNotificationPermissionRequest()
+                return true
+            }
+            else -> {
+                val buffer = data.createByteArray()
+                val stringData = buffer?.let { String(it) }
+                VPNActivity.getInstance().onServiceMessage(code, stringData)
+                return true
+            }
         }
-
-        val buffer = data.createByteArray()
-        val stringData = buffer?.let { String(it) }
-        VPNActivity.getInstance().onServiceMessage(code, stringData)
-        return true
     }
 }
