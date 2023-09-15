aa776805e989c427fb2d1a294884fe7a9c91e633
Sebastian Streich <strseb@users.noreply.github.com>
2023-09-15 16:08:47 +0000

    Enable edit&continue on msvc

--- src/cmake/msvc.cmake
+++ src/cmake/msvc.cmake
@@ -5,12 +5,19 @@
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang$")
     # We're using clang in msvc mode so we can only use 
     # msvc style compile flags
+    # We cannot use mt.exe as clang's replacement is ... not done. 
+    set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} /MANIFEST:NO")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /MANIFEST:NO")
 else()
-    # These compile flags are only supported on cl.exe 
-    # they do nothing on clang. 
-    add_compile_options(/MP /Zc:preprocessor)
+    # /MP -> Compile Files Paraell
+    # /Zc:preprocessor -> Enable Modern Macros, needed for settingsholder
+    set(CMAKE_CXX_FLAGS  "/MP /Zc:preprocessor")
+    # Enanble "edit and continue" when using msvc + debug build
+    set(CMAKE_CXX_FLAGS_DEBUG  "/MTd /ZI /Ob0 /Od /RTC1")
+    set(CMAKE_CXX_FLAGS_DEBUG  "/MTd /ZI /Ob0 /Od /RTC1")
+    set(CMAKE_MODULE_LINKER_FLAGS_DEBUG  "/debug /INCREMENTAL /LTCG:OFF")
+    set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "/debug /INCREMENTAL /LTCG:OFF")
+    set(CMAKE_EXE_LINKER_FLAGS_DEBUG  "/debug /INCREMENTAL /LTCG:OFF")
+    
 endif()
 
-
-set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} /MANIFEST:NO")
-set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /MANIFEST:NO")
