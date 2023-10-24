ec0986e55201eced399e5558991da7302e600731
Gela <gela@mozilla.com>
2023-10-24 08:31:28 -0700

    VPN-5740 - Add force_backend_failure to inspector commands (#8341)
    * Add mockSomethingWentWrong to inspector commands
    
    * change command name to force_backend_failure
    
    * fixup! change command name to force_backend_failure
    
    * Reorg commands alphabetically

--- src/mozillavpn.cpp
+++ src/mozillavpn.cpp
@@ -1973,6 +1973,13 @@ void MozillaVPN::registerInspectorCommands() {
       });
 
   InspectorHandler::registerCommand(
+      "force_backend_failure", "Force a backend failure", 0,
+      [](InspectorHandler*, const QList<QByteArray>&) {
+        MozillaVPN::instance()->connectionManager()->backendFailure();
+        return QJsonObject();
+      });
+
+  InspectorHandler::registerCommand(
       "force_captive_portal_check", "Force a captive portal check", 0,
       [](InspectorHandler*, const QList<QByteArray>&) {
         MozillaVPN::instance()->captivePortalDetection()->detectCaptivePortal();
