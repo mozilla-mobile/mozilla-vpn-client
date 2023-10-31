9da9e634c8e28bd994dd3d8e0a4619ef7e0e53b3
Beatriz Rizental <beatriz.rizental@gmail.com>
2023-10-31 16:22:31 +0100

    Remove cryptosettings from platforms sources.cmake

--- src/platforms/android/sources.cmake
+++ src/platforms/android/sources.cmake
@@ -2,10 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-target_sources(shared-sources INTERFACE
-     ${CMAKE_CURRENT_SOURCE_DIR}/platforms/android/androidcryptosettings.cpp
-)
-
 if(ADJUST_TOKEN)
     message(Adjust SDK enabled)
     # SDK Token present, let's enable that.
