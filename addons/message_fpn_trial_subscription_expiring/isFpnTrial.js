bcf9639ad4553b15e59a274155127454d39dc5c6
Matt Lichtenstein <mlichtenstein@mozilla.com>
2023-11-08 18:20:46 -0500

    add subscriptionDataChanged watcher

--- addons/message_fpn_trial_subscription_expiring/isFpnTrial.js
+++ addons/message_fpn_trial_subscription_expiring/isFpnTrial.js
@@ -1,8 +1,15 @@
 (function(api, condition) {
-  //Enable message for users on the FPN 6 month trial plan only
-  const fpnPlanId = "price_1MzNRCJNcmPzuWtRMCwUWADu"
-  const subscriptionDataJson = JSON.parse(api.settings.subscriptionData)
-  const subscriptionPlanId = subscriptionDataJson["plan_id"]
+  function computeCondition() {
 
-  subscriptionPlanId === fpnPlanId ? condition.enable() : condition.disable()
+    //Enable message for users on the FPN 6 month trial plan only
+    const fpnPlanId = "price_1MzNRCJNcmPzuWtRMCwUWADu"
+    const subscriptionDataJson = JSON.parse(api.settings.subscriptionData)
+    const subscriptionPlanId = subscriptionDataJson["plan_id"]
+
+    subscriptionPlanId === fpnPlanId ? condition.enable() : condition.disable()
+  }
+
+  api.connectSignal(api.settings.subscriptionData, 'changed', () => computeCondition());
+
+  computeCondition();
 });
