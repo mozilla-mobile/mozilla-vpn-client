2a5c15d024ff0529eaa785ee246de370a44f09aa
Sebastian Streich <streich.mobile@gmail.com>
2023-11-13 16:19:28 +0100

    Conda pack iOS enviroments

---
+++ taskcluster/scripts/toolchain/conda_pack_ios.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+set -e
+
+echo pwd 
+ls 
+cd vcs
+ls 
+
+print Y "Installing conda"
+chmod +x ${MOZ_FETCHES_DIR}/miniconda.sh
+bash ${MOZ_FETCHES_DIR}/miniconda.sh -b -u -p ${TASK_HOME}/miniconda
+source ${TASK_HOME}/miniconda/bin/activate
+
+conda install conda-pack -y
+conda env create -f env.yml -n vpn
+bash -l -c "conda activate vpn && conda env config vars set QT_VERSION=${QT_VERSION}"
+bash -l -c "conda activate vpn && ./scripts/macos/conda_install_extras.sh"
+bash -l -c "conda activate vpn && ./scripts/macos/conda_setup_qt.sh"
+    
+mkdir -p ../../public/build
+conda pack -n vpn -o $UPLOAD_DIR/conda-ios.tar.gz
