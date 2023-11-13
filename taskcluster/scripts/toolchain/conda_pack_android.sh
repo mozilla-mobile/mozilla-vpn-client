c3b1810732fb8c61fbe5ef962de722eb897acb9d
Sebastian Streich <streich.mobile@gmail.com>
2023-11-13 15:11:29 +0100

    Pin the qt version inside the packed conda env

--- taskcluster/scripts/toolchain/conda_pack_android.sh
+++ taskcluster/scripts/toolchain/conda_pack_android.sh
@@ -3,7 +3,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-set -e
+
 
 echo pwd 
 ls 
@@ -13,6 +13,7 @@ ls
 /opt/conda/bin/conda install conda-pack -y
 
 /opt/conda/bin/conda env create -f env.yml -n vpn
+bash -l -c "conda activate vpn && conda env config vars set QT_VERSION=${QT_VERSION}"
 bash -l -c "conda activate vpn && ./scripts/android/conda_setup_sdk.sh"
 bash -l -c "conda activate vpn && ./scripts/android/conda_setup_qt.sh"
 bash -l -c "conda activate vpn && ./scripts/android/conda_trim.sh"
