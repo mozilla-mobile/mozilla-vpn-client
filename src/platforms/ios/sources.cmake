38b7012c0dc42168104954d48b5461f19ac0389d
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2023-11-16 15:47:38 -0500

    remove unneeded line

--- src/platforms/ios/sources.cmake
+++ src/platforms/ios/sources.cmake
@@ -11,7 +11,6 @@ target_sources(shared-sources INTERFACE
 # Include the Adjust SDK
 if(DEFINED ENV{MVPN_IOS_ADJUST_TOKEN})
     target_compile_definitions(mozillavpn PUBLIC MZ_ADJUST)
-    target_compile_options(shared-sources INTERFACE -DADJUST_SDK_TOKEN=$ENV{MVPN_IOS_ADJUST_TOKEN})
     target_sources(shared-sources INTERFACE
         ${CMAKE_CURRENT_SOURCE_DIR}/adjust/adjustfiltering.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/adjust/adjustfiltering.h
