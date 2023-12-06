d4295254daa0ac1c0ffe278386def4fb49cf2606
Naomi Kirby <oskirby@gmail.com>
2023-12-06 14:04:11 -0800

    Repackage MSI task should avoid writing to src.

--- windows/installer/build_prod.cmd
+++ windows/installer/build_prod.cmd
@@ -5,7 +5,7 @@ rem Copyright (C) 2019 Edge Security LLC. All Rights Reserved.
 
 setlocal
 set PATHEXT=.exe
-set BUILDDIR=%~dp0
+set BUILDDIR=%TASK_WORKDIR%\build
 set ARTIFACTDIR=%TASK_WORKDIR%\artifacts
 set FETCHESDIR=%TASK_WORKDIR%\fetches
 cd /d %BUILDDIR% || exit /b 1
@@ -13,25 +13,22 @@ cd /d %BUILDDIR% || exit /b 1
 set WIX_CANDLE_FLAGS=-nologo -ext WiXUtilExtension
 set WIX_LIGHT_FLAGS=-nologo -spdb -ext WixUtilExtension
 
-if exist .deps\prepared goto :build
+if exist %BUILDDIR%\wix\prepared goto :build
 :installdeps
-	rmdir /s /q .deps 2> NUL
-	mkdir .deps || goto :error
-	cd .deps || goto :error
+	rmdir /s /q %BUILDDIR%\wix 2> NUL
+	mkdir %BUILDDIR%\wix || goto :error
 	call :download wix-binaries.zip https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip 2c1888d5d1dba377fc7fa14444cf556963747ff9a0a289a3599cf09da03b9e2e || goto :error
 	echo [+] Extracting wix-binaries.zip
-	mkdir wix\bin || goto :error
-	powershell -command "Expand-Archive" -Path wix-binaries.zip -DestinationPath wix\bin || exit /b 1
+	powershell -command "Expand-Archive" -Path wix-binaries.zip -DestinationPath %BUILDDIR%\wix || exit /b 1
 	echo [+] Cleaning up wix-binaries.zip
 	del wix-binaries.zip || goto :error
-	copy /y NUL prepared > NUL || goto :error
-	cd .. || goto :error
+	copy /y NUL %BUILDDIR%\wix\prepared > NUL || goto :error
 
 :build
 	echo [+] Extracting %FETCHESDIR%\unsigned.zip
-	mkdir %TASK_WORKDIR%\build\unsigned || goto :error
-	powershell -command "Expand-Archive" -Path %FETCHESDIR%\unsigned.zip -DestinationPath %TASK_WORKDIR%\build\unsigned || exit /b 1
-	set WIX=%BUILDDIR%.deps\wix\
+	mkdir %BUILDDIR%\unsigned || goto :error
+	powershell -command "Expand-Archive" -Path %FETCHESDIR%\unsigned.zip -DestinationPath %BUILDDIR%\unsigned || exit /b 1
+	set WIX=%BUILDDIR%wix\
 	call :msi x64 || goto :error
 
 :success
@@ -47,11 +44,11 @@ if exist .deps\prepared goto :build
 
 :msi
 	if not exist "%ARTIFACTDIR%" mkdir "%ARTIFACTDIR%"
-	pushd %TASK_WORKDIR%\build\unsigned
+	pushd %BUILDDIR%\unsigned
 	echo [+] Compiling %1
-       "%WIX%bin\candle" %WIX_CANDLE_FLAGS% -dPlatform=%1 -out "%ARTIFACTDIR%\MozillaVPN.wixobj" -arch %1 %TASK_WORKDIR%\build\src\windows\installer\MozillaVPN.wxs || exit /b %errorlevel%
+       "%WIX%\candle" %WIX_CANDLE_FLAGS% -dPlatform=%1 -out "%ARTIFACTDIR%\MozillaVPN.wixobj" -arch %1 %BUILDDIR%\src\windows\installer\MozillaVPN.wxs || exit /b %errorlevel%
 	echo [+] Linking %1
-       "%WIX%bin\light" %WIX_LIGHT_FLAGS% -out "%ARTIFACTDIR%/MozillaVPN.msi" "%ARTIFACTDIR%\MozillaVPN.wixobj" || exit /b %errorlevel%
+       "%WIX%\light" %WIX_LIGHT_FLAGS% -out "%ARTIFACTDIR%/MozillaVPN.msi" "%ARTIFACTDIR%\MozillaVPN.wixobj" || exit /b %errorlevel%
 	popd
 	goto :eof
 
