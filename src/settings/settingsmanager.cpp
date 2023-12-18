7671674b395d15f902ce36d4db6473b6b7c7c969
Beatriz Rizental <beatriz.rizental@gmail.com>
2023-12-18 13:52:58 +0100

    Fix lints

--- src/settings/settingsmanager.cpp
+++ src/settings/settingsmanager.cpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "settingsmanager.h"
+
 #include <QApplication>
 #include <QDir>
 #include <QFile>
@@ -12,7 +14,6 @@
 #include "cryptosettings.h"
 #include "leakdetector.h"
 #include "logger.h"
-#include "settingsmanager.h"
 
 namespace {
 
