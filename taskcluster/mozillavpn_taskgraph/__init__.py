8b0a0d1072bc100eab971e293db012354ba11c48
Andrew Halberstadt <ahal@mozilla.com>
2024-01-02 13:11:05 -0500

    Use 'release_promotion' action from mozilla-taskgraph (#8850)
    This is the same functionality, just moving code around so:
    
    1. We only need to maintain it in a single place
    2. VPN benefits from bug fixes / improvements from other repos

--- taskcluster/mozillavpn_taskgraph/__init__.py
+++ taskcluster/mozillavpn_taskgraph/__init__.py
@@ -5,6 +5,8 @@
 from importlib import import_module
 
 from taskgraph.util import schema
+from mozilla_taskgraph import register as register_mozilla_taskgraph
+from mozilla_taskgraph.actions import enable_action
 
 schema.EXCEPTED_SCHEMA_IDENTIFIERS.append(
     lambda path: any(
@@ -13,14 +15,26 @@ schema.EXCEPTED_SCHEMA_IDENTIFIERS.append(
 )
 
 
+RELEASE_PROMOTION_PROJECTS = (
+    "https://github.com/mozilla-mobile/mozilla-vpn-client",
+    "https://github.com/mozilla-releng/staging-mozilla-vpn-client",
+)
+
+
+def is_relpro_available(parameters):
+    return parameters["head_repository"] in RELEASE_PROMOTION_PROJECTS
+
+
 def register(graph_config):
-    """
-    Import all modules that are siblings of this one, triggering decorators in
-    the process.
-    """
+    """Setup for task generation."""
+    # Setup mozilla-taskgraph
+    register_mozilla_taskgraph(graph_config)
+    # Enable the relpro action from mozilla-taskgraph
+    enable_action("release-promotion", available=is_relpro_available)
+
+    # Import sibling modules, triggering decorators in the process
     _import_modules(
         [
-            "actions.release_promotion",
             "job",
             "parameters",
             "target_tasks",
