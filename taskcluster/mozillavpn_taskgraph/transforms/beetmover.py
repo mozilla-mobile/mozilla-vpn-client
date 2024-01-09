87c4a242225cbd689e0dafe5134afeabdb3e47df
Andrew Halberstadt <ahal@mozilla.com>
2024-01-09 15:42:11 -0500

    [VPN-6080] create distinct beetmover tasks for each shipping phase
    This refactors the beetmover transform to generate distinct tasks for
    each shipping phase. This is more inline with how other projects work
    and is a pre-requisite for integrating VPN with Shipit, which has
    assumptions around how the tasks are set up.
    
    Issue: #8919

--- taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
+++ taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
@@ -15,6 +15,7 @@ transforms = TransformSequence()
 
 beetmover_schema = Schema(
     {
+        Required("beetmover-action"): str,
         Required("attributes"): {
             Required("build-type"): str,
             Required("release-artifacts"): [dict],
@@ -92,60 +93,18 @@ def add_beetmover_worker_config(config, tasks):
     archive_url = (
         "https://ftp.mozilla.org/" if is_production else "https://ftp.stage.mozaws.net/"
     )
+    short_phase = config.kind[len("beetmover-"):]
 
     for task in tasks:
         worker_type = task["worker-type"]
         build_type = task["attributes"]["build-type"]
         build_os = build_type_os.get(build_type)
-        shipping_phase = config.params.get("shipping_phase", "")
-
-        destination_paths = []
-
-        if build_type == "addons/opt":
-            destination_paths.append(
-                os.path.join(
-                    "pub",
-                    "vpn",
-                    "addons",
-                    "releases" if shipping_phase.startswith("ship") else "candidates",
-                    build_id,
-                )
-            )
-        elif shipping_phase == "promote-client":
-            destination_paths.append(
-                os.path.join(
-                    "pub",
-                    "vpn",
-                    "candidates",
-                    f"{app_version}-candidates",
-                    f"build{build_id}",
-                    build_os,
-                )
-            )
-        elif shipping_phase == "ship-client":
-            destination_paths.append(
-                os.path.join(
-                    "pub",
-                    "vpn",
-                    "releases",
-                    app_version,
-                    build_os,
-                )
-            )
-
-        if shipping_phase == "ship-addons":
-            destination_paths.append(
-                os.path.join(
-                    "pub",
-                    "vpn",
-                    "addons",
-                    "releases",
-                    "latest",
-                )
-            )
+        phase = f"{short_phase}-{'addons' if task['name'].startswith('addons') else 'client'}"
 
         upstream_artifacts = []
         for dep in task["dependencies"]:
+            if dep not in ("build", "signing"):
+                continue
             upstream_artifacts.append(
                 {
                     "taskId": {"task-reference": f"<{dep}>"},
@@ -157,9 +116,69 @@ def add_beetmover_worker_config(config, tasks):
                 }
             )
 
-        artifact_map = []
+        worker = {
+            "action": task["beetmover-action"],
+            "artifact-map": [],
+            "bucket": bucket,
+            "build-number": int(build_id),
+            "release-properties": {
+                "app-name": "vpn",
+                "app-version": app_version,
+                "branch": config.params["head_ref"],
+                "build-id": build_id,
+                "platform": build_type,
+            },
+            "upstream-artifacts": upstream_artifacts
+        }
+
+        destination_paths = []
+
+        if task["name"].startswith("addons"):
+            destination_paths.append(
+                os.path.join(
+                    "pub",
+                    "vpn",
+                    "addons",
+                    "releases" if short_phase == "ship" else "candidates",
+                    build_id,
+                )
+            )
+            if phase == "ship-addons":
+                destination_paths.append(
+                    os.path.join(
+                        "pub",
+                        "vpn",
+                        "addons",
+                        "releases",
+                        "latest",
+                    )
+                )
+        elif phase == "promote-client":
+            assert build_os
+            destination_paths.append(
+                os.path.join(
+                    "pub",
+                    "vpn",
+                    "candidates",
+                    f"{app_version}-candidates",
+                    f"build{build_id}",
+                    build_os,
+                )
+            )
+        elif phase == "ship-client":
+            assert build_os
+            destination_paths.append(
+                os.path.join(
+                    "pub",
+                    "vpn",
+                    "releases",
+                    app_version,
+                    build_os,
+                )
+            )
+
         for artifact in upstream_artifacts:
-            artifact_map.append(
+            worker["artifact-map"].append(
                 {
                     "taskId": artifact["taskId"],
                     "paths": {
@@ -179,48 +198,27 @@ def add_beetmover_worker_config(config, tasks):
 
         attributes = {
             **task["attributes"],
-            "shipping-phase": shipping_phase,
+            "shipping-phase": phase,
         }
 
         dest = (
-            f"{archive_url}{destination_paths[0]}" if destination_paths else archive_url
+            f"{archive_url}{destination_paths[0]}"
+            if destination_paths
+            else archive_url
         )
         if build_type == "addons/opt":
             task_description = (
                 f"This {worker_type} task will upload the {task['name']} to {dest}/"
             )
-        elif shipping_phase == "ship-client":
+        elif phase == "ship-client":
             task_description = f"This {worker_type} task will copy the v{app_version} build {build_id} candidate to releases"
         else:
             task_description = f"This {worker_type} task will upload a {build_os} release candidate for v{app_version} to {dest}/"
-
-        if not shipping_phase or shipping_phase.startswith("promote"):
-            action = "push-to-candidates"
-        elif shipping_phase == "ship-addons":
-            action = "direct-push-to-bucket"
-        elif shipping_phase == "ship-client":
-            action = "push-to-releases"
-        else:
-            raise Exception(f"Invalid shipping_phase `{shipping_phase}`")
 
         extra = {
             "release_destinations": [
                 f"{archive_url}{dest}/" for dest in destination_paths
             ]
-        }
-        worker = {
-            "upstream-artifacts": upstream_artifacts,
-            "bucket": bucket,
-            "action": action,
-            "release-properties": {
-                "app-name": "vpn",
-                "app-version": app_version,
-                "branch": config.params["head_ref"],
-                "build-id": build_id,
-                "platform": build_type,
-            },
-            "artifact-map": artifact_map,
-            "build-number": int(build_id),
         }
         task_def = {
             "name": task["name"],
