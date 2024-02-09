d43d0887021a4af6b9c5477105859be20836b2cd
Naomi Kirby <oskirby@gmail.com>
2024-02-08 17:15:39 -0800

    Add some debug to macos_build.sh to debug what we're installing

--- taskcluster/scripts/build/macos_build.sh
+++ taskcluster/scripts/build/macos_build.sh
@@ -37,12 +37,16 @@ export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 export PYTHONIOENCODING="UTF-8"
 
+print Y "Checking conda hash..."
+shasum -a 256 ${MOZ_FETCHES_DIR}/miniconda.sh
 
 print Y "Installing conda"
 chmod +x ${MOZ_FETCHES_DIR}/miniconda.sh
 bash ${MOZ_FETCHES_DIR}/miniconda.sh -b -u -p ${TASK_HOME}/miniconda
 source ${TASK_HOME}/miniconda/bin/activate
 
+print Y "Conda version is..."
+conda -V
 
 print Y "Installing provided conda env..."
 # TODO: Check why --force is needed if we install into TASK_HOME?
