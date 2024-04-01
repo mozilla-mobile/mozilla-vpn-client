7968d8453c2d655e69930d362e835fc3b84dda1f
Lesley Norton <lesley@mozilla.com>
2024-04-01 09:32:25 -0500

    VPN-6146 : Enable factory reset

--- src/feature/featurelist.h
+++ src/feature/featurelist.h
@@ -80,7 +80,7 @@ FEATURE(factoryReset,          // Feature ID
         FeatureCallback_true,  // Can be flipped on
         FeatureCallback_true,  // Can be flipped off
         QStringList(),         // feature dependencies
-        FeatureCallback_false)
+        FeatureCallback_true)
 
 FEATURE(freeTrial,              // Feature ID
         "Free trial",           // Feature name
