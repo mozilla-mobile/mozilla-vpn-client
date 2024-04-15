612f40c9317ca5127d727a72420cd6cd9c3126ad
Sebastian Streich <streich.mobile@gmail.com>
2024-04-15 15:39:17 +0200

    VPN-6267 Respect Errors of DNSPingsender

--- src/connectionhealth.cpp
+++ src/connectionhealth.cpp
@@ -42,6 +42,10 @@ ConnectionHealth::ConnectionHealth() : m_dnsPingSender(QHostAddress()) {
 
   connect(&m_dnsPingSender, &DnsPingSender::recvPing, this,
           &ConnectionHealth::dnsPingReceived, Qt::QueuedConnection);
+  connect(&m_dnsPingSender, &DnsPingSender::criticalPingError, this, [this]() {
+    m_dnsPingInitialized = false;
+    m_dnsPingTimer.stop();
+  });
 
   connect(&m_dnsPingTimer, &QTimer::timeout, this, [this]() {
     m_dnsPingSequence++;
@@ -103,14 +107,13 @@ void ConnectionHealth::startIdle() {
   m_dnsPingInitialized = false;
   m_dnsPingLatency = PING_TIME_UNSTABLE_SEC * 1000;
 
-  m_dnsPingSender.start();
-  m_dnsPingTimer.start(PING_INTERVAL_IDLE_SEC * 1000);
-
-  // Send an initial ping right away.
-  m_dnsPingTimestamp = QDateTime::currentMSecsSinceEpoch();
-  m_dnsPingSender.sendPing(QHostAddress(PING_WELL_KNOWN_ANYCAST_DNS),
-                           m_dnsPingSequence);
-
+  if (m_dnsPingSender.start()) {
+    m_dnsPingTimer.start(PING_INTERVAL_IDLE_SEC * 1000);
+    // Send an initial ping right away.
+    m_dnsPingTimestamp = QDateTime::currentMSecsSinceEpoch();
+    m_dnsPingSender.sendPing(QHostAddress(PING_WELL_KNOWN_ANYCAST_DNS),
+                             m_dnsPingSequence);
+  }
   stopTimingDistributionMetric(m_stability);
 }
 
