251490fd0d196188ee51940d2190a9729bde985b
Naomi Kirby <oskirby@gmail.com>
2024-05-09 12:38:15 -0700

    Let's try installing flatpak tooling

--- taskcluster/scripts/build/flatpak.sh
+++ taskcluster/scripts/build/flatpak.sh
@@ -12,3 +12,9 @@ env
 
 echo "Dumping /etc/os-release..."
 cat /etc/os-release
+
+echo "YOLO... installing flatpak..."
+sudo apt-get install flatpak
+
+echo "YOLO... intstalling flatpak-builder..."
+sudo apt-get install flatpak-builder
