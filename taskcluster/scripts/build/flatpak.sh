12a9ad65c390e623365c3b431a91878b90e8cea2
Naomi Kirby <oskirby@gmail.com>
2024-05-10 10:28:55 -0700

    Okay - we can't specify a tag as we load

--- taskcluster/scripts/build/flatpak.sh
+++ taskcluster/scripts/build/flatpak.sh
@@ -17,7 +17,7 @@ echo "Listing fetches..."
 ls -al $MOZ_FETCHES_DIR
 
 echo "Loading docker image..."
-podman load -i $MOZ_FETCHES_DIR/image.tar.zst worker:latest
+podman load -i $MOZ_FETCHES_DIR/image.tar.zst
 
 echo "Docker exists! Let's try it!"
-podman run --rm --privileged worker:latest /bin/echo "Hello World"
+podman run --rm --privileged docker.io/library/flatpak:latest /bin/echo "Hello World"
