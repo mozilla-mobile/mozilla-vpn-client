3ceb9e253a647f12c4c8de195b78f7c27e2a0efa
Naomi Kirby <oskirby@gmail.com>
2024-05-15 12:33:13 -0700

    Try to pass some envs down to the podman worker

--- taskcluster/scripts/podman_worker.sh
+++ taskcluster/scripts/podman_worker.sh
@@ -6,13 +6,34 @@
 
 set -e
 
-echo "Loading podman image..."
-podman load -i $MOZ_FETCHES_DIR/image.tar.zst | tee /tmp/podman-load.txt
-DOCKER_IMAGE_NAME=$(grep '^Loaded image' /tmp/podman-load.txt | cut -d: -f2-)
+# Create a temp dir for podman artifacts.
+PODMAN_TEMP_DIR=$(mktemp -d)
+cleanup() {
+  rm -rf $PODMAN_TEMP_DIR
+}
+trap cleanup INT ABRT TERM EXIT
 
-echo "Running ${DOCKER_IMAGE_NAME}..."
+# Load the image.
+echo "Loading podman image..."
+podman load -i $MOZ_FETCHES_DIR/image.tar.zst | tee ${PODMAN_TEMP_DIR}/podman-load.txt
+PODMAN_IMAGE_NAME=$(grep '^Loaded image' ${PODMAN_TEMP_DIR}/podman-load.txt | cut -d: -f2- | tr -d '[:space:]')
+
+# Prepare the image environment.
+# Nice-to-have: inherit the env from the taskcluster payload.
+cat << EOF > $PODMAN_TEMP_DIR/podman-env.txt
+BUILD_TIMESTAMP=${BUILD_TIMESTAMP}
+TASK_OWNER=${TASK_OWNER}
+TASK_ID=${TASK_ID}
+MOZ_SCM_LEVEL=${MOZ_SCM_LEVEL}
+MOZ_AUTOMATION=${MOZ_AUTOMATION}
+MOZ_FETCHES_DIR=/mnt/source
+EOF
+
+# Run the image.
+echo "Running ${PODMAN_IMAGE_NAME}..."
 mkdir -p $TASK_WORKDIR/artifacts
 podman run --rm --privileged \
+    --env-file $PODMAN_TEMP_DIR/podman-env.txt \
     --volume=$MOZ_FETCHES_DIR:/mnt/source:ro \
     --volume=$TASK_WORKDIR/artifacts:/mnt/artifacts:rw \
-    ${DOCKER_IMAGE_NAME} "$@"
+    ${PODMAN_IMAGE_NAME} "$@"
