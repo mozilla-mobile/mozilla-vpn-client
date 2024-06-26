a7f9fc5b2d0c2dbc5e690d90fc71c1d04bd1d680
Lesley Norton <lesley@mozilla.com>
2024-06-26 15:01:18 -0500

    VPN-2876 Change load policy for nav bar Screens

--- src/mozillavpn.cpp
+++ src/mozillavpn.cpp
@@ -1494,7 +1494,7 @@ void MozillaVPN::registerNavigatorScreens() {
       });
 
   Navigator::registerScreen(
-      MozillaVPN::ScreenHome, Navigator::LoadPolicy::LoadPersistently,
+      MozillaVPN::ScreenHome, Navigator::LoadPolicy::LoadTemporarily,
       "qrc:/qt/qml/Mozilla/VPN/screens/ScreenHome.qml",
       QVector<int>{App::StateMain}, [](int*) -> int8_t { return 99; },
       []() -> bool { return false; });
@@ -1506,7 +1506,7 @@ void MozillaVPN::registerNavigatorScreens() {
       []() -> bool { return false; });
 
   Navigator::registerScreen(
-      MozillaVPN::ScreenMessaging, Navigator::LoadPolicy::LoadPersistently,
+      MozillaVPN::ScreenMessaging, Navigator::LoadPolicy::LoadTemporarily,
       "qrc:/qt/qml/Mozilla/VPN/screens/ScreenMessaging.qml",
       QVector<int>{App::StateMain}, [](int*) -> int8_t { return 0; },
       []() -> bool {
@@ -1530,7 +1530,7 @@ void MozillaVPN::registerNavigatorScreens() {
       []() -> bool { return false; });
 
   Navigator::registerScreen(
-      MozillaVPN::ScreenSettings, Navigator::LoadPolicy::LoadPersistently,
+      MozillaVPN::ScreenSettings, Navigator::LoadPolicy::LoadTemporarily,
       "qrc:/qt/qml/Mozilla/VPN/screens/ScreenSettings.qml",
       QVector<int>{App::StateMain}, [](int*) -> int8_t { return 0; },
       []() -> bool {
