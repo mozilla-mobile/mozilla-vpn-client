f9f26f527f0d7ff27b2d2c1981f4b37293b73146
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2024-07-25 15:26:41 -0700

    VPN-6529 update addon - change criteria

--- addons/message_update_v2.24/osCheck.js
+++ addons/message_update_v2.24/osCheck.js
@@ -1,23 +1,32 @@
-// Disable on iOS 13 and earlier
+// Disable on iOS 13 and earlier, all Android versions, and all macOS versions before 11
 (function(api, condition) {
-  // First check for iOS...
+  // First check for Android...
+  const isAndroid = (api.env.platform === "android");
+  if (isAndroid) {
+    condition.disable();
+    return;
+  }
+
+  // Then check for iOS or macOS...
   const isIOS = (api.env.platform === "ios");
-  if (!isIOS) {
-    condition.enable();
-    return;
+  const isMacOS = (api.env.platform === "macos");
+  if (isIOS || isMacOS) {
+    // ...then check for the minimum version - minimum is 14 for iOS, 11 for macOS
+    const minVersion = isIOS ? 14 : 11;
+    const osVersion = api.env.osVersion;
+    if (!osVersion || (typeof osVersion !== "string") || osVersion.length === 0) {
+      // Something unexpected happened. Enable on failure.
+      condition.enable();
+      return;
+    }
+    const majorVersionString = osVersion.split(".", 1)[0];
+    const majorVersion = Number(majorVersionString);
+
+    majorVersion >= minVersion ? condition.enable() : condition.disable();
   }
 
-  // ...then check for iOS 14 or later
-  const minVersion = 14;
-  const osVersion = api.env.osVersion;
-  if (!osVersion || (typeof osVersion !== "string") || osVersion.length === 0) {
-    // Something unexpected happened. Enable on failure.
-    condition.enable();
-    return;
-  }
-  const majorVersionString = osVersion.split(".", 1)[0];
-  const majorVersion = Number(majorVersionString);
-
-  majorVersion >= minVersion ? condition.enable() : condition.disable();
+  // For all other platforms, enable the addon.
+  condition.enable();
+  return;
 });
   
