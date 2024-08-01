ebe06580fb23f3e5b52eaf2e20e413100a66d454
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2024-08-01 10:40:38 -0700

    always hide on iOS

--- addons/message_update_v2.24/osCheck.js
+++ addons/message_update_v2.24/osCheck.js
@@ -1,18 +1,18 @@
-// Disable on iOS 13 and earlier, all Android versions, and all macOS versions before 11
+// Disable all Android and iOS versions, and all macOS versions before 11
 (function(api, condition) {
   // First check for Android...
   const isAndroid = (api.env.platform === "android");
-  if (isAndroid) {
+  const isIOS = (api.env.platform === "ios");
+  if (isAndroid || isIOS) {
     condition.disable();
     return;
   }
 
-  // Then check for iOS or macOS...
-  const isIOS = (api.env.platform === "ios");
+  // Then check for macOS...
   const isMacOS = (api.env.platform === "macos");
-  if (isIOS || isMacOS) {
-    // ...then check for the minimum version - minimum is 14 for iOS, 11 for macOS
-    const minVersion = isIOS ? 14 : 11;
+  if (isMacOS) {
+    // ...then check for the minimum version - minimum is 11 for macOS
+    const minVersion = 11;
     const osVersion = api.env.osVersion;
     if (!osVersion || (typeof osVersion !== "string") || osVersion.length === 0) {
       // Something unexpected happened. Enable on failure.
