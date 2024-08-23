cb154179a938e6eb4296c16ac251c50f472abc22
Naomi Kirby <oskirby@gmail.com>
2024-08-23 12:35:06 -0700

    Convert crate manifests into JSON

--- linux/flatpak/flatpak-update-crates.sh
+++ linux/flatpak/flatpak-update-crates.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Update the manifest for rust crates used by the VPN project.
 #
@@ -9,6 +9,36 @@ TMPDIR=$(mktemp -d)
 function cleanup(){ rm -rf $TMPDIR; }
 trap cleanup EXIT
 
+# Parse a flatpak manifest and determine the Cargo.lock URL
+# The path to the manifest should be provided as an argument.
+function parse_cargo_url() {
+python - << EOF
+import sys, yaml, json
+
+def treeish(gitsrc):
+    if 'commit' in gitsrc:
+        return gitsrc['commit']
+    if 'tag' in gitsrc:
+        return gitsrc['tag']
+    if 'branch' in gitsrc:
+        return gitsrc['branch']
+    return 'main'
+
+with open("$1", 'r') as fp:
+    for x in yaml.load(fp, Loader=yaml.SafeLoader)['modules'][-1]['sources']:
+        if not isinstance(x, dict):
+            continue
+        if x['type'] != 'git':
+            continue
+
+        print(f"{x['url']}/raw/{treeish(x)}/Cargo.lock")
+        exit(0)
+
+print("Unable to parse git URL from $1", file=sys.stderr)
+exit(1)
+EOF
+}
+
 # Grab the latest flatpak builder tools
 curl -sSL https://github.com/flatpak/flatpak-builder-tools/raw/master/cargo/flatpak-cargo-generator.py -o $TMPDIR/flatpak-cargo-generator.py
 
@@ -17,13 +47,10 @@ if [ $# -ge 2 ]; then
   CARGO_LOCK_FILE=$1
 else
   # Otherwise - we need to download the Cargo.lock from the source
+  CARGO_LOCK_URL=$(parse_cargo_url ${SRCDIR}/org.mozilla.vpn.yml)
   CARGO_LOCK_FILE=$TMPDIR/Cargo.lock
-  GITINFO=$(python -m yq -o=json '.modules[-1].sources[0]' ${SRCDIR}/org.mozilla.vpn.yml)
-  GITVERSION=$(echo "${GITINFO}" | jq -r '.commit,.tag,.branch | select(. != null)' | head -1)
-  # TODO: Looking at the source's .url would be better
-  curl -sSL https://github.com/mozilla-mobile/mozilla-vpn-client/raw/${GITVERSION}/Cargo.lock -o $CARGO_LOCK_FILE
+  curl -sSL $CARGO_LOCK_URL -o $CARGO_LOCK_FILE
 fi
 
 # Generate the cargo dependencies and convert to YAML
-python $TMPDIR/flatpak-cargo-generator.py -o $TMPDIR/generated-sources.json $CARGO_LOCK_FILE
-python -m yq -y -w120 $TMPDIR/generated-sources.json > ${SRCDIR}/flatpak-vpn-crates.yaml
+python $TMPDIR/flatpak-cargo-generator.py -o ${SRCDIR}/flatpak-vpn-crates.json $CARGO_LOCK_FILE
