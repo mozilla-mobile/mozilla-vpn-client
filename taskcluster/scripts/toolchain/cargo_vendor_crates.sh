ff6866c89e5f668bced508a1c5214813bdd22a96
Naomi Kirby <oskirby@gmail.com>
2024-09-05 17:51:38 -0700

    Cargo manifest-path needs a toml not a lock

--- taskcluster/scripts/toolchain/cargo_vendor_crates.sh
+++ taskcluster/scripts/toolchain/cargo_vendor_crates.sh
@@ -5,5 +5,5 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 set -e
 
-cargo vendor --manifest-path $VCS_PATH/Cargo.lock $(pwd)/cargo-vendor
+cargo vendor --manifest-path $VCS_PATH/Cargo.toml $(pwd)/cargo-vendor
 tar -cJf $UPLOAD_DIR/cargo-vendor.tar.xz cargo-vendor/
