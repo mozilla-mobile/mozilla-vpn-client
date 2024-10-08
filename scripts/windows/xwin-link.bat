17c8e7db5b915387d85bf9ac6b74b3b60e54e61a
Naomi Kirby <oskirby@gmail.com>
2024-10-07 19:54:21 -0700

    Remove some unused files

--- scripts/windows/xwin-link.bat
+++ scripts/windows/xwin-link.bat
@@ -1,8 +1,8 @@
 @echo off
 Rem Wrapper script to combine LLVM's lld-link.exe with Xwin
 Rem This is mostly used to pass the /winsysroot argument to the Cargo/Rust
-Rem linker, because I can't for get it to respect "-Clink-args=..." when
-Rem building for MSVC. I suspect this is a bug.
+Rem linker, because I can't get it to respect "-Clink-args=..." when building
+Rem for MSVC. I suspect this is a bug in Cargo somewhere.
 Rem
 Rem LLVM also has a bug that fails to find the sysroot if the target machine
 Rem is not specified. See: https://github.com/llvm/llvm-project/issues/54409
