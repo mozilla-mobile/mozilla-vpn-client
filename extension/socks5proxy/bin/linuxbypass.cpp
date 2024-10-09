202793ff7be337b0e50944aebec061037f3d211f
Naomi Kirby <oskirby@gmail.com>
2024-10-09 09:53:05 -0700

    Handle Linux caps and depend on CAP_NET_RAW for split tunnel

--- extension/socks5proxy/bin/linuxbypass.cpp
+++ extension/socks5proxy/bin/linuxbypass.cpp
@@ -5,15 +5,36 @@
 #include "linuxbypass.h"
 
 #include <errno.h>
+#include <sys/capability.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include <QAbstractSocket>
 #include <QHostAddress>
+#include <QScopeGuard>
 
 #include "socks5.h"
 
 LinuxBypass::LinuxBypass(Socks5* proxy) : QObject(proxy) {
+  cap_t caps = cap_init();
+  if (caps == nullptr) {
+    qWarning() << "Failed to init process capabilities";
+    return;
+  }
+  auto guard = qScopeGuard([&] { cap_free(caps); });
+
+  // Try to acquire CAP_NET_RAW in order to set the firewall mark.
+  cap_value_t newcaps[] = {CAP_NET_RAW};
+  const int numcaps = sizeof(newcaps) / sizeof(cap_value_t);
+  cap_set_flag(caps, CAP_EFFECTIVE, numcaps, newcaps, CAP_SET);
+  if (cap_set_proc(caps) != 0) {
+    qWarning() << "Failed to set CAP_NET_RAW: disabling split tunnel";
+    cap_clear(caps);
+    cap_set_proc(caps);
+    return;
+  }
+
+  // If we get this far - we can perform split tunneling.
   connect(proxy, &Socks5::outgoingConnection, this,
           &LinuxBypass::outgoingConnection);
 }
