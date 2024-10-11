c5b6fc655a17069b6879865b79f2f5fba629f843
Naomi Kirby <oskirby@gmail.com>
2024-10-11 16:06:53 -0700

    Fix windows include ordering

--- extension/socks5proxy/src/socks5local_windows.cpp
+++ extension/socks5proxy/src/socks5local_windows.cpp
@@ -2,8 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <psapi.h>
+// NOLINTBEGIN: psapi.h must be included after windows.h
 #include <windows.h>
+#include <psapi.h>
+// NOLINTEND
 
 #include <QLocalSocket>
 #include <QScopeGuard>
