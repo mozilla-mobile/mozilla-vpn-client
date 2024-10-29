86c585387efa0a09c7937dfe799a90a666404fcd
Sebastian Streich <strseb@users.noreply.github.com>
2024-10-29 10:03:51 +0100

    Fix split tunnel on ipv4 only networks (#9982)
    * Fix split tunnel on ipv4 only networks
    
    * Do not return uninitalized memory

--- src/platforms/windows/daemon/windowssplittunnel.cpp
+++ src/platforms/windows/daemon/windowssplittunnel.cpp
@@ -484,6 +484,10 @@ bool WindowsSplitTunnel::getAddress(int adapterIndex, IN_ADDR* out_ipv4,
     logger.debug() << "Ipv4 Conversation error" << WSAGetLastError();
     return false;
   }
+  if (ipv6.empty()) {
+    std::memset(out_ipv6, 0x00, sizeof(IN6_ADDR));
+    return true;
+  }
   if (InetPtonW(AF_INET6, ipv6.c_str(), out_ipv6) != 1) {
     logger.debug() << "Ipv6 Conversation error" << WSAGetLastError();
     return false;
