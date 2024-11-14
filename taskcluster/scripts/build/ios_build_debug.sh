e19cfbe36e0e1200c86cb990f98d1097ccc2fbad
Naomi Kirby <oskirby@gmail.com>
2024-11-14 10:13:10 -0800

    Tidy up some debug crufyiness

--- taskcluster/scripts/build/ios_build_debug.sh
+++ taskcluster/scripts/build/ios_build_debug.sh
@@ -44,12 +44,11 @@ if [ -d ${TASK_HOME}/build ]; then
 fi
 mkdir ${TASK_HOME}/build
 
+# Clear the SDKROOT and rely on CMake to figure it out. Otherwise, if this
+# is set to the host SDK it will break the iOS toolchain detection.
 unset SDKROOT
-env
-whereis qt-cmake
-cat $TASK_WORKDIR/fetches/bin/qt-cmake
 
-qt-cmake -S . -B ${TASK_HOME}/build \
+$TASK_WORKDIR/fetches/bin/qt-cmake -S . -B ${TASK_HOME}/build \
   -DCMAKE_OSX_ARCHITECTURES="arm64" \
   -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="" \
   -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED="NO" \
