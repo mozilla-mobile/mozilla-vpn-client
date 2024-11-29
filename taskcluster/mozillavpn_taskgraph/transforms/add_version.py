2efe7b81d1093e60fd7feef0cd13efce063df394
Naomi Kirby <oskirby@gmail.com>
2024-11-29 13:27:39 -0800

    Append VPN client version to deb release artifact name

---
+++ taskcluster/mozillavpn_taskgraph/transforms/add_version.py
@@ -0,0 +1,33 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os.path
+
+from taskgraph.transforms.base import TransformSequence
+
+transforms = TransformSequence()
+
+# Append the VPN client version to the filenames of the release-artifacts.
+@transforms.add
+def append_version(config, tasks):
+    # Get the version number from version.txt
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    with open(cwd, '..', '..', 'version.txt', 'r') as fp:
+        version = fp.readline().strip()
+
+    for task in tasks:
+        if task["name"] not in ("repackage-deb-release", "beetmover-ship-linux64-deb", "beetmover-apt-ship-linux64-deb"):
+            yield task
+            continue
+
+        # Get the release artifacts and append -<version> to their filenames.
+        for artifact in task["attributes"]["release-artifacts"]:  
+            if "name" in artifact:
+                root, ext = os.path.splitext(artifact["name"])
+                artifact["name"] = root + "-" + version + ext
+            if "path" in artifact:
+                root, ext = os.path.splitext(artifact["path"])
+                artifact["path"] = root + "-" + version + ext
+
+        yield task
