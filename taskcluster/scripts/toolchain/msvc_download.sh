113c0e7946b3280dc6da53d47728fec381020f58
Sebastian Streich <streich.mobile@gmail.com>
2025-01-29 16:43:01 +0100

    Add PowerShell script to msvc bundle
    - Added a PowerShell script to the msvc bundle.
    - The script is used to enter the development shell.
    - This change enhances the msvc bundle by providing a more comprehensive toolchain.

--- taskcluster/scripts/toolchain/msvc_download.sh
+++ taskcluster/scripts/toolchain/msvc_download.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-python3 taskcluster/scripts/vsdownload.py --accept-license --dest msvc \
+python3 $VCS_PATH/taskcluster/scripts/vsdownload.py --accept-license --dest msvc \
 Microsoft.VisualStudio.Component.VC.Redist.14.Latest \
 Microsoft.VisualCpp.Tools.HostX64.TargetARM64 \
 Microsoft.VisualCpp.Tools.HostX64.TargetX64 \
@@ -17,6 +17,6 @@ Microsoft.VisualStudio.Component.VC.CoreBuildTools \
 Microsoft.VisualStudio.Component.Windows10SDK.19041 \ \
 Microsoft.VisualStudio.VC.MSBuild.Llvm
 
-cp taskcluster/scripts/fetch/enter_dev_shell.ps1 msvc/
+cp $VCS_PATH/taskcluster/scripts/fetch/enter_dev_shell.ps1 msvc/
 
 tar -cJf $UPLOAD_DIR/msvc.tar.xz msvc/
