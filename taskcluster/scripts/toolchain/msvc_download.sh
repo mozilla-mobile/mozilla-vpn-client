f23d5b24509a2c0a96a4f217593cffadb909fc96
Sebastian Streich <streich.mobile@gmail.com>
2025-01-30 18:40:47 +0100

    Update toolchain scripts for Windows ARM64 development
    - Updated the `enter_dev_shell.ps1` script to support Windows ARM64 development.
    - Updated the `msvc_download.sh` script to download and install the necessary components for Windows ARM64 development.
    - Removed the `vctip.exe` file, which is no longer needed for Windows ARM64 development.
    - Updated the `enter_dev_shell.

--- taskcluster/scripts/toolchain/msvc_download.sh
+++ taskcluster/scripts/toolchain/msvc_download.sh
@@ -4,19 +4,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-python3 $VCS_PATH/taskcluster/scripts/vsdownload.py --accept-license --dest msvc \
-Microsoft.VisualStudio.Component.VC.Redist.14.Latest \
-Microsoft.VisualCpp.Tools.HostX64.TargetARM64 \
-Microsoft.VisualCpp.Tools.HostX64.TargetX64 \
-Microsoft.VisualStudio.Component.VC.14.42.17.12.Tools.x86.x64 \
-Microsoft.VisualStudio.Component.VC.14.42.17.12.Tools.ARM64 \
-Microsoft.VisualStudio.Component.VC.14.42.17.12.ATL.ARM64 \
-Microsoft.VisualStudio.Component.VC.14.42.17.12.ATL \
-Microsoft.VisualStudio.Component.VC.Redist.MSM \
-Microsoft.VisualStudio.Component.VC.CoreBuildTools \
-Microsoft.VisualStudio.Component.Windows10SDK.19041 \ \
-Microsoft.VisualStudio.VC.MSBuild.Llvm
+python3 $VCS_PATH/taskcluster/scripts/vsdownload.py \
+    --major 17 \
+    --architecture arm64 \
+    --host-arch=arm64 \
+    --only-host yes \
+    --sdk-version 10.0.19041  \
+    --accept-license \
+    --dest msvc
 
-cp $VCS_PATH/taskcluster/scripts/fetch/enter_dev_shell.ps1 msvc/
+# Remove vctip.exe it no need to send build telemetry to ms :) 
+find msvc -type f -name "vctip.exe" -exec rm -f {} \;
+
+# Update the "enter dev shell" script to init with host ARM64 and target ARM64
+sed -e 's/*TARGET_ARCH*/arm64/g'  \
+    -e 's/*HOST_ARCH*/arm64/g' \
+      $VCS_PATH/taskcluster/scripts/fetch/enter_dev_shell.ps1.in > "msvc/enter_dev_shell.ps1
 
 tar -cJf $UPLOAD_DIR/msvc.tar.xz msvc/
