a3544bd5e43db5a4a8e2f2f819060aeea1b2fca1
Sebastian Streich <streich.mobile@gmail.com>
2025-01-30 21:05:16 +0100

    Update toolchain scripts for ARM64 support
    - Updated the `enter_dev_shell.ps1.in` script to support ARM64 target and host architectures.
    - Modified the `msvc_download.sh` script to update the `enter_dev_shell.ps1` script with ARM64 support.
    - These changes ensure that the toolchain scripts are compatible with ARM64 environments, enabling developers to use the toolchain effectively on

--- taskcluster/scripts/toolchain/msvc_download.sh
+++ taskcluster/scripts/toolchain/msvc_download.sh
@@ -17,8 +17,7 @@ python3 $VCS_PATH/taskcluster/scripts/vsdownload.py \
 find msvc -type f -name "vctip.exe" -exec rm -f {} \;
 
 # Update the "enter dev shell" script to init with host ARM64 and target ARM64
-sed -e 's/*TARGET_ARCH*/arm64/g'  \
-    -e 's/*HOST_ARCH*/arm64/g' \
-      $VCS_PATH/taskcluster/scripts/fetch/enter_dev_shell.ps1.in > msvc/enter_dev_shell.ps1
+sed -e 's/*TARGET_ARCH/arm64/g' -e 's/*HOST_ARCH/arm64/g' 
+      $VCS_PATH/taskcluster/scripts/toolchain/enter_dev_shell.ps1.in > msvc/enter_dev_shell.ps1
 
 tar -cJf $UPLOAD_DIR/msvc.tar.xz msvc/
