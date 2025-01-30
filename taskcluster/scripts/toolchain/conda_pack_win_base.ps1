440580ac2ee238815a4c95347f5c6deae2c39cb7
Sebastian Streich <streich.mobile@gmail.com>
2025-01-30 13:45:27 +0100

    Update conda environment setup script for Windows
    - Updated the conda environment setup script for Windows to use the latest version of MiniConda.
    - Removed the conda init step as it requires admin rights, which is not available in the task cluster environment.
    - Updated the conda environment creation command to use the latest version of the environment file.
    - Added a step to set up the Windows SDK using a custom script.
    - Updated the PATH environment

--- taskcluster/scripts/toolchain/conda_pack_win_base.ps1
+++ taskcluster/scripts/toolchain/conda_pack_win_base.ps1
@@ -3,37 +3,17 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-# This Script will build a conda-environment suitable to build 
-# the vpn for windows. 
-# It currently includes the windows-sdk, go, rust, c++ 
-# compilers.
+# This Script will build a conda-environment containing non-compiler essentials
+# needed to build (cmake, python)
+# It does not ship a compiler not a Windows SDK
 #
-# Note: It does not ship QT right now. 
 
-$REPO_ROOT_PATH =resolve-path "$PSScriptRoot/../../../"
-$TASK_WORKDIR =resolve-path "$REPO_ROOT_PATH/../../"
-$FETCHES_PATH =resolve-path "$TASK_WORKDIR/fetches"
+. "$PSScriptRoot/../common/helpers.ps1"
 
-New-Item -ItemType Directory -Force -Path "$TASK_WORKDIR/conda"
-$CONDA_DIR =resolve-path "$TASK_WORKDIR/conda"
-# Prep Env:
-
-## Install MiniConda 
-Start-Process "$FETCHES_PATH/miniconda_installer.exe" -Wait -ArgumentList @('/S',"/D=$CONDA_DIR")
-# We don't have the ability to do conda init - as that need's admin rights.
-# So let's just do that ourselves :3
-$Env:CONDA_EXE = "$CONDA_DIR\Scripts\conda.exe"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "$CONDA_DIR"
-$Env:_CONDA_EXE = "$CONDA_DIR\Scripts\conda.exe"
-$CondaModuleArgs = @{ChangePs1 = $False}
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
-
-$env:PATH ="$CONDA_DIR;$FETCHES_PATH;$QTPATH;$env:PATH"
+Install-MiniConda
 
 ## Conda is now ready - let's enable the env
 conda env create --force -f  $REPO_ROOT_PATH/taskcluster/conda/env-win-base.yml
 
 New-Item -ItemType Directory $TASK_WORKDIR\public\build
-conda run -n VPN conda-pack --name win-base -o $TASK_WORKDIR\public\build\conda-windows.tar.gz
+conda run -n win-base conda-pack --name win-base -o $TASK_WORKDIR\public\build\conda-windows.tar.gz
