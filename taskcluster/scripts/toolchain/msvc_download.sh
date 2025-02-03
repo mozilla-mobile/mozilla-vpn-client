97e3b04ada9b5fa2a2f4b131cf74435a6ab0bf59
Sebastian Streich <streich.mobile@gmail.com>
2025-02-03 17:44:33 +0100

    is it too old ?

--- taskcluster/scripts/toolchain/msvc_download.sh
+++ taskcluster/scripts/toolchain/msvc_download.sh
@@ -3,6 +3,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+echo "MSI TOOLS VERSION "
+msiextract --version
 
 python3 $VCS_PATH/taskcluster/scripts/vsdownload.py \
     --major 17 \
