e3f71c89172020a35f058f6712442ae5cee697b1
Sebastian Streich <streich.mobile@gmail.com>
2025-02-12 12:43:39 +0100

    use c-ares

--- extension/socks5proxy/src/dnsserverlookup.h
+++ extension/socks5proxy/src/dnsserverlookup.h
@@ -2,26 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <qexception.h>
-
-#include <QFuture>
 #include <QHostAddress>
-#include <exception>
 
 namespace DNSServerLookup {
-constexpr const int MAX_DNS_LOOKUP_ATTEMPTS = 5;
 std::optional<QHostAddress> getLocalDNSName();
 
-QFuture<QHostAddress> resolve(const QString& hostname,
-                              std::optional<QHostAddress> nameServer);
-
-class DnsFetchException : public QException {
- public:
-  DnsFetchException(const QString& msg) : QException() { m_msg = msg; }
-
-  const QString msg() const { return m_msg; }
-
- private:
-  QString m_msg;
-};
+void resolve(const QString& hostname, std::optional<QHostAddress> nameServer,
+             QObject* parent);
 }  // namespace DNSServerLookup
