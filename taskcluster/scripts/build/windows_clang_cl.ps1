beb8b8b156de9881dd6dafcec200476e80ba4330
Sebastian Streich <streich.mobile@gmail.com>
2025-02-25 00:29:37 +0100

    Forward toolchain files to c-ares

--- taskcluster/scripts/build/windows_clang_cl.ps1
+++ taskcluster/scripts/build/windows_clang_cl.ps1
@@ -57,6 +57,7 @@ $BUILD_DIR =resolve-path "$TASK_WORKDIR/cmake_build"
 # Do the generic build
 cmake -S $REPO_ROOT_PATH -B $BUILD_DIR -GNinja `
         -DCMAKE_BUILD_TYPE=Release `
+        -DCMAKE_TOOLCHAIN_FILE="scripts/windows/conda-toolchain.cmake" `
         -DWIREGUARD_FOLDER="$FETCHES_PATH\wireguard-nt" `
         -DCMAKE_PREFIX_PATH="$QTPATH/lib/cmake" `
         -DBUILD_TESTS=OFF
