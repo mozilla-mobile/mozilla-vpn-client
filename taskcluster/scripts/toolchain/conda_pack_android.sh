446ba9849d2654ef23db7e0e5d6e1b81a08b7348
Sebastian Streich <streich.mobile@gmail.com>
2025-03-02 22:32:40 +0100

    Split env files, mac does not work with go1.23 and i am not in the mood to handle that here aswell

--- taskcluster/scripts/toolchain/conda_pack_android.sh
+++ taskcluster/scripts/toolchain/conda_pack_android.sh
@@ -12,7 +12,7 @@ ls
 
 /opt/conda/bin/conda install conda-pack -y
 
-/opt/conda/bin/conda env create -f env.yml -n vpn
+/opt/conda/bin/conda env create -f env-android.yml -n vpn
 bash -l -c "conda activate vpn && conda env config vars set QT_VERSION=${QT_VERSION}"
 bash -l -c "conda activate vpn && ./scripts/android/conda_setup_sdk.sh"
 bash -l -c "conda activate vpn && ./scripts/android/conda_setup_qt.sh"
