54fde61ca7010d81ee968dfd339b63dcab44723c
Sebastian Streich <streich.mobile@gmail.com>
2025-04-10 13:15:22 +0200

    Format

--- tools/qml_hot_reload/main.cpp
+++ tools/qml_hot_reload/main.cpp
@@ -2,16 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "filewatcherclient.h"
 #include <QCoreApplication>
 #include <csignal>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName("HotReloader");
-    QCoreApplication::setApplicationVersion("1.0");
+#include "filewatcherclient.h"
 
-    FileWatcherClient client(&app);
-    return app.exec();
+int main(int argc, char* argv[]) {
+  QCoreApplication app(argc, argv);
+  QCoreApplication::setApplicationName("HotReloader");
+  QCoreApplication::setApplicationVersion("1.0");
+
+  FileWatcherClient client(&app);
+  return app.exec();
 }
