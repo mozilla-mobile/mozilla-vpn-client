b037fc00595e643309d93a0c341920482b4935a3
Sebastian Streich <streich.mobile@gmail.com>
2025-04-10 13:15:22 +0200

    Format

--- tools/qml_hot_reload/filewatcherclient.h
+++ tools/qml_hot_reload/filewatcherclient.h
@@ -2,43 +2,41 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
 #pragma once
 
-#include <QObject>
+#include <QDir>
 #include <QFileSystemWatcher>
-#include <QWebSocket>
+#include <QObject>
 #include <QString>
 #include <QUrl>
-#include <QDir>
+#include <QWebSocket>
 
 class QCoreApplication;
 class QSocketNotifier;
 
-class FileWatcherClient : public QObject
-{
-    Q_OBJECT
+class FileWatcherClient : public QObject {
+  Q_OBJECT
 
-public:
-    explicit FileWatcherClient(QCoreApplication* app, QObject *parent = nullptr);
-    ~FileWatcherClient() override;
+ public:
+  explicit FileWatcherClient(QCoreApplication* app, QObject* parent = nullptr);
+  ~FileWatcherClient() override;
 
-    void startWatching();
+  void startWatching();
 
-private slots:
-    void onConnected();
-    void onDisconnected();
+ private slots:
+  void onConnected();
+  void onDisconnected();
 
-    void onFileChanged(const QString &path);
-    void aboutToQuit();
+  void onFileChanged(const QString& path);
+  void aboutToQuit();
 
-private:
-    void announceChange(const QString& path);
+ private:
+  void announceChange(const QString& path);
 
-    QCoreApplication* m_app = nullptr;
-    QFileSystemWatcher m_fileWatcher;
-    QWebSocket m_webSocket;
-    QUrl m_wsUrl;
-    QString m_origin;
-    QString m_watchRootPath;
+  QCoreApplication* m_app = nullptr;
+  QFileSystemWatcher m_fileWatcher;
+  QWebSocket m_webSocket;
+  QUrl m_wsUrl;
+  QString m_origin;
+  QString m_watchRootPath;
 };
