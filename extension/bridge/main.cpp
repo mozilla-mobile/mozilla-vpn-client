62502deaac4001621af5f21f37a93681fad01552
Naomi Kirby <oskirby@gmail.com>
2025-04-17 10:08:44 -0700

    Add command line argument parsing

--- extension/bridge/main.cpp
+++ extension/bridge/main.cpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QFile>
 #include <QJsonDocument>
@@ -24,10 +26,47 @@ int main(int argc, char** argv) {
   QCoreApplication::setApplicationName("mozillavpn-webext-bridge");
   QCoreApplication::setApplicationVersion("0.1");
 
-  QStringList args = app.arguments();
-  if (args.length() != 3) { 
-    qWarning() << "Expected 2 arguments got:" << args.length() - 1;
-    qInfo() << "Please invoke using <manifest path> <extension id>";
+  // Prepare to parse the command line arguments.
+  QCommandLineParser parser;
+  const QString desc = "Web extension bridge to the Mozilla VPN client";
+  parser.setApplicationDescription(desc);
+  parser.addPositionalArgument("manifest", "Path to the extension manifest");
+  parser.addPositionalArgument("ext-id", "The extension idedentifier");
+
+  QCommandLineOption optVersion = parser.addVersionOption();
+  QCommandLineOption optHelp = parser.addHelpOption();
+  QCommandLineOption optPort(QStringList({"p", "port"}),
+                             "Port number of the Mozilla VPN client", "PORT");
+  optPort.setDefaultValue(QString::number(VPN_WEBEXTENSION_PORT));
+  parser.addOption(optPort);
+
+  // Parse command line arguments.
+  parser.parse(app.arguments());
+  if (parser.isSet(optHelp)) {
+    parser.showHelp();
+    return 0;
+  }
+  if (parser.isSet(optVersion)) {
+    parser.showVersion();
+    return 0;
+  }
+
+  quint16 portNum = VPN_WEBEXTENSION_PORT;
+  if (parser.isSet(optPort)) {
+    bool okay = false;
+    QString vstring = parser.value(optPort);
+    ulong value = vstring.toULong(&okay);
+    if (!okay || value > UINT16_MAX) {
+      qWarning() << "Invalid port specified:" << vstring;
+      return 1;
+    }
+    portNum = value;
+  }
+
+  QStringList args = parser.positionalArguments();
+  if (args.length() != 2) { 
+    qWarning() << "Expected 2 arguments got:" << args.length();
+    parser.showHelp();
     return 1;
   }
   const QString extId = args.last();
@@ -42,7 +81,7 @@ int main(int argc, char** argv) {
   WebExtHandler handler(&stdoutFile);
 
   // Create a webextension bridge to the VPN client.
-  WebExtBridge bridge(VPN_WEBEXTENSION_PORT);
+  WebExtBridge bridge(portNum);
   QObject::connect(&bridge, &WebExtBridge::messageReceived, &handler,
                    &WebExtHandler::writeMsgStdout);
   QObject::connect(&handler, &WebExtHandler::unhandledMessage, &bridge,
