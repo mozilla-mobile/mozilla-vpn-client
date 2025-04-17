b274e842962f554d9b5e1f7fb85268488f51874e
Naomi Kirby <oskirby@gmail.com>
2025-04-17 13:23:31 -0700

    Get tests working

--- extension/bridge/webexthandler.h
+++ extension/bridge/webexthandler.h
@@ -5,32 +5,41 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "webextreader.h"
+
 #include <QFileDevice>
 #include <QObject>
+#include <QThread>
 
 class QByteArray;
 class QJsonObject;
+class WebExtReader;
 
 class WebExtHandler final : public QObject {
   Q_OBJECT
 
  public:
   WebExtHandler(QFileDevice* d, QObject* parent = nullptr);
+  ~WebExtHandler();
 
   // The methods we can handle locally are exposed as meta-methods.
   Q_INVOKABLE void bridge_ping(const QByteArray& msg);
   Q_INVOKABLE void proc_info(const QByteArray& msg);
   Q_INVOKABLE void start(const QByteArray& msg);
 
-  void handleMessage(const QByteArray& msg);
   void writeMsgStdout(const QByteArray& msg);
   void writeJsonStdout(const QJsonObject& msg);
 
  signals:
   void unhandledMessage(const QByteArray& msg);
+  void eofReceived();
 
  private:
+  void handleMessage(const QByteArray& msg);
+
+  QThread m_thread;
   QFileDevice* m_output;
+  WebExtReader* m_reader;
 };
 
 #endif  // COMMAND_H
