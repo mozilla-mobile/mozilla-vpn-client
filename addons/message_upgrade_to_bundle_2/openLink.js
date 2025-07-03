28a2ecae63325097218f08b375d1f8de288ca1e7
MCleinman <9295855+mcleinman@users.noreply.github.com>
2025-07-03 13:18:32 -0700

    VPN-7135 touch up (#10626)

--- addons/message_upgrade_to_bundle_2/openLink.js
+++ addons/message_upgrade_to_bundle_2/openLink.js
@@ -1,9 +1,9 @@
 ((api) => {
   if (api.env.inProduction) {
     return api.urlOpener.openUrl(
-        `https://www.mozilla.org/products/vpn/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=megabundle-promotion&geo=US&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=c#pricing`);
+        `https://www.mozilla.org/products/vpn/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=megabundle-promotion&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=c#pricing`);
   } else {
     return api.urlOpener.openUrl(
-        `https://www-dev.allizom.org/products/vpn/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=megabundle-promotion&geo=US&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=c#pricing`);
+        `https://www-dev.allizom.org/products/vpn/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=megabundle-promotion&entrypoint_experiment=vpn-landing-bundle-promo&entrypoint_variation=c#pricing`);
   }
 });
