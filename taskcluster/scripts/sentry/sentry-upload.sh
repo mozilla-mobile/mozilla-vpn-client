4cb677097ec4455cd66b785de58623e674a83dda
Naomi Kirby <oskirby@gmail.com>
2025-07-29 14:30:21 -0700

    sentry-cli check only works on one file at a time

--- taskcluster/scripts/sentry/sentry-upload.sh
+++ taskcluster/scripts/sentry/sentry-upload.sh
@@ -7,11 +7,14 @@
 set -e
 
 echo "Checking & genrating a symbols bundle"
+SYMBOL_FILES=$(find ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/ -type f)
 find ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM
-sentry-cli difutil check ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
-sentry-cli difutil bundle-sources ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
+for FILENAME in ${SYMBOL_FILES}; do
+  sentry-cli difutil check ${FILENAME}
+done
+sentry-cli difutil bundle-sources ${SYMBOL_FILES}
 
 echo "Uploading symbol bundle"
 get-secret -s project/mozillavpn/level-1/sentry -k sentry_debug_file_upload_key -f sentry_debug_file_upload_key
 sentry-cli login --auth-token $(cat sentry_debug_file_upload_key)
-sentry-cli debug-files upload --org mozilla -p vpn-client {MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
+sentry-cli debug-files upload --org mozilla -p vpn-client ${SYMBOL_FILES}
