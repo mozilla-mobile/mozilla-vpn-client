1d05c5ffd901b4e8f581c06fd812dacd119e8b0f
Naomi Kirby <oskirby@gmail.com>
2025-07-29 13:50:44 -0700

    Try creating a sentry docker image instead

---
+++ taskcluster/scripts/sentry/sentry-upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -e
+
+echo "Checking & genrating a symbols bundle"
+find ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM
+sentry-cli difutil check ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
+sentry-cli difutil bundle-sources ${MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
+
+echo "Uploading symbol bundle"
+get-secret -s project/mozillavpn/level-1/sentry -k sentry_debug_file_upload_key -f sentry_debug_file_upload_key
+sentry-cli login --auth-token $(cat sentry_debug_file_upload_key)
+sentry-cli debug-files upload --org mozilla -p vpn-client {MOZ_FETCHES_DIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/*
