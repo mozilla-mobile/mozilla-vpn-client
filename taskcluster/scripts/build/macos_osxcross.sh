34fe56f87e306e11b8c6a352341975754e65b89f
Naomi Kirby <nkirby@mozilla.com>
2025-07-30 13:50:15 -0700

    Generate dsym bundles for osxcross builds

--- taskcluster/scripts/build/macos_osxcross.sh
+++ taskcluster/scripts/build/macos_osxcross.sh
@@ -13,6 +13,12 @@ print Y "Installing conda"
 source ${MOZ_FETCHES_DIR}/conda/bin/activate
 conda-unpack
 
+# Provide symlinks for some cctools to their standard name.
+# This is needed to make dsymutil happy as it will attempt to use 'lipo' to
+# extract debug symbols from universal Mach-O binaries.
+CCTOOLS_ARCH_TRIPLE=arm64-apple-darwin20.0.0
+ln -s ${CCTOOLS_ARCH_TRIPLE}-lipo ${CONDA_PREFIX}/bin/lipo
+
 # Use vendored crates - if available.
 if [ -d ${MOZ_FETCHES_DIR}/cargo-vendor ]; then
 mkdir -p .cargo
@@ -43,5 +49,18 @@ cmake --build ${TASK_WORKDIR}/build-osxcross
 print Y "Compressing the build artifacts..."
 mkdir -p ${TASK_WORKDIR}/artifacts/
 tar -C ${TASK_WORKDIR}/build-osxcross/src/ -czvf ${TASK_WORKDIR}/artifacts/MozillaVPN.tar.gz "Mozilla VPN.app" || die
+
+print Y "Generating dSYM bundle..."
+CONTENTS_DIR="${TASK_WORKDIR}/build-osxcross/src/Mozilla VPN.app/Contents"
+find "${TASK_WORKDIR}/build-osxcross/src/Mozilla VPN.app/Contents" -type f -executable | while read EXEFILE; do
+  if dsymutil -s "${EXEFILE}" | grep N_OSO -q; then
+    dsymutil "${EXEFILE}" -o ${TASK_WORKDIR}/MozillaVPN.dSYM
+  fi
+done
+find ${TASK_WORKDIR}/MozillaVPN.dSYM/Contents/Resources/DWARF/ -type f | while read DBGFILE; do
+  sentry-cli difutil check "${DBGFILE}"
+  sentry-cli difutil bundle-sources "${DBGFILE}"
+done
+tar -C ${TASK_WORKDIR} -cJvf ${TASK_WORKDIR}/artifacts/MozillaVPN-dsym.tar.xz MozillaVPN.dSYM || die 
 
 print G "Done!"
