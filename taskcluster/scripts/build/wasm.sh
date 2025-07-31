38b01b401f3f599b24a647a80075b5916940c991
Naomi Kirby <nkirby@mozilla.com>
2025-07-31 10:13:09 -0700

    Switch the taskcluster wasm build to use conda

--- taskcluster/scripts/build/wasm.sh
+++ taskcluster/scripts/build/wasm.sh
@@ -3,29 +3,29 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 set -e
+. $(dirname $0)/../../../scripts/utils/commons.sh
 
-source /opt/emsdk/emsdk_env.sh
-
-mkdir -p .ccache
-
-
-# Reqs
+# Ensure all git submodules are checked out
 git submodule update --init --depth 1
 
-pip3 install -r requirements.txt
+# We need to call bash with a login shell, so that conda is initialized
+print Y "Installing conda"
+source ${MOZ_FETCHES_DIR}/conda/bin/activate
+conda-unpack
 
-export PATH="$QTPATH/wasm_singlethread/bin:$PATH"
-mkdir build
-$QTPATH/wasm_singlethread/bin/qt-cmake -S . -B build \
+print Y "Configuring the build..."
+mkdir ${TASK_WORKDIR}/build-wasm
+${MOZ_FETCHES_DIR}/qt-wasm/bin/qt-cmake -S . -B build-wasm -GNinja \
+    -DQT_HOST_PATH=${MOZ_FETCHES_DIR}/qt-host-tools/ \
+    -DQT_HOST_PATH_CMAKE_DIR=${MOZ_FETCHES_DIR}/qt-host-tools/lib/cmake \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DQT_HOST_PATH=/$QTPATH/gcc_64 \
-    -DQT_HOST_PATH_CMAKE_DIR=/$QTPATH/gcc_64/lib/cmake \
     -DBUILD_TESTS=OFF
-cmake --build build -j8
 
-# Artifacts should be placed here!
-mkdir -p /builds/worker/artifacts/
+print Y "Building the client..."
+cmake --build ${TASK_WORKDIR}/build-wasm
 
-cp -r build/wasm_build/* /builds/worker/artifacts/
+print Y "Compressing the build artifacts..."
+mkdir -p ${TASK_WORKDIR}/artifacts/
+cp -r build-wasm/wasm_build/* ${TASK_WORKDIR}/artifacts/
 
-ccache -s
+print G "Done!"
