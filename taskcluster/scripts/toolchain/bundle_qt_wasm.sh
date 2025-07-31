15da3636016d17f607e06b1df35bdab5583f39dd
Naomi Kirby <nkirby@mozilla.com>
2025-07-31 10:03:58 -0700

    Add a toolchain task to bundle Qt for wasm

---
+++ taskcluster/scripts/toolchain/bundle_qt_wasm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+set -e
+
+python3 -m pip install -r ${VCS_PATH}/requirements.txt
+python3 -m aqt install-qt -O $(pwd)/qt-install linux desktop ${QT_VERSION} wasm_singlethread -m qtwebsockets qt5compat qtshadertools
+
+mv $(pwd)/qt-install/${QT_VERSION}/wasm_singlethread $(pwd)/qt-wasm
+
+mkdir -p ${UPLOAD_DIR}
+tar -cJf ${UPLOAD_DIR}/qt-wasm.tar.xz qt-wasm/
