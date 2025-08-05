32e85d6f83ec4dd0b70b760510f7e46caf11f259
Naomi Kirby <nkirby@mozilla.com>
2025-08-05 15:38:47 -0700

    Try to dry-run the upload at level-1

--- taskcluster/scripts/build/sentry_upload.sh
+++ taskcluster/scripts/build/sentry_upload.sh
@@ -4,16 +4,17 @@
 
 set -e
 
+# Auth using the sentry upload key
+SENTRY_UPLOAD_ARGS="--org mozilla -p vpn-client"
+
 # Only on a release build we have access to those secrects.
 if [ "${MOZ_SCM_LEVEL}" -ge "3" ]; then
     get-secret -s project/mozillavpn/level-1/sentry -k sentry_debug_file_upload_key -f sentry_debug_file_upload_key
     sentry-cli login --auth-token $(cat sentry_debug_file_upload_key)   
 else
     echo "Sentry upload requires task level 3" >&2
+    SENTRY_UPLOAD_ARGS+=" --no-upload"
 fi
-
-# Auth using the sentry upload key
-SENTRY_UPLOAD_ARGS="--org mozilla -p vpn-client"
 
 # Upload support for macOS dSYM bundles.
 upload_dsym_bundle() {
