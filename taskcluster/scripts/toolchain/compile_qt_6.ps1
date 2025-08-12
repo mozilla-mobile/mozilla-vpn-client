91a87c1b4abca46979f20a5fd3517088510ec205
Naomi Kirby <oskirby@gmail.com>
2025-08-12 09:08:53 -0700

    More path fiddling

--- taskcluster/scripts/toolchain/compile_qt_6.ps1
+++ taskcluster/scripts/toolchain/compile_qt_6.ps1
@@ -2,42 +2,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+Set-Location $env:TASK_WORKDIR
+$QT_SRC_ARCHIVE =resolve-path "$env:MOZ_FETCHES_DIR/qt-everywhere-src-*.zip"
 
-$REPO_ROOT_PATH =resolve-path "$PSScriptRoot/../../../"
-$TASK_WORKDIR =resolve-path "$REPO_ROOT_PATH/../../"
-$FETCHES_PATH =resolve-path "$TASK_WORKDIR/fetches"
-$QT_SRC_ARCHIVE =resolve-path "$TASK_WORKDIR/fetches/qt-everywhere-src-*.zip"
+unzip -o -qq -d "$env:MOZ_FETCHES_DIR" $QT_SRC_ARCHIVE
 
-$BIN_PATH = "$REPO_ROOT_PATH/bin"
+Get-ChildItem env:
+# Enter the DEV Shell
+. "$env:MOZ_FETCHES_DIR/VisualStudio/enter_dev_shell.ps1"
 
-Set-Location $FETCHES_PATH
-unzip -o -qq $QT_SRC_ARCHIVE
-unzip -o -qq open_ssl_win.zip # See toolchain/qt.yml for why
+$QT_INSTALL_PATH = "$env:TASK_WORKDIR/qt-windows"
+if(!(Test-Path qt-windows)){
+  New-Item -Path qt-windows -ItemType "directory"
+}
+Copy-Item -Path "$env:VCS_PATH/taskcluster/scripts/toolchain/configure_qt.ps1" -Destination qt-windows/
 
 # Setup Openssl Import
-
-# Setup Openssl Import
-$SSL_PATH = "$FETCHES_PATH/SSL"
+unzip -o -qq -d "$env:TASK_WORKDIR/qt-windows" "$env:MOZ_FETCHES_DIR/open_ssl_win.zip" # See toolchain/qt.yml for why
+$SSL_PATH = "$env:TASK_WORKDIR/qt-windows/SSL"
 if (Test-Path -Path $SSL_PATH) {
   $env:OPENSSL_ROOT_DIR = (resolve-path "$SSL_PATH").toString()
   $env:OPENSSL_USE_STATIC_LIBS = "TRUE"
 }
 
-Get-ChildItem env:
-# Enter the DEV Shell
-. "$FETCHES_PATH/VisualStudio/enter_dev_shell.ps1"
-
-if(!(Test-Path $BIN_PATH)){
-  New-Item -Path $BIN_PATH -ItemType "directory"
-}
-if(!(Test-Path $REPO_ROOT_PATH/qt-windows)){
-  New-Item -Path $REPO_ROOT_PATH/qt-windows -ItemType "directory"
-}
-
 $ErrorActionPreference = "Stop"
 
-$BUILD_PREFIX = (resolve-path "$REPO_ROOT_PATH/qt-windows").toString()
-$QT_SRC_PATH = (resolve-path "$TASK_WORKDIR/fetches/qt-everywhere-src-*/configure.bat" | Split-Path -Parent)
+$BUILD_PREFIX = (resolve-path "$env:TASK_WORKDIR/qt-windows").toString()
+$QT_SRC_PATH = (resolve-path "$env:MOZ_FETCHES_DIR/qt-everywhere-src-*/configure.bat" | Split-Path -Parent)
 Set-Location $QT_SRC_PATH
 
 # Let's trim what we dont need.
@@ -109,16 +100,11 @@ cmake --build . --parallel
 cmake --install . --config Debug
 cmake --install . --config Release
 
-Copy-Item -Path $REPO_ROOT_PATH/taskcluster/scripts/toolchain/configure_qt.ps1 -Destination qt-windows/
-
-if (Test-Path -Path $SSL_PATH) {
-  Copy-Item -Path $SSL_PATH -Recurse -Destination qt-windows/
-}
-
+Set-Location $env:TASK_WORKDIR
 tar -cJf qt6_win.tar.xz qt-windows/
 
-New-Item -ItemType Directory -Path "$TASK_WORKDIR/public/build" -Force
-Move-Item -Path qt6_win.tar.xz -Destination "$TASK_WORKDIR/public/build"
+New-Item -ItemType Directory -Path "$env:TASK_WORKDIR/public/build" -Force
+Move-Item -Path qt6_win.tar.xz -Destination "$env:TASK_WORKDIR/public/build"
 
 Write-Output "Build complete, tarball created:"
 
