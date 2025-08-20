0e7aa4931d8899bc4e6eb84b5b64b33806222ad2
Naomi Kirby <nkirby@mozilla.com>
2025-08-20 11:15:31 -0700

    Try to shrink down object paths by building out-of-tree

--- taskcluster/scripts/toolchain/compile_qt_6.ps1
+++ taskcluster/scripts/toolchain/compile_qt_6.ps1
@@ -7,15 +7,20 @@ Get-ChildItem env:
 
 $QT_SRC_ARCHIVE =resolve-path "$env:MOZ_FETCHES_DIR/qt-everywhere-src-*.zip"
 
-unzip -o -qq -d "$env:MOZ_FETCHES_DIR" $QT_SRC_ARCHIVE
+unzip -o -qq -d "$env:TASK_WORKDIR" $QT_SRC_ARCHIVE
 
 # Activate the visual studio developer shell.
 $VS_SHELL_HELPER = resolve-path "$env:MOZ_FETCHES_DIR/*/enter_dev_shell.ps1"
 . "$VS_SHELL_HELPER"
 
+$QT_SRC_PATH = (resolve-path "$env:TASK_WORKDIR/qt-everywhere-src-*/configure.bat" | Split-Path -Parent)
+$QT_BUILD_PATH = "$env:TASK_WORKDIR/qt-build"
 $QT_INSTALL_PATH = "$env:TASK_WORKDIR/qt-windows"
-if(!(Test-Path qt-windows)){
+if(!(Test-Path $QT_INSTALL_PATH)){
   New-Item -Path qt-windows -ItemType "directory"
+}
+if(!(Test-Path $QT_BUILD_PATH)){
+  New-Item -Path qt-build -ItemType "directory"
 }
 Copy-Item -Path "$env:VCS_PATH/taskcluster/scripts/toolchain/configure_qt.ps1" -Destination qt-windows/
 
@@ -29,78 +34,75 @@ if (Test-Path -Path $SSL_PATH) {
 
 $ErrorActionPreference = "Stop"
 
-$BUILD_PREFIX = (resolve-path "$env:TASK_WORKDIR/qt-windows").toString()
-$QT_SRC_PATH = (resolve-path "$env:MOZ_FETCHES_DIR/qt-everywhere-src-*/configure.bat" | Split-Path -Parent)
-Set-Location $QT_SRC_PATH
-
 # Let's trim what we dont need.
 # See for general config: https://github.com/qt/qtbase/blob/dev/config_help.txt
 # For detailed feature flags, run the configuration, then check the CMakeLists.txt
 # Variables with FEATURE_XYZ can be switched off using -no-feature
 # Whole folders can be skipped using -skip <folder>
-./configure.bat `
-  -static  `
-  -opensource  `
-  -debug-and-release `
-  -confirm-license  `
-  -silent  `
-  -make libs  `
-  -nomake tests  `
-  -nomake examples  `
-  -no-feature-dynamicgl `
-  -no-feature-sql-odbc `
-  -no-feature-pixeltool `
-  -no-feature-qdbus `
-  -no-feature-qtattributionsscanner `
-  -no-feature-qtdiag `
-  -no-feature-qtplugininfo `
-  -no-feature-pixeltool `
-  -no-feature-distancefieldgenerator `
-  -no-feature-designer `
-  -no-feature-assistant `
-  -no-feature-sql-sqlite `
-  -no-feature-sql `
-  -no-feature-textodfwriter `
-  -no-feature-networklistmanager `
-  -no-feature-dbus `
-  -skip qtgraphs `
-  -skip qt3d  `
-  -skip qtdoc `
-  -skip qtgrpc `
-  -skip qtconnectivity `
-  -skip qtquickeffectmaker `
-  -skip qtwebengine  `
-  -skip qtlocation `
-  -skip qtserialport  `
-  -skip qtsensors  `
-  -skip qtgamepad  `
-  -skip qtandroidextras  `
-  -skip qtquick3dphysics `
-  -skip qtactiveqt  `
-  -skip qtcoap  `
-  -skip qtgrpc  `
-  -skip qtremoteobjects  `
-  -skip qtlottie  `
-  -skip qtmqtt  `
-  -skip qtopcua  `
-  -skip qtpositioning  `
-  -skip qtquick3d  `
-  -skip qtscxml  `
-  -skip qtserialbus  `
-  -skip qtserialport  `
-  -skip qtspeech  `
-  -skip qtwayland  `
-  -skip qtvirtualkeyboard  `
-  -skip qtweb `
-  -feature-imageformat_png  `
-  -qt-libpng  `
-  -qt-zlib  `
-  -prefix $BUILD_PREFIX `
+Start-Process -WorkingDirectory "$QT_BUILD_PATH" -NoNewWindow -Wait "$QT_SRC_PATH\configure.bat" -ArgumentList @(
+  '-static'
+  '-opensource'
+  '-debug-and-release'
+  '-confirm-license'
+  '-silent'
+  '-make libs'
+  '-nomake tests'
+  '-nomake examples'
+  '-no-feature-dynamicgl'
+  '-no-feature-sql-odbc'
+  '-no-feature-pixeltool'
+  '-no-feature-qdbus'
+  '-no-feature-qtattributionsscanner'
+  '-no-feature-qtdiag'
+  '-no-feature-qtplugininfo'
+  '-no-feature-pixeltool'
+  '-no-feature-distancefieldgenerator'
+  '-no-feature-designer'
+  '-no-feature-assistant'
+  '-no-feature-sql-sqlite'
+  '-no-feature-sql'
+  '-no-feature-textodfwriter'
+  '-no-feature-networklistmanager'
+  '-no-feature-dbus'
+  '-skip qtgraphs'
+  '-skip qt3d'
+  '-skip qtdoc'
+  '-skip qtgrpc'
+  '-skip qtconnectivity'
+  '-skip qtquickeffectmaker'
+  '-skip qtwebengine'
+  '-skip qtlocation'
+  '-skip qtserialport'
+  '-skip qtsensors'
+  '-skip qtgamepad'
+  '-skip qtandroidextras'
+  '-skip qtquick3dphysics'
+  '-skip qtactiveqt'
+  '-skip qtcoap'
+  '-skip qtgrpc'
+  '-skip qtremoteobjects'
+  '-skip qtlottie'
+  '-skip qtmqtt'
+  '-skip qtopcua'
+  '-skip qtpositioning'
+  '-skip qtquick3d'
+  '-skip qtscxml'
+  '-skip qtserialbus'
+  '-skip qtserialport'
+  '-skip qtspeech'
+  '-skip qtwayland'
+  '-skip qtvirtualkeyboard'
+  '-skip qtweb'
+  '-feature-imageformat_png'
+  '-qt-libpng'
+  '-qt-zlib'
+  "-prefix $QT_INSTALL_PATH"
+)
 
 # Build and install Qt
-cmake --build . --parallel
-cmake --install . --config Debug
-cmake --install . --config Release
+cmake --build $QT_BUILD_PATH --parallel
+cmake --install $QT_BUILD_PATH --config Debug
+cmake --install $QT_BUILD_PATH --config Release
 
 Set-Location $env:TASK_WORKDIR
 tar -cJf qt6_win.tar.xz qt-windows/
