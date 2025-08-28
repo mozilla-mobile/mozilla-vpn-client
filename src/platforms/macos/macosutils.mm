60afffeb8162280889d512103aa1df195e7d452f
Naomi Kirby <oskirby@gmail.com>
2025-08-28 08:50:20 -0700

    VPN-7230: Fix navigation test failure on macOS (#10748)
    * Use unregisterWithCompletionHandler for login item removal
    * Limit SSL intervention certs to Windows only

--- src/platforms/macos/macosutils.mm
+++ src/platforms/macos/macosutils.mm
@@ -60,16 +60,18 @@ void MacOSUtils::enableLoginItem(bool startAtBoot) {
 
     if (startAtBoot) {
       if (![[SMAppService mainAppService] registerAndReturnError: & error]) {
-        logger.error() << "Failed to register Mozilla VPN LoginItem: " << error.localizedDescription;
+        logger.error() << "Failed to register Mozilla VPN LoginItem: " << error;
       } else {
         logger.debug() << "Mozilla VPN LoginItem registered successfully.";
       }
     } else {
-      if (![[SMAppService mainAppService] unregisterAndReturnError: & error]) {
-        logger.error() << "Failed to unregister Mozilla VPN LoginItem: " << error.localizedDescription;
-      } else {
-        logger.debug() << "LoginItem unregistered successfully.";
-      }
+      [[SMAppService mainAppService] unregisterWithCompletionHandler:^(NSError* error){
+        if (error != nil) {
+          logger.warning() << "Failed to unregister Mozilla VPN LoginItem:" << error;
+        } else {
+          logger.debug() << "LoginItem unregistered successfully.";
+        }
+      }];
     }
   } else {
     CFStringRef cfs = (__bridge CFStringRef) loginItemAppId;
