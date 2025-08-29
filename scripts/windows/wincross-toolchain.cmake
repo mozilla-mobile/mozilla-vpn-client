43f316d5a54f8e64de6eec4ef3cfb3dcd835095b
Naomi Kirby <oskirby@gmail.com>
2025-08-29 15:29:09 -0700

    Set the clang cross-compilation target too

--- scripts/windows/wincross-toolchain.cmake
+++ scripts/windows/wincross-toolchain.cmake
@@ -30,9 +30,9 @@ endif()
 if(EXISTS $ENV{CONDA_PREFIX}/xwin)
     cmake_path(CONVERT "$ENV{CONDA_PREFIX}/xwin" TO_CMAKE_PATH_LIST XWIN_PREFIX)
 
-    set(CMAKE_C_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link")
-    set(CMAKE_CXX_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link")
-    set(CMAKE_RC_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link")
+    set(CMAKE_C_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link -target ${CMAKE_SYSTEM_PROCESSOR}-pc-windows-msvc")
+    set(CMAKE_CXX_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link -target ${CMAKE_SYSTEM_PROCESSOR}-pc-windows-msvc")
+    set(CMAKE_RC_FLAGS_INIT "/winsysroot ${XWIN_PREFIX} -fuse-ld=lld-link -target ${CMAKE_SYSTEM_PROCESSOR}-pc-windows-msvc")
 
     set(CMAKE_EXE_LINKER_FLAGS_INIT "/winsysroot:${XWIN_PREFIX}")
     set(CMAKE_MODULE_LINKER_FLAGS_INIT "/winsysroot:${XWIN_PREFIX}")
