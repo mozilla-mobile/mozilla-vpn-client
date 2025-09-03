13b1981ecd82da68be4c009db9ca9e6d4e10d6d3
Naomi Kirby <oskirby@gmail.com>
2025-09-03 08:54:28 -0700

    VPN-6767: Add a Qt next build for Windows (#10724)
    * Add a Qt next toolchain task for Windows
    * Add a fetch task for windows Qt zip archive
    * Upgrade to MSVC 2022 (manifest version 17)
    * Add a toolchain task to download Visual Studio
    * Support building Qt with VS2022
    * Version bump llvm/clang to 20.1.8
    * Specify MSVC version 17.3
    * Tweak enter_dev_shell.ps1
    * Try to add CMake and MSM packages too
    * Use the VSIX fetch script to install cmake then
    * Use urllib instead in fetch-vsix-package.py
    * The vs toolchain needs to depend on fetch-vsix-package.py
    * Try to shrink down object paths by building out-of-tree
    * Cleanup error handling
    * Switch to VisualStudio LTSC 17.8
    * Let's try building the app!
    * Try switching back to the Qt tarball
    * Try excluding qtwebengine while extracting

---
+++ taskcluster/scripts/toolchain/download_vs.ps1
@@ -0,0 +1,42 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+if (Test-Path Env:TASK_WORKDIR) {
+    Set-Location "$env:TASK_WORKDIR"
+} else {
+    $env:TASK_WORKDIR = Get-Location
+    $env:VCS_PATH = Resolve-Path "$PSScriptRoot\..\..\.."
+}
+Get-ChildItem env:
+
+# Fetch vsdownload.py from the msvc-wine repository.
+$MSVC_WIN_COMMIT = "cb78cc0bc91a9e3da69989b76b99d6f44a7d1a69"
+$VSDOWNLOAD_URL = "https://github.com/mstorsjo/msvc-wine/raw/$MSVC_WIN_COMMIT/vsdownload.py"
+Invoke-WebRequest -Uri $VSDOWNLOAD_URL -OutFile "$env:TASK_WORKDIR\vsdownload.py"
+
+$MSVC_VERSION = "17.8"
+$VS_EXTRA_PACKAGES = @(
+    'Microsoft.VisualStudio.Component.VC.Redist.MSM'
+    'Microsoft.VisualStudio.VC.CMake'
+    'Microsoft.VisualStudio.VC.CMake.Project'
+)
+
+# Download the Visual Studio SDK
+New-Item -ItemType Directory -Path "$env:TASK_WORKDIR\vs2022" -Force
+Copy-Item -Path "$PSScriptRoot\enter_dev_shell.ps1" -Destination "$env:TASK_WORKDIR\vs2022"
+python $env:TASK_WORKDIR\vsdownload.py `
+    --accept-license `
+    --skip-recommended `
+    --msvc-version $MSVC_VERSION `
+    --dest "$env:TASK_WORKDIR\vs2022"
+
+# Download additional extensions
+python "$env:VCS_PATH\scripts\windows\fetch-vsix-package.py" --ltsc-version $MSVC_VERSION --output "$env:TASK_WORKDIR\vs2022" $VS_EXTRA_PACKAGES
+
+# Compress the Visual Studio SDK
+New-Item -ItemType Directory -Path "$env:TASK_WORKDIR\public\build" -Force
+#Compress-Archive -Path "$env:TASK_WORKDIR\vs2022" -DestinationPath "$env:TASK_WORKDIR\public\build\visual-studio-2022.zip"
+zip -r -9 "$env:TASK_WORKDIR\public\build\visual-studio-2022.zip" vs2022
+
+Write-Output "Build complete, archive created"
