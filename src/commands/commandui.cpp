edfa5f1dc63250f531934f8318c6824a49abc9ab
Sebastian Streich <sstreich@mozilla.com>
2025-09-23 16:40:58 +0200

    Put things back i removed

--- src/commands/commandui.cpp
+++ src/commands/commandui.cpp
@@ -360,6 +360,10 @@ int CommandUI::run(QStringList& tokens) {
                      &MacOSMenuBar::controllerStateChanged);
 
 #endif
+    NotificationHandler* notificationHandler = NotificationHandler::create(qApp);
+    QObject::connect(vpn->controller(), &Controller::stateChanged,notificationHandler,
+                     &NotificationHandler::showNotification);
+
 
     QObject::connect(
         SettingsHolder::instance(), &SettingsHolder::languageCodeChanged, []() {
