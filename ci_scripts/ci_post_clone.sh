1b8d9a60a1217ec0339d76f74bc7bfb91d3aac83
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2025-10-02 10:29:55 -0700

    forgot one

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -33,7 +33,7 @@ EOF
 export INDEX_KEY=mozillavpn.v2.mozillavpn.cache.level-3.toolchains.v3.conda-ios-x86_64-6.6.0.latest
 curl -L https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/$INDEX_KEY/artifacts/public%2Fbuild%2Fconda-ios.tar.xz --output conda-ios.tar.xz
 
-tar -xf conda-ios.tar.gz 
+tar -xf conda-ios.tar.xz
 # Activate the Conda-ENV
 source bin/activate
 conda-unpack
