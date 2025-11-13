704ed40d2c0d9aeb4d60b190c69d55c4cde1720a
Naomi Kirby <oskirby@gmail.com>
2025-11-12 16:09:27 -0800

    Add check to prevent webextension access to other binaries

--- src/webextension/server_windows.cpp
+++ src/webextension/server_windows.cpp
@@ -4,6 +4,41 @@
 
 #include "server.h"
 
+#include <Windows.h>
+#include <winsvc.h>
+
+constexpr const DWORD MODULE_FILENAME_MAX = 32768;
+
 bool WebExtension::Server::isAllowedToConnect(qintptr sd) {
-  return true;
+  wchar_t clientModuleName[MODULE_FILENAME_MAX];
+  wchar_t selfModuleName[MODULE_FILENAME_MAX];
+  
+  // Get the filename of the current process.
+  DWORD len = ::GetModuleFileNameW(nullptr, selfModuleName, MODULE_FILENAME_MAX);
+  if (!len || (len > MODULE_FILENAME_MAX)) {
+    return false;
+  }
+
+  // Get a handle to the client process at the other end of the named pipe.
+  ULONG clientPid = 0;
+  if (!GetNamedPipeClientProcessId(reinterpret_cast<HANDLE>(sd), &clientPid)) {
+    return false;
+  }
+  if (clientPid == 0) {
+    return false;
+  }
+
+  HANDLE clientProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE,
+                                  clientPid);
+  if (!clientProc) {
+    return false;
+  }
+  auto procGuard = qScopeGuard([&] { ::CloseHandle(clientProc); });
+
+  // The client process should have the same module name.
+  len = MODULE_FILENAME_MAX;
+  if (!QueryFullProcessImageNameW(clientProc, 0, clientModuleName, &len)) {
+    return false;
+  }
+  return wcscmp(selfModuleName, clientModuleName) == 0;
 }
