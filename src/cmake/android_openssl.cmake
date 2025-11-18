bac0017735c1e11c15ba9dcfd19d67de5244a143
MCleinman <9295855+mcleinman@users.noreply.github.com>
2025-11-18 07:17:56 -0800

    VPN-6600 page size update (#10869)

--- src/cmake/android_openssl.cmake
+++ src/cmake/android_openssl.cmake
@@ -3,80 +3,47 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # This section integrates the OpenSSL-Prefab into our build system.  Google
-# Publishes builds of openSSL for android on maven. 
+# Publishes builds of openSSL for android on maven.
+# However, that Google Maven one is out of date. So we use the KDAB one.
 
-include(ExternalProject)
-
-ExternalProject_Add(ndk_openssl
-  URL https://maven.google.com/com/android/ndk/thirdparty/openssl/1.1.1q-beta-1/openssl-1.1.1q-beta-1.aar
-  URL_HASH SHA256=a5b05c4b362d35c022238ef9b2e4e2196248adea3bac9dd683845ee75a3a8d66
-  BUILD_IN_SOURCE 1
-  DOWNLOAD_NAME "openssl_android.zip" # Save it as zip, so cmake knows to extract.
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-)
-
-# Sadly, since we're not building the client in the gradle enviroment
-# We can't use this "raw". 
-# These 2 Tasks fetch the artifact and unpack them, making it usable for us.
-
-# It will Set 3 Global Properties to read from 
-# OPENSSL_CRYPTO_MODULE -> the prefab path of ssl::crypto (abi-checked)
+# It will set 3 Global Properties to read from 
 # OPENSSL_SSL_MODULE -> the prefab path of ssl::ssl (abi-checked)
-# OPENSSL_LIBS -> a folder containing both shared libarys.
-# Note: OPENSSL_LIBS has each file 2 times:
-# once named <libname>.so and <libname>_1_1.so
+# OPENSSL_LIBS -> a folder containing all shared libraries.
+# Note: OPENSSL_LIBS has each file 3 times:
+# once named <libname>.so and <libname>_1_1.so and <libname>_3.so
 # QT only seeems to load them if they are called x.1_1.so
 # For the Rust/Openssl-sys crate we need x.so
+# For newer versions of QT (6.4+), we need 3.x.x bundled alongside.
 
-SET( _OPENSSL_CRYPTO_MODULE "${CMAKE_BINARY_DIR}/ndk_openssl-prefix/src/ndk_openssl/prefab/modules/crypto")
-SET( _OPENSSL_SSL_MODULE "${CMAKE_BINARY_DIR}/ndk_openssl-prefix/src/ndk_openssl/prefab/modules/ssl")
-SET( _OPENSSL_LIBS "${CMAKE_BINARY_DIR}/ndk_openssl/libs") 
-SET( _OPENSSL_INCLUDE "${CMAKE_BINARY_DIR}/ndk_openssl/include") 
-
-file(MAKE_DIRECTORY ${_OPENSSL_LIBS})
-file(MAKE_DIRECTORY ${_OPENSSL_INCLUDE})
-
-set_property(GLOBAL PROPERTY OPENSSL_CRYPTO_MODULE ${_OPENSSL_CRYPTO_MODULE})
-set_property(GLOBAL PROPERTY OPENSSL_SSL_MODULE ${_OPENSSL_SSL_MODULE})
-set_property(GLOBAL PROPERTY OPENSSL_LIBS ${_OPENSSL_LIBS}) 
-
-# Copy them in one folder so that they can be used in 
-# rust-openssl's env:OPENSSL_LIB_DIR
-# and qt's extra libs. 
 add_custom_target(ndk_openssl_merged)
-add_dependencies(ndk_openssl_merged ndk_openssl)
-add_custom_command(
-        TARGET ndk_openssl_merged
-        COMMAND ${CMAKE_COMMAND} -E copy ${_OPENSSL_SSL_MODULE}/libs/android.${ANDROID_ABI}/libssl.so ${_OPENSSL_LIBS}/libssl_1_1.so
-        COMMAND ${CMAKE_COMMAND} -E copy ${_OPENSSL_CRYPTO_MODULE}/libs/android.${ANDROID_ABI}/libcrypto.so ${_OPENSSL_LIBS}/libcrypto_1_1.so
-        COMMAND ${CMAKE_COMMAND} -E copy ${_OPENSSL_SSL_MODULE}/libs/android.${ANDROID_ABI}/libssl.so ${_OPENSSL_LIBS}/libssl.so
-        COMMAND ${CMAKE_COMMAND} -E copy ${_OPENSSL_CRYPTO_MODULE}/libs/android.${ANDROID_ABI}/libcrypto.so ${_OPENSSL_LIBS}/libcrypto.so)
 
-
-# In case of newer versions of QT, for QtSLL we need to 
-# have 3.x.x bundled alongside.
-# Some rust crates seem to need 1.x? so ugh. both i guess. 
-# This piece just add's the _3.so libs into the merged folder.
 find_package(Qt6 COMPONENTS Core)
-if( ${Qt6_VERSION} VERSION_GREATER_EQUAL 6.4.0)
-  include(FetchContent)
+include(FetchContent)
 
-  # Google does not provide openssl v 3.0
-  # so let's use the builds from kdab
-  FetchContent_Declare(
-    android_openssl
-    DOWNLOAD_EXTRACT_TIMESTAMP true
-    GIT_REPOSITORY https://github.com/KDAB/android_openssl.git
-    GIT_TAG 0025bbe48f69792f95e02c9289df0fae68c954d6 # Latest commit as of July 2nd, 2025
-  )
-  FetchContent_MakeAvailable(android_openssl)
+FetchContent_Declare(
+  android_openssl
+  DOWNLOAD_EXTRACT_TIMESTAMP true
+  GIT_REPOSITORY https://github.com/KDAB/android_openssl.git
+  GIT_TAG 32ebe304ff064a9affb699b2185af78e3494f49a # Latest commit as of November 17, 2025
+)
+FetchContent_MakeAvailable(android_openssl)
 
-  add_custom_command(
-    TARGET ndk_openssl_merged
-    COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_3/${ANDROID_ABI}/libssl_3.so ${_OPENSSL_LIBS}/libssl_3.so
-    COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_3/${ANDROID_ABI}/libcrypto_3.so ${_OPENSSL_LIBS}/libcrypto_3.so
-  )
-endif()
+SET( _OPENSSL_MODULE "${android_openssl_SOURCE_DIR}/ssl_1.1/${ANDROID_ABI}/")
+SET( _OPENSSL_LIBS "${android_openssl_SOURCE_DIR}/combined_libs") 
+file(MAKE_DIRECTORY ${_OPENSSL_LIBS})
 
+set_property(GLOBAL PROPERTY OPENSSL_SSL_MODULE ${_OPENSSL_MODULE})
+set_property(GLOBAL PROPERTY OPENSSL_LIBS ${_OPENSSL_LIBS})
+
+# Copy them in one folder so that they can be used in
+# rust-openssl's env:OPENSSL_LIB_DIR
+# and qt's extra libs.
+add_custom_command(
+  TARGET ndk_openssl_merged
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_3/${ANDROID_ABI}/libssl_3.so ${_OPENSSL_LIBS}/libssl_3.so
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_3/${ANDROID_ABI}/libcrypto_3.so ${_OPENSSL_LIBS}/libcrypto_3.so
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_1.1/${ANDROID_ABI}/libssl.so ${_OPENSSL_LIBS}/libssl.so
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_1.1/${ANDROID_ABI}/libcrypto.so ${_OPENSSL_LIBS}/libcrypto.so
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_1.1/${ANDROID_ABI}/libssl_1_1.so ${_OPENSSL_LIBS}/libssl_1_1.so
+  COMMAND ${CMAKE_COMMAND} -E copy ${android_openssl_SOURCE_DIR}/ssl_1.1/${ANDROID_ABI}/libcrypto_1_1.so ${_OPENSSL_LIBS}/libcrypto_1_1.so
+)
