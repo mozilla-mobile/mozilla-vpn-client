73103e97e1ba83323c84e4c4441e23eb04906cf1
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2025-12-29 11:46:58 -0800

    make linter happy

--- src/feature/featuremodel.cpp
+++ src/feature/featuremodel.cpp
@@ -4,11 +4,6 @@
 
 #include "featuremodel.h"
 
-#include "feature.h"
-#include "logger.h"
-#include "qmlengineholder.h"
-#include "settingsholder.h"
-
 #include <QHash>
 #include <QJsonArray>
 #include <QJsonDocument>
@@ -16,6 +11,11 @@
 #include <QJsonValue>
 #include <QProcessEnvironment>
 #include <QQmlEngine>
+
+#include "feature.h"
+#include "logger.h"
+#include "qmlengineholder.h"
+#include "settingsholder.h"
 
 namespace {
 FeatureModel* s_instance = nullptr;
