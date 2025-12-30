88155035d130afa872b663efc50610d115fcbe26
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2025-12-30 09:14:46 -0800

    improvements

--- src/commands/commandui.cpp
+++ src/commands/commandui.cpp
@@ -176,9 +176,6 @@ int CommandUI::run(QStringList& tokens) {
   }
 #endif
 
-#ifdef MVPN_WEBEXTENSION
-  std::unique_ptr<WebExtension::Server> extensionServer{nullptr};
-#endif
   std::unique_ptr<KeyRegenerator> keyRegenerator{nullptr};
 
   // Ensure that external styling hints are disabled.
@@ -382,11 +379,11 @@ int CommandUI::run(QStringList& tokens) {
 #endif
 
 #ifdef MVPN_WEBEXTENSION
-    QPointer newServer =
+    QPointer webExtensionServer =
         new WebExtension::Server{new WebExtensionAdapter(qApp)};
-    extensionServer.reset();
     QObject::connect(vpn->controller(), &Controller::readyToQuit,
-                     extensionServer.get(), &WebExtension::Server::close);
+                     webExtensionServer.get(), &WebExtension::Server::close);
+    webExtensionServer.clear();
 #endif
 
 #ifdef MZ_ANDROID
