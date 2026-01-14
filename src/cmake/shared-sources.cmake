dfe3f230d78d36a67526fb17b9002927fe7389b0
Sebastian Streich <strseb@users.noreply.github.com>
2026-01-14 15:36:14 +0100

    Revamp Sentry Compile flow (#10918)
    Import the Sentry Subproject into our project instead of considering this an external job

--- src/cmake/shared-sources.cmake
+++ src/cmake/shared-sources.cmake
@@ -183,7 +183,7 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR
 endif()
 
 include(${CMAKE_SOURCE_DIR}/src/platforms/${MZ_PLATFORM_NAME}/sources.cmake)
-include(${CMAKE_SOURCE_DIR}/src/cmake/sentry.cmake)
+add_subdirectory(${CMAKE_SOURCE_DIR}/src/sentry)
 
 add_dependencies(shared-sources translations)
 
