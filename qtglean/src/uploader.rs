3e442166c75a3fdf7c303ddb20f50011fe4eb162
Naomi Kirby <oskirby@gmail.com>
2026-01-29 19:38:53 -0800

    Rewrite glean uploader to use Qt network libs

--- qtglean/src/uploader.rs
+++ qtglean/src/uploader.rs
@@ -3,21 +3,42 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use glean::net::{PingUploader, UploadResult, PingUploadRequest, CapablePingUploadRequest};
-use reqwest::blocking::Client;
-use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::os::raw::c_uchar;
+use std::os::raw::c_int;
+use std::ptr;
 use std::sync::Mutex;
 use std::sync::Arc;
 
+#[repr(C)]
+pub struct vpn_ping_header {
+    name: *const c_char,
+    value: *const c_char,
+}
+
+#[repr(C)]
+pub struct vpn_ping_payload {
+    url: *const c_char,
+    body: *const c_uchar,
+    length: usize,
+    has_info: bool,
+    headers: *const vpn_ping_header,
+}
+
+// The actual upload is handled in C++ using the Qt network library.
+unsafe extern "C" {
+    fn vpn_ping_upload(payload: *const vpn_ping_payload) -> c_int;
+}
+
 #[derive(Debug)]
 pub struct VPNPingUploader {
-    client: Client,
     forbidden_ping_list: Arc<Mutex<Vec<String>>>,
 }
 
 impl VPNPingUploader {
     pub fn new(forbidden_pings :Arc<Mutex<Vec<String>>> ) -> VPNPingUploader {
         VPNPingUploader {
-            client: Client::new(),
             forbidden_ping_list: forbidden_pings
         }
     }
@@ -31,6 +52,19 @@ impl VPNPingUploader {
         // Otherwise the ping_name MUST NOT be in the list.
         return data.iter().all(|e| e != &req.ping_name);
     }
+
+    fn free_ffi_headers(&self, headers: Vec<vpn_ping_header>) {
+        unsafe {
+            for x in headers {
+                if !x.name.is_null() {
+                    let _ = CString::from_raw(x.name as *mut c_char);
+                }
+                if !x.value.is_null() {
+                    let _ = CString::from_raw(x.value as *mut c_char);
+                }
+            }
+        }
+    }
 }
 
 impl PingUploader for VPNPingUploader {
@@ -41,37 +75,48 @@ impl PingUploader for VPNPingUploader {
             // so the data is dropped and not retried later. 
             return UploadResult::http_status(200); 
         }
-        let mut parsed_headers = HeaderMap::new();
+        let Ok(url) = CString::new(upload_request.url) else {
+            return UploadResult::unrecoverable_failure();
+        };
+
+        // Garbage collect the raw CStrings
+        let mut ffiHeaders: Vec<vpn_ping_header> = Vec::new();
+        ffiHeaders.reserve(upload_request.headers.len() + 1);
         for (name, value) in upload_request.headers {
-            if let (Ok(parsed_name), Ok(parsed_value)) = (
-                HeaderName::from_bytes(&name.as_bytes()),
-                HeaderValue::from_str(&value),
-            ) {
-                parsed_headers.insert(parsed_name, parsed_value);
-            }
+            let Ok(cName) = CString::new(name) else {
+                self.free_ffi_headers(ffiHeaders);
+                return UploadResult::unrecoverable_failure();
+            };
+            let Ok(cValue) = CString::new(value) else {
+                self.free_ffi_headers(ffiHeaders);
+                return UploadResult::unrecoverable_failure();
+            };
+            ffiHeaders.push(vpn_ping_header{
+                name: cName.into_raw(),
+                value: cValue.into_raw(),
+            });
         }
+        ffiHeaders.push(vpn_ping_header{
+            name: ptr::null(),
+            value: ptr::null(),
+        });
 
-        let response = self
-            .client
-            .post(upload_request.url)
-            .headers(parsed_headers)
-            .body(upload_request.body)
-            .send();
-
-        let status = match response {
-            Ok(result) => Some(result.status()),
-            Err(error) => error.status(),
+        let payload = vpn_ping_payload{
+            url: url.as_ptr(),
+            body: upload_request.body.as_ptr(),
+            length: upload_request.body.len(),
+            has_info: upload_request.body_has_info_sections,
+            headers: ffiHeaders.as_ptr(),
         };
+        let response = unsafe { vpn_ping_upload(&payload) };
+        self.free_ffi_headers(ffiHeaders);
 
-        if status.is_none() {
-            // If we didn't even get a status this is likely a network issue i.e. recoverable.
-            return UploadResult::recoverable_failure();
+        if response < 0 {
+            UploadResult::unrecoverable_failure()
+        } else if response == 0 {
+            UploadResult::done()
+        } else {
+            UploadResult::http_status(response)
         }
-
-        if let Ok(parsed_status) = i32::try_from(status.unwrap().as_u16()) {
-            return UploadResult::http_status(parsed_status);
-        };
-
-        UploadResult::unrecoverable_failure()
     }
 }
