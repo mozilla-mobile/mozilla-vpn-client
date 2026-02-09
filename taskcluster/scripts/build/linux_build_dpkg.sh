02b2ea84a0aedf481d02eeacce050a202cf30dcf
Naomi Kirby <oskirby@gmail.com>
2026-02-09 12:35:26 -0800

    Use the cargo-web package to support newer rust

--- taskcluster/scripts/build/linux_build_dpkg.sh
+++ taskcluster/scripts/build/linux_build_dpkg.sh
@@ -89,10 +89,7 @@ dch -c $(pwd)/mozillavpn-source/debian/changelog -v ${DPKG_PACKAGE_DIST_VERSION}
 if [[ "$STATICQT" == "Y" ]]; then
   export PATH=${MOZ_FETCHES_DIR}/qt-linux/bin:${PATH}
   sed -rie '/\s+(qt6-|qml6-|libqt6|qmake)/d' $(pwd)/mozillavpn-source/debian/control
-  # Remove the cargo, golang and cmake dependencies if manually installed
-  if which cargo >/dev/null 2>&1; then
-    sed -rie '/\s+(cargo)/d' $(pwd)/mozillavpn-source/debian/control
-  fi
+  # Remove the golang and cmake dependencies if manually installed
   if which cmake >/dev/null 2>&1; then
     sed -rie '/\s+(cmake)/d' $(pwd)/mozillavpn-source/debian/control
   fi
