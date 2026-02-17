bdbfca9fdd8870718af3f5f36b2cf964f34891ca
Matt Cleinman <9295855+mcleinman@users.noreply.github.com>
2026-02-17 10:55:55 -0800

    VPN-7489 2.32 addon for older macOS versions

---
+++ addons/message_update_v2.32/osCheck.js
@@ -0,0 +1,27 @@
+// Only enable for macOS 11 and 12
+(function(api, condition) {
+// Then check for macOS...
+const isMacOS = (api.env.platform === 'macos');
+if (!isMacOS) {
+  // This shouldn't be possible. Do not show.
+  condition.disable();
+  return;
+}
+
+// Platform filtering is done in manifest.json, this only checks version
+const osVersion = api.env.osVersion;
+if (!osVersion || (typeof osVersion !== 'string') || osVersion.length === 0) {
+  // Something unexpected happened. Disable on failure.
+  condition.disable();
+  return;
+}
+const majorVersionString = osVersion.split('.', 1)[0];
+const majorVersion = Number(majorVersionString);
+
+// Only enable for macOS 11 or 12
+if (majorVersion === 11 || majorVersion === 12) {
+  condition.enable();
+} else {
+  condition.disable();
+}
+});
