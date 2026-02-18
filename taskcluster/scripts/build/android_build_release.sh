721ef7e361f0945a872ec06868ae4c6e323e9b84
encrypt <encrypt@labr.xyz>
2026-02-18 15:30:14 +0100

    Refactor android cmake.sh script to build two different flavors: playstore and website and updated build scrips and taskcluster files accordingly

--- taskcluster/scripts/build/android_build_release.sh
+++ taskcluster/scripts/build/android_build_release.sh
@@ -56,9 +56,11 @@ echo "Finding all .apk files in .tmp directory:"
 find .tmp -name "*.apk" -type f -exec ls -la {} \;
 
 # Artifacts should be placed here!
-mkdir -p /builds/worker/artifacts/
-cp -r .tmp/src/android-build/build/outputs/apk/release/*  /builds/worker/artifacts/
-echo "Moved .apk files in /builds/worker/artifacts:"
+mkdir -p /builds/worker/artifacts
+
+# Move and rename playstore builds
+cp -r .tmp/playstore/src/android-build/build/outputs/apk/release/*  /builds/worker/artifacts/
+echo "Moved playstore .apk files in /builds/worker/artifacts:"
 ls  /builds/worker/artifacts/
 
 echo "Renaming Artifacts"
@@ -66,7 +68,16 @@ echo "Renaming Artifacts"
 mv /builds/worker/artifacts/android-build-x86_64-release-unsigned.apk /builds/worker/artifacts/mozillavpn-x86_64-release.apk
 mv /builds/worker/artifacts/android-build-arm64-v8a-release-unsigned.apk /builds/worker/artifacts/mozillavpn-arm64-v8a-release.apk
 mv /builds/worker/artifacts/android-build-armeabi-v7a-release-unsigned.apk /builds/worker/artifacts/mozillavpn-armeabi-v7a-release.apk
-mv /builds/worker/artifacts/android-build-x86-release-unsigned.apk /builds/worker/artifacts//mozillavpn-x86-release.apk
+mv /builds/worker/artifacts/android-build-x86-release-unsigned.apk /builds/worker/artifacts/mozillavpn-x86-release.apk
+
+# Move and rename website builds
+echo "Moved website .apk files in /builds/worker/artifacts:"
+ls  /builds/worker/artifacts/
+cp -r .tmp/website/src/android-build/build/outputs/apk/release/*  /builds/worker/artifacts/
+mv /builds/worker/artifacts/android-build-x86_64-release-unsigned.apk /builds/worker/artifacts/mozillavpn-website-x86_64-release.apk
+mv /builds/worker/artifacts/android-build-arm64-v8a-release-unsigned.apk /builds/worker/artifacts/mozillavpn-website-arm64-v8a-release.apk
+mv /builds/worker/artifacts/android-build-armeabi-v7a-release-unsigned.apk /builds/worker/artifacts/mozillavpn-website-armeabi-v7a-release.apk
+mv /builds/worker/artifacts/android-build-x86-release-unsigned.apk /builds/worker/artifacts/mozillavpn-website-x86-release.apk
 ls  /builds/worker/artifacts/
 
 
