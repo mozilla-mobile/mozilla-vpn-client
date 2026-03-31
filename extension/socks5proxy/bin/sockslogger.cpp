155b6b495786971bca99c2aa36f9583d57f3f4eb
Artem Popov <apopov@mozilla.com>
2026-03-31 16:26:23 +0200

    Fix socksproxy deadlock in setLogfile() when running as a service (#11167)

--- extension/socks5proxy/bin/sockslogger.cpp
+++ extension/socks5proxy/bin/sockslogger.cpp
@@ -239,12 +239,12 @@ void SocksLogger::setLogfile(const QString& logfile) {
         QIODeviceBase::WriteOnly | QIODeviceBase::Text | QIODeviceBase::Append;
     const auto perms = QFileDevice::ReadOwner | QFileDevice::WriteOwner |
                        QFileDevice::ReadGroup;
-    const auto ok = !m_logFileDevice->open(mode);
-    if (!ok) {
-      qInfo() << "Failed open log file";
-      return;
+    if (!m_logFileDevice->open(mode)) {
+      delete m_logFileDevice;
+      m_logFileDevice = nullptr;
+    } else {
+      m_logFileDevice->setPermissions(perms);
     }
-    m_logFileDevice->setPermissions(perms);
   }
 }
 
