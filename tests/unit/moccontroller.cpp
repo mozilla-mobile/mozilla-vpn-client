2fe133488c4b18187d0a6f4328da04145c13881b
Naomi Kirby <oskirby@gmail.com>
2026-04-11 11:09:06 -0700

    Fix unit test build

--- tests/unit/moccontroller.cpp
+++ tests/unit/moccontroller.cpp
@@ -97,3 +97,5 @@ void Controller::forceDaemonSilentServerSwitch() {}
 void Controller::handleBackendFailure(ErrorCode code) {}
 
 void Controller::startKeyRegeneration() {}
+
+bool Controller::splitTunnelSupported() const { return true; }
