510579c16dcf45ba237f33bf2329e22c85c32de7
Naomi Kirby <oskirby@gmail.com>
2026-04-13 14:43:49 -0700

    Initial attempt at UDP bypass handling

---
+++ macos/split-tunnel/bypassudpflow.h
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+@interface BypassUdpFlow : NSObject
+
++ (id)createBypass:(NEAppProxyUDPFlow *)flow
+     withInterface:(nw_interface_t)interface;
+
+- (void)startBypass:(void (^)(NSError* error))completionHandler;
+
+@property (strong) NEAppProxyUDPFlow* flow;
+
+@end
