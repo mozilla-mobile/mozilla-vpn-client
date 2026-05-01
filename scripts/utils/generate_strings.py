f903e1dd70a256d63b46b57362651d68aeca0e2a
MCleinman <9295855+mcleinman@users.noreply.github.com>
2026-05-01 13:48:15 -0700

    VPN-5455: iOS app intents (#11200)
    * VPN-5455 App Intents
    
    * small tweaks
    
    * first pass at translation script
    
    * whoops
    
    * add files to xcode for building
    
    * allow for chinese
    
    * error handling for turning on VPN
    
    * improve 'turn off' error handling
    
    * improve feedback
    
    * fix color
    
    * adding in city names - but translations aren't working yet
    
    * update format
    
    * use string IDs to allow string interpolation to work
    
    * add checks
    
    * increase liklihood of showing image
    
    * skip notifications when appropriate
    
    * fix failing tests
    
    * a couple more Siri phrasings
    
    * add to l10n readme
    
    * use generic placeholder for
    
    * the linter thinks it will be happier if it knows Siri exists, but once it uses Siri...
    
    * PR feedback
    
    * one translation block per intent phrase set
    
    * make more generic
    
    * Update src/translations/strings.yaml
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update scripts/utils/generate_xcstrings.py
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update scripts/utils/generate_xcstrings.py
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * PR feedback
    
    * linting
    
    * fix formatting
    
    * fix comments
    
    * fix failing iOS build - ensure it is successful even for new strings
    
    * fix linter issue from github merge
    
    * improve translation flow
    
    * updates to cmake
    
    ---------
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>

--- scripts/utils/generate_strings.py
+++ scripts/utils/generate_strings.py
@@ -231,9 +231,13 @@ const char* const I18nStrings::_ids[] = {
             return ret.replace('"', '\\"')
 
         for key, data in strings.items():
-            output.write(f"    //% \"{serialize(data['value'])}\"\n")
+            processed = [v.replace('\n', '\\n') for v in data['value']]
+            output_value = serialize(processed)
+            output.write(f"    //% \"{output_value}\"\n")
+
             for comment in data["comments"]:
-                output.write(f"    //: {comment}\n")
+                processed = comment.replace('\n', '\\n')
+                output.write(f"    //: {processed}\n")
             output.write(f"    QT_TRID_NOOP(\"{data['string_id']}\"),\n\n")
 
         # This is done to make windows compiler happy
