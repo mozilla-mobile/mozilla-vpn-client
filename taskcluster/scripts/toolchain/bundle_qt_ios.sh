9daaf2d01a589fc7af1f8caf1185f9e4e902c4c9
Sebastian Streich <sstreich@mozilla.com>
2026-06-15 12:36:26 +0200

    No longer pull qt5compat in TC

--- taskcluster/scripts/toolchain/bundle_qt_ios.sh
+++ taskcluster/scripts/toolchain/bundle_qt_ios.sh
@@ -20,8 +20,8 @@ fi
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade aqtinstall
 
-aqt install-qt -O qt_ios mac desktop $QT_VERSION -m qtwebsockets qt5compat qtnetworkauth
-aqt install-qt -O qt_ios mac ios $QT_VERSION -m qtwebsockets qt5compat qtnetworkauth
+aqt install-qt -O qt_ios mac desktop $QT_VERSION -m qtwebsockets qtnetworkauth
+aqt install-qt -O qt_ios mac ios $QT_VERSION -m qtwebsockets qtnetworkauth
 
 zip -qr $UPLOAD_DIR/qt6_ios.zip qt_ios/*
 
