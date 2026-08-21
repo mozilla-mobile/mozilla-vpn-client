7e4f6e743113e81b1d73c5a8efdfa6c7ceb4fc46
Naomi Kirby <oskirby@gmail.com>
2026-08-21 14:50:50 +0200

    Add a webext command to return the ProxyInfo structure

--- src/webextension/webexthandler_linux.cpp
+++ src/webextension/webexthandler_linux.cpp
@@ -28,6 +28,17 @@ void WebExtHandler::proc_info(const QByteArray& msg) {
   writeJsonStdout(obj);
 }
 
+void WebExtHandler::proxy_info(const QByteArray& msg) {
+  Q_UNUSED(msg);
+
+  QJsonObject obj;
+  obj["type"] = "socks";
+  obj["host"] = "file:/var/run/mozillavpn.proxy";
+  obj["port"] = QJsonValue(1234);
+  obj["proxyDNS"] = QJsonValue(true);
+  writeJsonStdout(obj);
+}
+
 void WebExtHandler::start(const QByteArray& msg) {
   Q_UNUSED(msg);
 
