7d6b7cf88a65b9003a78dcefdd81bef48dda0862
MCleinman <9295855+mcleinman@users.noreply.github.com>
2026-09-15 11:18:50 -0700

    VPN-7776 (part 2): Remove Firefox extension promo addon (#11551)

--- addons/message_try_firefox_extension/downloadFirefox.js
+++
@@ -1,4 +0,0 @@
-((api) => {
-  api.urlOpener.openUrl(
-      `https://www.mozilla.org/firefox/new/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=extension-promotion`);
-});
--- addons/message_try_firefox_extension/getExtension.js
+++
@@ -1,4 +0,0 @@
-((api) => {
-  api.urlOpener.openUrl(
-      `https://addons.mozilla.org/firefox/addon/mozilla-vpn-extension/?utm_medium=mozilla-vpn-client&utm_source=in-app-message&utm_content=extension-promotion`);
-});
